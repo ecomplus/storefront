73518238610c4f93f5d5640e7834cea90f8066d5
Leonardo Matos <leomp120894@gmail.com>
2018-04-05 14:22:11 -0300

    frontend frameworks

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ export default {
 </script>
 
 <style>
+@import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+// frontend frameworks
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+// https://fontawesome.com/how-to-use/use-with-node-js
+import fontawesome from '@fortawesome/fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
+import regular from '@fortawesome/fontawesome-free-regular'
+import brands from '@fortawesome/fontawesome-free-brands'
+fontawesome.library.add(solid)
+fontawesome.library.add(regular)
+fontawesome.library.add(brands)
+
 Vue.use(Router)
+Vue.use(Vuex)
 
 export default new Router({
   routes: [
