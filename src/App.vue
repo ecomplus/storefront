a0713d6511a0d14ed9e57287afa8eb84ee1e3e6d
Leonardo Matos <leomp120894@gmail.com>
2018-04-05 14:53:01 -0300

    minor change, comment css

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,9 @@ export default {
 </script>
 
 <style>
+/* Bootstrap 4 */
 @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
