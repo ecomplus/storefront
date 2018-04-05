d267180e8d8ffd5900552def904273db36c38f63
Leonardo Matos <leomp120894@gmail.com>
2018-04-05 16:55:59 -0300

    set globals

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,28 @@
 
 <script>
 import Vue from 'vue'
+// E-Com Plus JS SDK
+import EcomIo from 'ecomplus-sdk'
+
+// frontend frameworks
+import $ from 'jquery'
+import 'popper.js'
+import 'bootstrap'
+// https://fontawesome.com/how-to-use/use-with-node-js
+import FontAwesome from '@fortawesome/fontawesome'
+import FaSolid from '@fortawesome/fontawesome-free-solid'
+import FaRegular from '@fortawesome/fontawesome-free-regular'
+import FaBrands from '@fortawesome/fontawesome-free-brands'
+FontAwesome.library.add(FaSolid)
+FontAwesome.library.add(FaRegular)
+FontAwesome.library.add(FaBrands)
+
 // set globally
 // prevent 'is not defined' error
 window.Vue = Vue
-// E-Com Plus storefront libraries
-import EcomIo from 'ecomplus-sdk'
 window.EcomIo = EcomIo
+window.$ = $
+// E-Com Plus storefront libraries
 require('ecomplus-passport-client')
 require('ecomplus-render')
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,6 @@ import Vuex from 'vuex'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
-// frontend frameworks
-import 'jquery'
-import 'popper.js'
-import 'bootstrap'
-// https://fontawesome.com/how-to-use/use-with-node-js
-import FontAwesome from '@fortawesome/fontawesome'
-import FaSolid from '@fortawesome/fontawesome-free-solid'
-import FaRegular from '@fortawesome/fontawesome-free-regular'
-import FaBrands from '@fortawesome/fontawesome-free-brands'
-FontAwesome.library.add(FaSolid)
-FontAwesome.library.add(FaRegular)
-FontAwesome.library.add(FaBrands)
-
 Vue.use(Router)
 Vue.use(Vuex)
 
