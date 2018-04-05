2e28a22d2f3a3c0e2696c793c40a6e6e49d781f6
Leonardo Matos <leomp120894@gmail.com>
2018-04-05 19:50:45 -0300

    work with vue-i18n 5.x

--- src/main.js
+++ src/main.js
@@ -4,25 +4,29 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// Vuex Centralized State Management
+// https://vuex.vuejs.org/en/
+import Vuex from 'vuex'
+
 // Element UI kit
 // http://element.eleme.io
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale'
+
+// internationalization
+import VueI18n from 'vue-i18n'
 import en from 'element-ui/lib/locale/lang/en'
 import br from 'element-ui/lib/locale/lang/pt-br'
-
-// Vuex Centralized State Management
-// https://vuex.vuejs.org/en/
-import Vuex from 'vuex'
 
 // setup Vuex
 Vue.use(Vuex)
 // setup Element UI fully
 Vue.use(ElementUI)
 // i18n
-locale.use(en)
-locale.use(br)
+Vue.use(VueI18n)
+Vue.config.lang = 'en_us'
+Vue.locale('en_us', en)
+Vue.locale('pt_br', br)
 
 Vue.config.productionTip = false
 
