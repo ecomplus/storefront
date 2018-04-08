f76b9d14a5bf17377e4e6b2eb045930fecf0e7f8
Leonardo Matos <leomp120894@gmail.com>
2018-04-08 12:02:31 -0300

    initialize E-Com Plus SDK JS

--- src/store/index.js
+++ src/store/index.js
@@ -8,10 +8,24 @@ import products from './modules/products'
 import customer from './modules/customer'
 import createLogger from 'vuex/dist/logger'
 
+// E-Com Plus SDK JS
+import EcomIo from 'ecomplus-sdk'
+
 // setup Vuex
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
+
+const initCallback = () => {
+  console.log('x')
+}
+if (debug) {
+  // test
+  EcomIo.init(initCallback, 1004, '5a99b7e338f4774795b90773')
+} else {
+  // production
+  EcomIo.init(initCallback)
+}
 
 export default new Vuex.Store({
   modules: {
