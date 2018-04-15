685e36a75b03fa090c6d07bf4c86bc7f65ac0518
Leonardo Matos <leomp120894@gmail.com>
2018-04-15 14:41:33 -0300

    routes components

---
+++ src/components/routes/ShoppingCart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="_cart">
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ShoppingCart',
+  computed: mapGetters([
+    'cart'
+  ])
+}
+</script>
