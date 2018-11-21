6786419a7b9b407c0796323da5feb921050c602e
Leonardo Matos <leomp120894@gmail.com>
2018-11-21 14:10:11 -0200

    custom build (dist) static folder

--- config/index.js
+++ config/index.js
@@ -20,7 +20,6 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
     /**
      * Source Maps
      */
@@ -42,7 +41,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '__static',
     assetsPublicPath: '/',
 
     /**
