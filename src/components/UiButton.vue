3c1bbfdce0f8e791216fc7a4852652b1d6bb47e7
Leonardo Matos <leomp120894@gmail.com>
2019-02-25 18:36:28 -0300

    feat: first component (button)

---
+++ src/components/UiButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :class="[ 'btn', color, size, { active } ]">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'UiButton',
+  props: {
+    color: String,
+    size: String,
+    active: Boolean
+  }
+}
+</script>
