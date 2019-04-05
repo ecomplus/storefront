87b7aa35a008abe57d8ac503c577df0411811360
Leonardo Matos <leomp120894@gmail.com>
2019-04-05 18:19:23 -0300

    build: start building storefront with ejs

---
+++ src/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+/*
+Build the strorefront for procution
+Consume E-Com Plus APIs and headless CMS content
+*/
+
+const fs = require('fs')
+// const https = require('https')
+// output directory
+const dir = './dist'
+
+// EJS for templating
+// https://ejs.co/
+const ejs = require('ejs')
+
+const build = () => {
+  // homepage
+  ejs.renderFile('./src/views/index.ejs', {}, {}, (err, html) => {
+    if (!err) {
+      fs.writeFile('./dist/index.html', html, err => {
+        if (err) {
+          return console.error(err)
+        }
+      })
+    }
+  })
+}
+
+// create dist directory or clear it
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+  build()
+} else {
+  // clear dir content
+  const rimraf = require('rimraf')
+  rimraf(dir + '/*', () => {
+    build()
+  })
+}
