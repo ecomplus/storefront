f35e8b72d17cfe9c4bc5fae15bc81d7b8b4071b0
Leonardo Matos <leomp120894@gmail.com>
2019-04-11 14:42:58 -0300

    build(feat): read Netlify CMS content folder recursivily

--- build/cms.js
+++ build/cms.js
@@ -1,16 +1,51 @@
 'use strict'
 
-// use Node.js path module for compatibility
+// Node.js modules
 const path = require('path')
+const fs = require('fs')
 // read Netlify CMS content folder recursivily
 const recursive = require('recursive-readdir')
 
 // read content JSON files
 const dir = path.resolve(process.cwd(), 'content')
-recursive(dir, [ 'README.md' ], (err, files) => {
-  if (!err) {
-    console.log(files)
-  } else {
-    console.error(err)
-  }
+const dirLength = dir.length
+const data = {}
+
+// returns a promise for the async process
+module.exports = new Promise((resolve, reject) => {
+  // start recursive dir read
+  recursive(dir, [ 'README.md' ], (err, files) => {
+    if (!err) {
+      files.forEach(file => {
+        // moving inside data dir by reference
+        let obj = data
+
+        file.slice(dirLength).split('/').forEach(path => {
+          if (path !== '') {
+            if (!obj.hasOwnProperty(path)) {
+              let filename = path.replace('.json', '')
+              if (filename === path) {
+                // folder
+                obj[path] = {}
+                obj = obj[path]
+              } else {
+                // the JSON file itself
+                try {
+                  obj[filename] = JSON.parse(fs.readFileSync(file, 'utf8'))
+                } catch (e) {
+                  // ignore invalid JSON string
+                  obj[filename] = {}
+                }
+              }
+            }
+          }
+        })
+      })
+
+      // all done
+      resolve(data)
+    } else {
+      reject(err)
+    }
+  })
 })
