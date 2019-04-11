fd1d3588be79c077b16d0dc05f42d2cb74a030a0
Leonardo Matos <leomp120894@gmail.com>
2019-04-11 15:03:32 -0300

    build(fix): async webpack config

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,6 +4,8 @@
 const path = require('path')
 // load project directories
 const { src, pub, output } = require('./paths')
+// Netlify CMS content
+const content = require('./cms')
 
 // load Webpack and plugins
 // const webpack = require('webpack')
@@ -15,88 +17,95 @@ const WorkboxPlugin = require('workbox-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const devMode = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  entry: [
-    path.resolve(src, 'js', 'index.js'),
-    path.resolve(src, 'scss', 'styles.scss')
-  ],
-  output: {
-    path: output,
-    filename: 'storefront.[chunkhash].js'
-  },
-  devServer: {
-    compress: true,
-    port: 9123
-  },
-  module: {
-    rules: [{
-      test: /\.s?css$/,
-      use: [
-        // fallback to style-loader in development
-        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-        'css-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: [
-              require('autoprefixer')()
-            ]
-          }
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+    content.catch(reject).then(cms => {
+      // resolve promise with webpack config object
+      resolve({
+        entry: [
+          path.resolve(src, 'js', 'index.js'),
+          path.resolve(src, 'scss', 'styles.scss')
+        ],
+        output: {
+          path: output,
+          filename: 'storefront.[chunkhash].js'
         },
-        'sass-loader'
-      ]
-    }, {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [ '@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 } ]
-          ]
-        }
-      }
-    }]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[chunkhash].css'
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(src, 'views', 'index.ejs')
-    }),
-    new WebpackPwaManifest({
-      name: 'My Progressive Web App',
-      short_name: 'MyPWA',
-      description: 'My awesome Progressive Web App!',
-      background_color: '#ffffff',
-      // can be null, use-credentials or anonymous
-      crossorigin: 'use-credentials' /*,
-      icons: [{
-        src: path.resolve('src/assets/icon.png'),
-        // multiple sizes
-        sizes: [ 96, 128, 192, 256, 384, 512 ]
-      }, {
-        src: path.resolve('src/assets/large-icon.png'),
-        // can also use the specifications pattern
-        size: '1024x1024'
-      }] */
-    }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true
-    }),
-    new CopyPlugin([
-      { from: pub, to: output }
-    ])
-  ],
-  stats: {
-    colors: true
-  },
-  devtool: 'source-map'
+        devServer: {
+          compress: true,
+          port: 9123
+        },
+        module: {
+          rules: [{
+            test: /\.s?css$/,
+            use: [
+              // fallback to style-loader in development
+              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: [
+                    require('autoprefixer')()
+                  ]
+                }
+              },
+              'sass-loader'
+            ]
+          }, {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [ '@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 } ]
+                ]
+              }
+            }
+          }]
+        },
+        plugins: [
+          new MiniCssExtractPlugin({
+            filename: '[name].[chunkhash].css'
+          }),
+          new CleanWebpackPlugin(),
+          new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(src, 'views', 'index.ejs')
+          }),
+          new WebpackPwaManifest({
+            name: 'My Progressive Web App',
+            short_name: 'MyPWA',
+            description: 'My awesome Progressive Web App!',
+            background_color: '#ffffff',
+            // can be null, use-credentials or anonymous
+            crossorigin: 'use-credentials' /*,
+            icons: [{
+              src: path.resolve('src/assets/icon.png'),
+              // multiple sizes
+              sizes: [ 96, 128, 192, 256, 384, 512 ]
+            }, {
+              src: path.resolve('src/assets/large-icon.png'),
+              // can also use the specifications pattern
+              size: '1024x1024'
+            }] */
+          }),
+          new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true
+          }),
+          new CopyPlugin([
+            { from: pub, to: output }
+          ])
+        ],
+        stats: {
+          colors: true
+        },
+        devtool: 'source-map'
+      })
+    })
+  })
 }
