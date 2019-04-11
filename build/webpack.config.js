3fca1b328a685f5abd124a3a81f29627f8e4d85b
Leonardo Matos <leomp120894@gmail.com>
2019-04-11 18:54:33 -0300

    build(feat): read and parse all views and cms slugs

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,6 +6,8 @@ const path = require('path')
 const { src, pub, output } = require('./paths')
 // Netlify CMS content
 const content = require('./cms')
+// read views folder recursivily
+const recursive = require('recursive-readdir')
 
 // load Webpack and plugins
 // const webpack = require('webpack')
@@ -20,110 +22,152 @@ const devMode = process.env.NODE_ENV !== 'production'
 module.exports = () => {
   return new Promise((resolve, reject) => {
     content.catch(reject).then(cms => {
-      // resolve promise with webpack config object
-      resolve({
-        entry: [
-          path.resolve(src, 'js', 'index.js'),
-          path.resolve(src, 'scss', 'styles.scss')
-        ],
-        output: {
-          path: output,
-          filename: 'storefront.[chunkhash].js'
-        },
-        devServer: {
-          compress: true,
-          port: 9123
-        },
-        stats: {
-          colors: true
-        },
-        devtool: 'source-map',
+      // setup Webpack plugins
+      const plugins = [
+        // clear dist folder
+        new CleanWebpackPlugin(),
 
-        module: {
-          rules: [
-            // parse SCSS and fix compiled CSS with Postcss
-            {
-              test: /\.s?css$/,
-              use: [
-                // fallback to style-loader in development
-                devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                'css-loader',
-                {
-                  loader: 'postcss-loader',
-                  options: {
-                    ident: 'postcss',
-                    plugins: [
-                      require('autoprefixer')()
-                    ]
+        // extract CSS to file
+        new MiniCssExtractPlugin({
+          filename: '[name].[chunkhash].css'
+        }),
+
+        // create manifest.json file
+        new WebpackPwaManifest({
+          name: 'My Progressive Web App',
+          short_name: 'MyPWA',
+          description: 'My awesome Progressive Web App!',
+          background_color: '#ffffff',
+          // can be null, use-credentials or anonymous
+          crossorigin: 'use-credentials' /*,
+          icons: [{
+            src: path.resolve('src/assets/icon.png'),
+            // multiple sizes
+            sizes: [ 96, 128, 192, 256, 384, 512 ]
+          }, {
+            src: path.resolve('src/assets/large-icon.png'),
+            // can also use the specifications pattern
+            size: '1024x1024'
+          }] */
+        }),
+
+        // create service-worker.js file
+        new WorkboxPlugin.GenerateSW({
+          // these options encourage the ServiceWorkers to get in there fast
+          // and not allow any straggling "old" SWs to hang around
+          clientsClaim: true,
+          skipWaiting: true
+        }),
+
+        // just copy files from public folder recursivily
+        new CopyPlugin([
+          { from: pub, to: output }
+        ])
+      ]
+
+      // setup common options for HTML plugin
+      const views = path.resolve(src, 'views')
+      const templateOptions = {
+        templateParameters: cms,
+        minify: !devMode
+      }
+
+      // parse EJS views to HTML files
+      recursive(views, (err, files) => {
+        if (err) {
+          reject(err)
+        } else {
+          files.forEach(template => {
+            let addView = filename => {
+              // add a view to compile
+              plugins.push(new HtmlWebpackPlugin({
+                filename,
+                template,
+                ...templateOptions
+              }))
+            }
+
+            // remove the path from template filename string
+            let filename = template.slice(views.length + 1).replace('.ejs', '')
+            if (filename.startsWith('_cms')) {
+              // compile multiple files
+              // for blog posts and extra pages
+              // remove '_cms/' to get 'blog-posts' string
+              let folder = filename.slice(5)
+              if (cms.hasOwnProperty(folder)) {
+                // render each slug
+                for (let slug in cms[folder]) {
+                  if (cms[folder].hasOwnProperty(slug)) {
+                    addView(slug)
                   }
+                }
+              }
+            } else {
+              // preseted view
+              addView(filename + '.html')
+            }
+          })
+
+          // resolve promise with webpack config object
+          resolve({
+            entry: [
+              path.resolve(src, 'js', 'index.js'),
+              path.resolve(src, 'scss', 'styles.scss')
+            ],
+            output: {
+              path: output,
+              filename: 'storefront.[chunkhash].js'
+            },
+            devServer: {
+              compress: true,
+              port: 9123
+            },
+            stats: {
+              colors: true
+            },
+            devtool: 'source-map',
+
+            module: {
+              rules: [
+                // parse SCSS and fix compiled CSS with Postcss
+                {
+                  test: /\.s?css$/,
+                  use: [
+                    // fallback to style-loader in development
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                      loader: 'postcss-loader',
+                      options: {
+                        ident: 'postcss',
+                        plugins: [
+                          require('autoprefixer')()
+                        ]
+                      }
+                    },
+                    'sass-loader'
+                  ]
                 },
-                'sass-loader'
+
+                // transpile and polyfill JS with Babel
+                {
+                  test: /\.m?js$/,
+                  exclude: /(node_modules|bower_components)/,
+                  use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: [
+                        [ '@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 } ]
+                      ]
+                    }
+                  }
+                }
               ]
             },
 
-            // transpile and polyfill JS with Babel
-            {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: [
-                    [ '@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 } ]
-                  ]
-                }
-              }
-            }
-          ]
-        },
-
-        plugins: [
-          // clear dist folder
-          new CleanWebpackPlugin(),
-
-          // extract CSS to file
-          new MiniCssExtractPlugin({
-            filename: '[name].[chunkhash].css'
-          }),
-
-          // parse EJS views to HTML files
-          new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.resolve(src, 'views', 'index.ejs')
-          }),
-
-          // create manifest.json file
-          new WebpackPwaManifest({
-            name: 'My Progressive Web App',
-            short_name: 'MyPWA',
-            description: 'My awesome Progressive Web App!',
-            background_color: '#ffffff',
-            // can be null, use-credentials or anonymous
-            crossorigin: 'use-credentials' /*,
-            icons: [{
-              src: path.resolve('src/assets/icon.png'),
-              // multiple sizes
-              sizes: [ 96, 128, 192, 256, 384, 512 ]
-            }, {
-              src: path.resolve('src/assets/large-icon.png'),
-              // can also use the specifications pattern
-              size: '1024x1024'
-            }] */
-          }),
-
-          // create service-worker.js file
-          new WorkboxPlugin.GenerateSW({
-            // these options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around
-            clientsClaim: true,
-            skipWaiting: true
-          }),
-
-          // just copy files from public folder recursivily
-          new CopyPlugin([
-            { from: pub, to: output }
-          ])
-        ]
+            plugins
+          })
+        }
       })
     })
   })
