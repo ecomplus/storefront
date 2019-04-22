450cf24c80dd9c7abfc58833580b50daedfbd22a
Leonardo Matos <leomp120894@gmail.com>
2019-04-22 19:25:55 -0300

    docs: fix demo files

---
+++ docs/demo.js
@@ -0,0 +1,66 @@
+/* global EcomCart */
+
+EcomCart.clearCart()
+
+EcomCart.addItem({
+  '_id': '951050155052163296800000',
+  'sku': 'IPZ7060-613-1',
+  'product_id': '5bbbb3ea2707d825d174c0c5',
+  'currency_symbol': 'R$',
+  'currency_id': 'BRL',
+  'price': 15,
+  'quantity': 168,
+  'name': 'Camisa / P',
+  'picture': {
+    'zoom': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1529703757624-tattly_james_gulliver_hancock_saugaro_cactus_web_design_01_grande.jpg'
+    },
+    'small': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1529703757624-tattly_james_gulliver_hancock_saugaro_cactus_web_design_01_grande.jpg'
+    },
+    'normal': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/400px/@1529703757624-tattly_james_gulliver_hancock_saugaro_cactus_web_design_01_grande.jpg'
+    },
+    'big': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/700px/@1529703757624-tattly_james_gulliver_hancock_saugaro_cactus_web_design_01_grande.jpg'
+    }
+  },
+  'keep_item_quantity': false,
+  'keep_item_price': false
+})
+
+EcomCart.addItem({
+  '_id': '540050155052164232000000',
+  'sku': 'IPZ7060-507-2',
+  'product_id': '5bbbb3ea2707d825d174c0c6',
+  'currency_symbol': 'R$',
+  'currency_id': 'BRL',
+  'price': 10,
+  'min_quantity': 100,
+  'quantity': 168,
+  'name': 'Camisa / M',
+  'picture': {
+    'zoom': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/@1539286314913-camisa.png',
+      'size': '1200x1068',
+      'alt': 'camisa'
+    },
+    'small': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/100px/@1539286314913-camisa.png',
+      'size': '100x89',
+      'alt': 'camisa'
+    },
+    'normal': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/400px/@1539286314913-camisa.png',
+      'size': '400x356',
+      'alt': 'camisa'
+    },
+    'big': {
+      'url': 'https://ecom-ptqgjveg.nyc3.digitaloceanspaces.com/imgs/700px/@1539286314913-camisa.png',
+      'size': '700x623',
+      'alt': 'camisa'
+    }
+  },
+  'keep_item_quantity': false,
+  'keep_item_price': false
+})
