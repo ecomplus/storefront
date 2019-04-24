014e4702619c536cc4b0331ef4413156fb197055
Leonardo Matos <leomp120894@gmail.com>
2019-04-24 12:48:57 -0300

    chore: setup dictionary object and function

--- src/lib/dictionary.js
+++ src/lib/dictionary.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const dictionary = {
+}
+
+export default function (word, lang) {
+  if (!lang) {
+    // use english if lang undefined
+    lang = (this && this.lang) || 'en_us'
+  }
+  return (dictionary[word] && dictionary[word][lang]) || ''
+}
