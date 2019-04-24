79c4a2051e27d4ae777dde8ba8146c0f0b50e9a4
Leonardo Matos <leomp120894@gmail.com>
2019-04-24 12:47:03 -0300

    chore: setup EcomUser component files

---
+++ src/EcomUser.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="ecom-user">
+  </div>
+</template>
+
+<script src="./EcomUser.js"></script>
+<style lang="scss" src="./EcomUser.scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import EcomUser from './EcomUser.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(EcomUser)
+}).$mount('#user')
