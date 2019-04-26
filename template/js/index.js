4db3997e795a7bc934a8f02df85f98ac85ddd800
Leonardo Matos <leomp120894@gmail.com>
2019-04-26 10:56:55 -0300

    chore: fix dependencies, jquery and popper.js not required (per project)

--- template/js/index.js
+++ template/js/index.js
@@ -1,13 +1,8 @@
 'use strict'
 
 // setup dependencies
-import $ from 'jquery/dist/jquery.slim.js'
-import 'popper.js'
-import 'bootstrap'
 import '@ecomplus/storefront-renderer/dist/storefront.min.js'
 import '@ecomplus/shopping-cart'
-
-console.log($)
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
