a3e6b3d278719afb5920163d163ca16e52d42b21
Leonardo Matos <leomp120894@gmail.com>
2019-05-22 20:54:14 -0300

    fix: try to setup consts (config) with document metadata

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -8,11 +8,11 @@ const getMeta = metaName => {
   return null
 }
 
-export const DEFAULT_LANG = getMeta('ecom_lang') || 'pt_br'
-export const DEFAULT_COUNTRY_CODE = getMeta('ecom_country_code') || 'br'
-export const DEFAULT_CURRENCY = getMeta('ecom_currency') || 'BRL'
-export const DEFAULT_CURRENCY_SYMBOL = getMeta('ecom_currency_symbol') || 'R$'
-export const STORE_ID = parseInt(process.env.ECOM_STORE_ID || getMeta('ecom_store_id'), 10)
-export const STORE_OBJECT_ID = process.env.ECOM_STORE_OBJECT_ID || getMeta('ecom_store_id')
+export const DEFAULT_LANG = getMeta('ecom-lang') || 'pt_br'
+export const DEFAULT_COUNTRY_CODE = getMeta('ecom-country-code') || 'br'
+export const DEFAULT_CURRENCY = getMeta('ecom-currency') || 'BRL'
+export const DEFAULT_CURRENCY_SYMBOL = getMeta('ecom-currency-symbol') || 'R$'
+export const STORE_ID = parseInt(process.env.ECOM_STORE_ID || getMeta('ecom-store-id'), 10)
+export const STORE_OBJECT_ID = process.env.ECOM_STORE_OBJECT_ID || getMeta('ecom-store-object-id')
 
 console.log('--> E-Com Plus Store ID ' + STORE_ID)
