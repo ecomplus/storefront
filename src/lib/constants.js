795413bae4d79c651d854d44e834206a69c9a7cf
Leonardo Matos <leomp120894@gmail.com>
2019-05-22 20:32:33 -0300

    feat: try to get store ids from env vars

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,5 +2,5 @@ export const DEFAULT_LANG = 'pt_br'
 export const DEFAULT_COUNTRY_CODE = 'br'
 export const DEFAULT_CURRENCY = 'BRL'
 export const DEFAULT_CURRENCY_SYMBOL = 'R$'
-export const STORE_ID = null
-export const STORE_OBJECT_ID = null
+export const STORE_ID = process.env.ECOM_STORE_ID
+export const STORE_OBJECT_ID = process.env.ECOM_STORE_OBJECT_ID
