b4cebc452c3fedf2f4fbfeab57f78a56c675f41b
Leonardo Matos <leomp120894@gmail.com>
2019-08-09 04:48:43 -0300

    fix(main): fix exported object

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@
 const paths = require('./lib/paths')
 const config = require('./lib/config')
 const getStoreData = require('./lib/get-store-data')
-const getCmsData = require('./lib/get-cms-data')
 const webpackConfig = require('./webpack.config')
 const renderer = require('./renderer')
 
 module.exports = {
   paths,
   config,
-  getCmsData,
   getStoreData,
   webpackConfig,
   renderer
