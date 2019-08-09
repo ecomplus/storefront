2cfd589d9962112b851c58b96067fcea5d14d0b1
Leonardo Matos <leomp120894@gmail.com>
2019-08-09 04:47:20 -0300

    chore(paths): unifying pages path

--- src/lib/paths.js
+++ src/lib/paths.js
@@ -7,9 +7,7 @@ const path = require('path')
 const src = path.resolve(process.cwd(), 'template')
 const js = path.resolve(src, 'js')
 const scss = path.resolve(src, 'scss')
-const views = path.resolve(src, 'views')
-const partials = path.resolve(views, 'partials')
-const pages = path.resolve(views, 'pages')
+const pages = path.resolve(src, 'pages')
 const pub = path.resolve(src, 'public')
 const img = path.resolve(pub, 'img')
 const content = path.resolve(process.cwd(), 'content')
@@ -22,8 +20,6 @@ module.exports = {
   src,
   js,
   scss,
-  views,
-  partials,
   pages,
   pub,
   img,
