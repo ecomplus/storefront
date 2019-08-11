86cc50cb50307194c9ea9bf8cf46715b4bb759e9
Leonardo Matos <leomp120894@gmail.com>
2019-08-11 02:02:14 -0300

    build(webpack): setup webpack config for js and scss build and serve

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const devMode = process.env.NODE_ENV !== 'production'
+const path = require('path')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: devMode ? 'development' : 'production',
+  entry: [
+    path.resolve(__dirname, 'src/index.js'),
+    path.resolve(__dirname, 'scss/storefront-twbs.scss')
+  ],
+  output: {
+    library: '__storefront_twbs',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'storefront-twbs.js'
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'test'),
+    port: 3376,
+    open: true
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'storefront-twbs.css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              minimize: !devMode,
+              plugins: [
+                require('autoprefixer')(),
+                require('cssnano')({ preset: 'default' })
+              ]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  externals: {
+    vue: {
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      root: 'Vue'
+    }
+  }
+}
