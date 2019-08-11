24409446c92640f180b407ed6130ce388ea3ece3
Leonardo Matos <leomp120894@gmail.com>
2019-08-11 19:35:31 -0300

    chore(babel): removing babel config (not using babel)

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+
+import {
+  AlertPlugin,
+  CollapsePlugin,
+  DropdownPlugin,
+  FormPlugin,
+  FormCheckboxPlugin,
+  FormFilePlugin,
+  FormGroupPlugin,
+  FormInputPlugin,
+  FormRadioPlugin,
+  FormSelectPlugin,
+  FormTextareaPlugin,
+  PaginationPlugin,
+  ModalPlugin,
+  TabsPlugin,
+  ToastPlugin,
+  VBPopoverPlugin,
+  VBTooltipPlugin
+} from 'bootstrap-vue'
+
+Vue.use(AlertPlugin)
+Vue.use(CollapsePlugin)
+Vue.use(DropdownPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormCheckboxPlugin)
+Vue.use(FormFilePlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(FormInputPlugin)
+Vue.use(FormRadioPlugin)
+Vue.use(FormSelectPlugin)
+Vue.use(FormTextareaPlugin)
+Vue.use(PaginationPlugin)
+Vue.use(ModalPlugin)
+Vue.use(TabsPlugin)
+Vue.use(ToastPlugin)
+Vue.use(VBPopoverPlugin)
+Vue.use(VBTooltipPlugin)
+
+export {
+  Vue
+}
