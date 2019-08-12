6df7ce5214ffa1a927cd7edec87a3e0adfde7853
Leonardo Matos <leomp120894@gmail.com>
2019-08-12 11:49:33 -0300

    perf(bootstrap-vue): removing form components by default
    BREAKING CHANGE: form components plugin no more available

--- src/index.js
+++ src/index.js
@@ -7,14 +7,6 @@ import {
   AlertPlugin,
   CollapsePlugin,
   DropdownPlugin,
-  FormPlugin,
-  FormCheckboxPlugin,
-  FormFilePlugin,
-  FormGroupPlugin,
-  FormInputPlugin,
-  FormRadioPlugin,
-  FormSelectPlugin,
-  FormTextareaPlugin,
   PaginationPlugin,
   ModalPlugin,
   TabsPlugin,
@@ -26,14 +18,6 @@ import {
 Vue.use(AlertPlugin)
 Vue.use(CollapsePlugin)
 Vue.use(DropdownPlugin)
-Vue.use(FormPlugin)
-Vue.use(FormCheckboxPlugin)
-Vue.use(FormFilePlugin)
-Vue.use(FormGroupPlugin)
-Vue.use(FormInputPlugin)
-Vue.use(FormRadioPlugin)
-Vue.use(FormSelectPlugin)
-Vue.use(FormTextareaPlugin)
 Vue.use(PaginationPlugin)
 Vue.use(ModalPlugin)
 Vue.use(TabsPlugin)
