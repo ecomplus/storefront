6029de81448c71457e08b23169d0838df838e4f9
Leonardo Matos <leomp120894@gmail.com>
2019-08-13 20:42:07 -0300

    build(webpack): fix webpack config for lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 'use strict'
 
 process.env.WEBPACK_BUILD_LIB = true
+const path = require('path')
 const config = require('@ecomplus/storefront-framework/src/webpack.config')
 
 module.exports = [
   config,
   {
     ...config,
+    entry: path.resolve(__dirname, 'template/js/index.js'),
     output: {
       ...config.output,
+      path: path.resolve(config.output.path, 'lib'),
       library: '__storefront_template',
       libraryTarget: 'umd',
-      filename: 'storefront.lib.js'
+      filename: 'storefront-template.js'
     },
     externals: /^[^./].*$/
   }
