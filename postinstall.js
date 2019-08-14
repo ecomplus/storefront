a606027b51ae46e0e3d5288907ce9445f9ec37a7
Leonardo Matos <leomp120894@gmail.com>
2019-08-14 10:03:55 -0300

    fix(postinstall): copy to template/public/assets/storefront-template

--- postinstall.js
+++ postinstall.js
@@ -6,10 +6,13 @@ const path = require('path')
 const fs = require('fs')
 
 const dir = path.join(__dirname, 'dist/lib')
-const copyTo = path.join(process.env.INIT_CWD, 'template/public/storefront-template')
-if (!fs.existsSync(copyTo)) {
-  fs.mkdirSync(copyTo)
-}
+let copyTo = path.join(process.env.INIT_CWD, 'template/public')
+;['assets', 'storefront-template'].forEach(folder => {
+  copyTo = path.join(copyTo, folder)
+  if (!fs.existsSync(copyTo)) {
+    fs.mkdirSync(copyTo)
+  }
+})
 
 fs.readdir(dir, (err, files) => {
   if (!err) {
