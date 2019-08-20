c2953ec7ece7a1e3d33189f6d316879caa40a6d1
Leonardo Matos <leomp120894@gmail.com>
2019-08-20 19:58:30 -0300

    fix(postinstall): check if template/public is a directory first

--- postinstall.js
+++ postinstall.js
@@ -6,34 +6,38 @@ const path = require('path')
 const fs = require('fs')
 const dirBase = process.env.INIT_CWD
 const dirDest = path.join(dirBase, 'template/public')
-let pathsTo, dirFrom
 
-const copyFolder = () => {
-  let dirTo = dirDest
-  pathsTo.forEach(folder => {
-    dirTo = path.join(dirTo, folder)
-    if (!fs.existsSync(dirTo)) {
-      fs.mkdirSync(dirTo)
-    }
-  })
+if (fs.statSync(dirDest).isDirectory()) {
+  let pathsTo, dirFrom
 
-  const files = fs.readdirSync(dirFrom)
-  files.forEach(file => {
-    const filepath = path.join(dirFrom, file)
-    if (!fs.statSync(filepath).isDirectory()) {
-      fs.copyFileSync(filepath, path.join(dirTo, file))
-    }
-  })
-  console.log(`[OK] files copied to ${dirTo.slice(dirBase.length)} folder`)
+  const copyFolder = () => {
+    let dirTo = dirDest
+    pathsTo.forEach(folder => {
+      dirTo = path.join(dirTo, folder)
+      if (!fs.existsSync(dirTo)) {
+        fs.mkdirSync(dirTo)
+      }
+    })
+
+    const files = fs.readdirSync(dirFrom)
+    files.forEach(file => {
+      const filepath = path.join(dirFrom, file)
+      if (!fs.statSync(filepath).isDirectory()) {
+        fs.copyFileSync(filepath, path.join(dirTo, file))
+      }
+    })
+    console.log(`[OK] files copied to ${dirTo.slice(dirBase.length)} folder`)
+  }
+
+  dirFrom = path.join(__dirname, 'dist/lib')
+  pathsTo = ['assets', 'storefront-template']
+  copyFolder()
+
+  dirFrom = path.join(__dirname, 'template/public/admin')
+  pathsTo = ['admin', 'cms']
+  copyFolder()
+
+  console.log('[DONE] @ecomplus/storefront-template')
 }
 
-dirFrom = path.join(__dirname, 'dist/lib')
-pathsTo = ['assets', 'storefront-template']
-copyFolder()
-
-dirFrom = path.join(__dirname, 'template/public/admin')
-pathsTo = ['admin', 'cms']
-copyFolder()
-
-console.log('[DONE] @ecomplus/storefront-template')
 console.log()
