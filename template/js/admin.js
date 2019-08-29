3447b71b1adbf27ddc91469f7aaef19bd3d3db1c
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2019-08-29 18:08:31 -0300

    feat(cms): adding collection select widget

--- template/js/admin.js
+++ template/js/admin.js
@@ -1,7 +1,9 @@
 import './lib/config'
 import * as ColorWidget from 'netlify-cms-widget-color'
+import youtube from "netlify-cms-widget-ecomplus-collections";
 /* global CMS */
 
 document.title = `Admin ~ ${document.title}`
 
 CMS.registerWidget('color', ColorWidget.Control)
+CMS.registerWidget("collections", collectionsControl, collectionsPreview);
