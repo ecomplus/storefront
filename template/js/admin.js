dcc06fd427da2032d6f3cdebf70fc75798db1ebc
Leonardo Matos <leomp120894@gmail.com>
2019-08-30 19:05:50 -0300

    fix(cms): importing cms core and widgets properly from cdn

--- template/js/admin.js
+++ template/js/admin.js
@@ -1,7 +1,3 @@
 import './lib/config'
-import * as ColorWidget from 'netlify-cms-widget-color'
-/* global CMS */
 
 document.title = `Admin ~ ${document.title}`
-
-CMS.registerWidget('color', ColorWidget.Control)
