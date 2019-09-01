421d9063919d6f01176a15d9c8b5f677b6b16801
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2019-09-01 09:35:58 -0300

    chore(template): solving conflicts to merje

--- template/js/admin.js
+++ template/js/admin.js
@@ -1,9 +1,3 @@
 import './lib/config'
-import * as ColorWidget from 'netlify-cms-widget-color'
-import { CollectionsControl } from 'netlify-cms-widget-ecomplus-collections'
-/* global CMS */
 
 document.title = `Admin ~ ${document.title}`
-
-CMS.registerWidget('color', ColorWidget.Control)
-CMS.registerWidget('collections', CollectionsControl)
