87f21231228799cf416f4126db6b061ea49be921
Leonardo Matos <leomp120894@gmail.com>
2019-09-03 02:17:27 -0300

    style(js): remove unecessary comment

--- src/lib/dictionary.js
+++ src/lib/dictionary.js
@@ -59,7 +59,6 @@ const dictionary = {
 
 export default function (word, lang) {
   if (!lang) {
-    // use english if lang undefined
     lang = (this && this.lang) || 'en_us'
   }
   return (dictionary[word] && dictionary[word][lang]) || ''
