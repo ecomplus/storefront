a2012f31ad80f02f4dc69242da05bf725e475d49
Leonardo Matos <leomp120894@gmail.com>
2019-09-05 13:48:53 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import '@ecomplus/storefront-twbs'
+import MyComponent from './components/MyComponent.vue'
+
+export default options => {
+  const elId = 'some-el-id'
+  new Vue({
+    components: {
+      MyComponent
+    },
+    template: `
+    <my-component id="${elId}"/>`
+  }).$mount(`#${elId}`)
+}
