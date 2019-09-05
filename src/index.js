80f7323aebd6cd5f665940c2b774f6e18a133a94
Leonardo Matos <leomp120894@gmail.com>
2019-09-05 12:56:57 -0300

    fix(widget): check element before starting

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import '@ecomplus/storefront-twbs'
 import EcUser from './components/EcUser.vue'
 
 export default options => {
-  const elId = 'user-button'
-  new Vue({
-    components: {
-      EcUser
-    },
-    template: `
-    <ec-user id="${elId}"/>`
-  }).$mount(`#${elId}`)
+  const elId = (options && options.elId) || 'user-button'
+  const $userButton = document.getElementById(elId)
+
+  if ($userButton) {
+    new Vue({
+      components: {
+        EcUser
+      },
+      template: `
+      <ec-user id="${elId}"/>`
+    }).$mount($userButton)
+  }
 }
