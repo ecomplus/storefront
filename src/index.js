d2413f45157cbfbe8f63df2ce02df059633dfabe
Leonardo Matos <leomp120894@gmail.com>
2019-09-05 17:39:20 -0300

    feat(widget): setup widget function, use lozad to load cards on demand

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import Vue from 'vue'
+import lozad from 'lozad'
 import '@ecomplus/storefront-twbs'
-import MyComponent from './components/EcProductCard.vue'
+import EcProductCard from './components/EcProductCard.vue'
+import EcomCart from '@ecomplus/shopping-cart'
 
 export default options => {
-  const elId = 'some-el-id'
-  new Vue({
-    components: {
-      MyComponent
-    },
-    template: `
-    <my-component id="${elId}"/>`
-  }).$mount(`#${elId}`)
+  const elClass = (options && options.elClass) || 'product-card'
+  const cart = new EcomCart()
+
+  const load = $productCard => {
+    if (!$productCard.classList.contains('ec-product-card')) {
+      const { productId, sku } = $productCard.dataset
+
+      new Vue({
+        components: {
+          EcProductCard
+        },
+
+        data () {
+          return {
+            productId,
+            sku
+          }
+        },
+
+        methods: {
+          addToCart ({ product }) {
+            cart.addProduct(product)
+          }
+        },
+
+        template: `
+        <ec-product-card
+          class="${elClass}"
+          :data-product-id="productId"
+          :data-sku="sku"
+          :productId="productId"
+          @buy="addToCart"
+        >
+          ${$productCard.outerHTML}
+        </ec-product-card>`
+      }).$mount($productCard)
+    }
+  }
+
+  const observer = lozad(`.${elClass}`, { load })
+  observer.observe()
 }
