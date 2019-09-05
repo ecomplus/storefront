e8726e00869d9407b0d03a3a235654ee90bdcd8b
Leonardo Matos <leomp120894@gmail.com>
2019-09-05 20:26:46 -0300

    feat(product-card): vue component for storefront product card

--- src/components/js/EcProductCard.js
+++ src/components/js/EcProductCard.js
@@ -1,3 +1,88 @@
+import { _config, name, inStock } from '@ecomplus/utils'
+import EcomSearch from '@ecomplus/search-engine'
+import dictionary from '@ecomplus/widget-product/src/lib/dictionary'
+import EcImage from '@ecomplus/widget-product/src/components/EcImage.vue'
+import EcPrices from '@ecomplus/widget-product/src/components/EcPrices.vue'
+import { FadeTransition } from 'vue2-transitions'
+
 export default {
-  name: 'MyComponent'
+  name: 'EcProductCard',
+
+  components: {
+    EcImage,
+    EcPrices,
+    FadeTransition
+  },
+
+  props: {
+    lang: {
+      type: String,
+      default: _config.get('lang')
+    },
+    storeId: {
+      type: Number,
+      default: _config.get('store_id')
+    },
+    productId: {
+      type: String
+    },
+    item: {
+      type: Object
+    },
+    buyText: {
+      type: String
+    }
+  },
+
+  data () {
+    return {
+      body: {}
+    }
+  },
+
+  computed: {
+    strBuy () {
+      return this.buyText || this.dictionary('buy')
+    }
+  },
+
+  methods: {
+    dictionary,
+    name,
+    inStock,
+
+    fetchItem () {
+      if (this.productId) {
+        const { storeId } = this
+        const search = new EcomSearch({ storeId })
+        search.setProductIds([this.productId]).fetch()
+          .then(() => {
+            const items = search.getItems()
+            if (Array.isArray(items) && items.length) {
+              this.body = items[0]
+            }
+          })
+          .catch(err => {
+            console.error(err)
+            const errorMsg = this.lang === 'pt_br'
+              ? 'Não foi possível carregar informações do produto, por favor verifique sua conexão'
+              : 'Unable to load product information, please check your internet connection'
+            this.$bvToast.toast(errorMsg, {
+              title: 'Offline',
+              variant: 'danger',
+              noAutoHide: true,
+              solid: true
+            })
+          })
+      }
+    }
+  },
+
+  created () {
+    if (this.item) {
+      this.body = this.item
+    } else {
+      this.fetchItem()
+    }
+  }
 }
