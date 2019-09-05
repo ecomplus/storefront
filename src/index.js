f81f7a5a7c2378a98cfe1300fc17059e866fc703
Leonardo Matos <leomp120894@gmail.com>
2019-09-05 12:51:17 -0300

    fix(widget): check element before starting

--- src/index.js
+++ src/index.js
@@ -4,27 +4,29 @@ import EcProduct from './components/EcProduct.vue'
 import EcomCart from '@ecomplus/shopping-cart'
 
 export default options => {
-  const elId = 'product-block'
+  const elId = (options && options.elId) || 'product-block'
   const $productBlock = document.getElementById(elId)
   const cart = new EcomCart()
 
-  new Vue({
-    components: {
-      EcProduct
-    },
+  if ($productBlock) {
+    new Vue({
+      components: {
+        EcProduct
+      },
 
-    methods: {
-      addToCart ({ product }) {
-        cart.addProduct(product)
-      }
-    },
+      methods: {
+        addToCart ({ product }) {
+          cart.addProduct(product)
+        }
+      },
 
-    template: `
-    <ec-product
-      id="${elId}"
-      @buy="addToCart"
-    >
-      ${$productBlock.outerHTML}
-    </ec-product>`
-  }).$mount($productBlock)
+      template: `
+      <ec-product
+        id="${elId}"
+        @buy="addToCart"
+      >
+        ${$productBlock.outerHTML}
+      </ec-product>`
+    }).$mount($productBlock)
+  }
 }
