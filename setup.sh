99e4551eac5419620c88ac4c1d328292c150f241
Leonardo Matos <leomp120894@gmail.com>
2019-09-06 18:58:17 -0300

    build(setup): fix setup.sh script (mkdir)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # !/bin/bash
 
+mkdir -p ./scss/bootstrap/scss;
 cp -r ./node_modules/bootstrap/scss/* ./scss/bootstrap/scss/;
+mkdir -p ./scss/bootstrap-vue/src;
 cp -r ./node_modules/bootstrap-vue/src/* ./scss/bootstrap-vue/src/;
 find ./scss/ -type f ! -name "*.scss" -delete;
 rm -rf ./dist;
