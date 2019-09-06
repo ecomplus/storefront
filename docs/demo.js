e5717baf157a194ad578cd6e2f62b62e918e1dfe
Leonardo Matos <leomp120894@gmail.com>
2019-09-06 12:18:49 -0300

    test(demo): testing props and slots on widget options

--- docs/demo.js
+++ docs/demo.js
@@ -1,6 +1,15 @@
 import widget from './../src/'
 
 window._widgets = false
-widget()
+widget({
+  /*
+  props: {
+    buyText: 'BUY'
+  },
+  */
+  slots: {
+    buy: '<img id="logo" class="header__logo" src="/img/uploads/logo.png" alt="My Shop">'
+  }
+})
 
 export default widget
