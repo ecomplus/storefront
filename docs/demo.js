4c82fcf5dc77e84fcf8f5afce84caa44b956fc60
Leonardo Matos <leomp120894@gmail.com>
2019-09-12 23:31:27 -0300

    test(demo): testing ecomcart event

--- docs/demo.js
+++ docs/demo.js
@@ -1,6 +1,11 @@
 import widget from './../src/'
+import EcomCart from '@ecomplus/shopping-cart'
 
 window._widgets = false
 widget()
+
+EcomCart.on('increaseItemQnt', () => {
+  window.alert('demo')
+})
 
 export default widget
