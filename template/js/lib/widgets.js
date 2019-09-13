865e721dd74046890b92bce08da682f6679f14c4
Leonardo Matos <leomp120894@gmail.com>
2019-09-12 23:29:10 -0300

    feat(widgets): import and setup ecomplus/widget-minicart

--- template/js/lib/widgets.js
+++ template/js/lib/widgets.js
@@ -13,6 +13,7 @@ const loadWidget = (pkg, runImport) => {
 loadWidget('@ecomplus/widget-user', () => import('@ecomplus/widget-user'))
 loadWidget('@ecomplus/widget-product-card', () => import('@ecomplus/widget-product-card'))
 loadWidget('@ecomplus/widget-search', () => import('@ecomplus/widget-search'))
+loadWidget('@ecomplus/widget-minicart', () => import('@ecomplus/widget-minicart'))
 
 const { resource } = document.body.dataset
 switch (resource) {
