b4e1ea4980695ec65c4d2641aff8b3dc690cfcd1
Leonardo Matos <leomp120894@gmail.com>
2019-09-19 14:48:59 -0300

    build(vue): fix vue cli config for build process

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
-module.exports = {
+const config = {
   lintOnSave: false
 }
+
+if (process.argv.indexOf('build') > -1) {
+  config.css = {
+    extract: false
+  }
+  config.chainWebpack = config => {
+    config.externals(require('@ecomplus/storefront-template/webpack.externals'))
+  }
+}
+
+module.exports = config
