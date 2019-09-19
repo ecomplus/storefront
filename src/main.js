8fe8f0533b22258b09fdd8e06a3fc5d54cb20fa3
Leonardo Matos <leomp120894@gmail.com>
2019-09-19 20:40:30 -0300

    chore(src): remove unused public folder, minor startup fixes

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#storefront-app')
