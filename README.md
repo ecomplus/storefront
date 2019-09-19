e134773d9bc3bc99d5a53bf9349b56fd5463b823
Leonardo Matos <leomp120894@gmail.com>
2019-09-19 13:34:38 -0300

    chore(v1): setup pkg for v1

--- README.md
+++ README.md
@@ -6,73 +6,20 @@
 
 > Vue.js ecommerce app with cart, checkout and account pages
 
-## Deploy
-
-Demo with [Netlify](https://www.netlify.com/) published
-[here](https://storefront-app.e-com.plus/).
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/ecomclub/storefront-app)
-
-## Using as library
-
-If you don't want to customize,
-you can just use the default _Storefront-App_ built files:
-
-- Styles:
-  1. `https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/app.min.css`;
-- Scripts:
-  1. `https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/manifest.min.js`;
-  2. `https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/vendor.min.js`;
-  3. `https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/app.min.js`;
-
-Importing it into your HTML view as following:
-
-```ejs
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <meta name="ecom-store-id" content="<%= store_id %>">
-    <meta name="ecom-store-object-id" content="<%= store_object_id %>">
-    <title></title>
-    <link href="https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/app.min.css" rel="stylesheet">
-  </head>
-  <body style="opacity: 0">
-    <div id="app"></div>
-    <div id="credits">
-      Powered by
-      <a href="https://e-com.plus" target="_blank">E-Com Plus</a>
-    </div>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/manifest.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/vendor.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@ecomplus/storefront-app/lib/app.min.js"></script>
-  </body>
-</html>
+## Project setup
 ```
-
-Meta tags `ecom-store-id` and `ecom-store-object-id`
-are not required if you're using a domain associated to your E-Com Plus store.
-
-## Build setup
-
-``` bash
-# install dependencies
 npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production as library
-npm run build:lib
-
-# build for production and view the bundle analyzer report
-npm run build --report
 ```
 
-For a detailed explanation on how things work,
-check out the [guide](http://vuejs-templates.github.io/webpack/)
-and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,103 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// Element UI kit
-// http://element.eleme.io
-import ElementUI from 'element-ui'
+Vue.config.productionTip = false
 
-// internationalization
-import VueI18n from 'vue-i18n'
-import enUs from 'element-ui/lib/locale/lang/en'
-import ptBr from 'element-ui/lib/locale/lang/pt-br'
-// custom dictionary
-import dictionary from './lib/dictionary'
-import { DEFAULT_LANG, DEFAULT_COUNTRY_CODE, DEFAULT_CURRENCY } from '@/lib/constants'
-
-// custom additional plugins
-import Inputmask from 'inputmask'
-import VueSticky from 'vue-sticky'
-import VueClipboard from 'vue-clipboard2'
-import creditCardType from 'credit-card-type'
-
-// Font Awesome Icons
-import './lib/icons'
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-Vue.component('a-icon', FontAwesomeIcon)
-
-// setup Element UI fully
-Vue.use(ElementUI)
-// setup i18n
-Vue.use(VueI18n)
-Vue.locale('en_us', { ...enUs, ...dictionary.enUs })
-Vue.locale('pt_br', { ...ptBr, ...dictionary.ptBr })
-let locale = (lang, country, currency) => {
-  // handle internationalization
-  if (lang) {
-    Vue.config.lang = Vue.prototype.$lang = lang
-  }
-  if (country) {
-    Vue.prototype.$country = country
-  }
-  if (currency) {
-    Vue.prototype.$currency = currency
-  }
-}
-Vue.prototype.$locale = locale
-// preset default language
-locale(DEFAULT_LANG, DEFAULT_COUNTRY_CODE, DEFAULT_CURRENCY)
-
-// handle directives for plugins
-Vue.directive('mask', {
-  bind (el, { value }) {
-    Inputmask(value).mask(el.getElementsByTagName('INPUT')[0])
-  }
-})
-Vue.directive('sticky', VueSticky)
-// handle keyup for children input element
-Vue.directive('on-keyup', {
-  bind (el, { value }) {
-    el.getElementsByTagName('INPUT')[0].onkeyup = value
-  }
-})
-Vue.directive('on-key-enter', {
-  bind (el, { value }) {
-    el.getElementsByTagName('INPUT')[0].onkeyup = e => {
-      if ((e.which || e.keyCode) === 13) {
-        value(e)
-      }
-    }
-  }
-})
-// setup copy to clipboard tool
-VueClipboard.config.autoSetContainer = true
-Vue.use(VueClipboard)
-
-/* eslint-disable no-new */
-// set Vue instance
 new Vue({
-  el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
-})
-
-// handle credit card types
-// https://github.com/braintree/credit-card-type
-creditCardType.addCard({
-  niceType: 'Hipercard',
-  type: 'hipercard',
-  patterns: [
-    606282
-  ],
-  gaps: [4, 8, 12],
-  lengths: [16],
-  code: {
-    name: 'CVV',
-    size: 3
-  }
-})
+  render: h => h(App)
+}).$mount('#app')
