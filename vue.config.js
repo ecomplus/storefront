63ac51d9eb1d100bff33c745cc30cf371cc3bb21
Leonardo Matos <leomp120894@gmail.com>
2019-09-19 20:37:49 -0300

    build(vue): custom vue cli config for serve and build:app (template)

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,32 @@
+const path = require('path')
+const templatePath = path.join(process.cwd(), 'node_modules/@ecomplus/storefront-template/dist')
+
 const config = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    port: 9130,
+    contentBase: templatePath
+  }
 }
 
-if (process.argv.indexOf('build') > -1) {
+if (process.argv.indexOf('--target') === process.argv.indexOf('lib') - 1) {
+  config.chainWebpack = config => {
+    config.externals(require('@ecomplus/storefront-template/webpack.externals'))
+  }
   config.css = {
     extract: false
   }
+} else {
   config.chainWebpack = config => {
-    config.externals(require('@ecomplus/storefront-template/webpack.externals'))
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = path.join(templatePath, 'app/index.html')
+        return args
+      })
+  }
+  if (process.env.NODE_ENV === 'production') {
+    config.outputDir = 'dist/app'
   }
 }
 
