76f7ca357926841805a3557c0421922b49f2b2d5
Leonardo Matos <leomp120894@gmail.com>
2019-09-19 21:58:19 -0300

    build(vue): comments and minor edits on vue cli config

--- vue.config.js
+++ vue.config.js
@@ -10,23 +10,29 @@ const config = {
 }
 
 if (process.argv.indexOf('--target') === process.argv.indexOf('lib') - 1) {
+  // production Vue CLI lib output
   config.chainWebpack = config => {
+    // exclude all libs imported by storefront-template
     config.externals(require('@ecomplus/storefront-template/webpack.externals'))
   }
+
   config.css = {
     extract: false
   }
 } else {
+  // build/serve SPA
+  if (process.env.NODE_ENV === 'production') {
+    config.outputDir = 'dist/app'
+  }
+
   config.chainWebpack = config => {
     config
+      // HTML file from template to load global vars and styles
       .plugin('html')
       .tap(args => {
         args[0].template = path.join(templatePath, 'app/test.html')
         return args
       })
-  }
-  if (process.env.NODE_ENV === 'production') {
-    config.outputDir = 'dist/app'
   }
 }
 
