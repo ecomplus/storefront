366037c1bb3f4fc39a549ab70cca9155123a6503
Leonardo Matos <leomp120894@gmail.com>
2019-09-20 17:10:53 -0300

    feat(checkout-widgets): check and setup widgets on /app/ routes

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,3 @@
 import './lib/config'
-// import '@ecomplus/storefront-app'
+
+import(/* webpackPrefetch: true */ './lib/widgets')
--- template/js/lib/widgets.js
+++ template/js/lib/widgets.js
@@ -1,8 +1,15 @@
+const isCheckout = window.location.pathname.startsWith('/app/')
+const isMobile = window.screen.width < 768
+
 const loadWidget = (pkg, runImport) => {
   const widget = window._widgets[pkg]
   if (widget) {
-    const { active, desktopOnly, options } = widget
-    if (active && (!desktopOnly || window.screen.width >= 768)) {
+    const { active, options, desktopOnly, enableCheckout, disablePages } = widget
+    if (
+      active &&
+      (!desktopOnly || !isMobile) &&
+      (isCheckout ? enableCheckout : !disablePages)
+    ) {
       runImport().then(exp => {
         if (typeof exp.default === 'function') {
           exp.default(options)
@@ -13,14 +20,16 @@ const loadWidget = (pkg, runImport) => {
   }
 }
 
-loadWidget('@ecomplus/widget-user', () => import('@ecomplus/widget-user'))
-loadWidget('@ecomplus/widget-product-card', () => import('@ecomplus/widget-product-card'))
-loadWidget('@ecomplus/widget-search', () => import('@ecomplus/widget-search'))
-loadWidget('@ecomplus/widget-minicart', () => import('@ecomplus/widget-minicart'))
+if (!isCheckout) {
+  loadWidget('@ecomplus/widget-user', () => import('@ecomplus/widget-user'))
+  loadWidget('@ecomplus/widget-product-card', () => import('@ecomplus/widget-product-card'))
+  loadWidget('@ecomplus/widget-search', () => import('@ecomplus/widget-search'))
+  loadWidget('@ecomplus/widget-minicart', () => import('@ecomplus/widget-minicart'))
 
-const { resource } = document.body.dataset
-if (resource && resource.startsWith('product')) {
-  loadWidget('@ecomplus/widget-product', () => import('@ecomplus/widget-product'))
-} else if (document.getElementById('search')) {
-  loadWidget('@ecomplus/widget-search-engine', () => import('@ecomplus/widget-search-engine'))
+  const { resource } = document.body.dataset
+  if (resource && resource.startsWith('product')) {
+    loadWidget('@ecomplus/widget-product', () => import('@ecomplus/widget-product'))
+  } else if (document.getElementById('search')) {
+    loadWidget('@ecomplus/widget-search-engine', () => import('@ecomplus/widget-search-engine'))
+  }
 }
