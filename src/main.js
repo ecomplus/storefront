c0c81f062e0907242bf848ba2bb59b0011e5f3db
Leonardo Matos <leomp120894@gmail.com>
2019-09-20 19:09:04 -0300

    refactor(src): autostart, not a function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-import storefrontApp from './'
+import { name, version } from './../package.json'
+import Vue from 'vue'
+import '@ecomplus/storefront-twbs'
+import { _config } from '@ecomplus/utils'
+import App from './App.vue'
+import router from './router/'
+import store from './store/'
 
-storefrontApp()
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#storefront-app')
+
+console.log(`Starting ${name}@${version} with Store ID #${_config.get('store_id')}`)
