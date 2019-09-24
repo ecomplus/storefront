5c38e6ffed9948509f514d23a3cddbd58de8ccf0
Leonardo Matos <leomp120894@gmail.com>
2019-09-24 19:18:13 -0300

    test: importing fetch info, icons and utils

--- src/test.js
+++ src/test.js
@@ -1,4 +1,9 @@
 import '@ecomplus/storefront-template/template/js/lib/config'
+import '@ecomplus/storefront-template/template/js/lib/fetch-info'
+
 import './main'
+
+import '@ecomplus/storefront-template/template/js/lib/icons'
+import '@ecomplus/storefront-template/template/js/lib/utils'
 
 console.info('Storefront App Dev Server')
