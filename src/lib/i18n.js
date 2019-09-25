f1307b0256eafb9b992c3639c2eec7dadb966089
Leonardo Matos <leomp120894@gmail.com>
2019-09-25 03:53:26 -0300

    feat(i18n): setup dictionary

---
+++ src/lib/i18n.js
@@ -0,0 +1,59 @@
+export const Continue = {
+  en_us: 'Continue',
+  pt_br: 'Continuar'
+}
+
+export const EnterYourEmail = {
+  en_us: 'Enter your email to proceed',
+  pt_br: 'Informe seu email para prosseguir'
+}
+
+export const IdentifyAccount = {
+  en_us: 'Identify your account',
+  pt_br: 'Identificar ou criar sua conta'
+}
+
+export const LoginError = {
+  en_us: 'Login error',
+  pt_br: 'Erro no login'
+}
+
+export const ManageYourHistory = {
+  en_us: 'Manage your purchase history',
+  pt_br: 'Gerenciar seu histórico de compras'
+}
+
+export const NotifyAboutOrders = {
+  en_us: 'Notify about progress of the order',
+  pt_br: 'Notificar sobre o andamento do pedido'
+}
+
+export const OauthOnPopup = {
+  en_us: 'Continue login on popup',
+  pt_br: 'Continue o login no pop-up'
+}
+
+export const OrProceedWith = {
+  en_us: 'Or proceed with',
+  pt_br: 'Ou prossiga com'
+}
+
+export const ProfileNotFound = {
+  en_us: 'No profile found with email address',
+  pt_br: 'Nenhum perfil encontrado com o endereço de e-mail'
+}
+
+export const SignInWith = {
+  en_us: 'Sign in with',
+  pt_br: 'Entrar com'
+}
+
+export const WasAnErrorOnLogin = {
+  en_us: 'There was an error trying to login, please try again or select another option',
+  pt_br: 'Houve um erro ao tentar o login, por favor tente novamente ou selecione outra opção'
+}
+
+export const WeUseYourDataTo = {
+  en_us: 'We will use your data safely and responsibly to',
+  pt_br: 'Usaremos seus dados com segurança e responsabilidade para'
+}
