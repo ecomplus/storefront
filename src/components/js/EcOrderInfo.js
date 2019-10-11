a753cfb94de66a436fddbf94ed7f0ded343151ba
Leonardo Matos <leomp120894@gmail.com>
2019-10-11 13:31:13 -0300

    feat(confirmation): handle new order confirmation with EcOrderInfo

--- src/components/js/EcOrderInfo.js
+++ src/components/js/EcOrderInfo.js
@@ -1,8 +1,27 @@
 import { i18n } from '@ecomplus/utils'
 import { store } from '@ecomplus/client'
+import { SlideYUpTransition } from 'vue2-transitions'
+
+import {
+  CodeCopied,
+  CopyCode,
+  CopyErrorMsg,
+  DoPaymentMsg,
+  OrderConfirmationMsg,
+  OrderNumber,
+  PrintBillet,
+  RedirectToPayment,
+  TicketCode,
+  _FinancialStatus,
+  _OrderStatus
+} from './../../lib/i18n'
 
 export default {
   name: 'EcOrderInfo',
+
+  components: {
+    SlideYUpTransition
+  },
 
   props: {
     mergeDictionary: {
@@ -12,35 +31,107 @@ export default {
     order: {
       type: Object,
       required: true
+    },
+    isNew: {
+      type: Boolean
+    },
+    skipDataLoad: {
+      type: Boolean
     }
   },
 
   data () {
     return {
-      updateInterval: null
+      loaded: this.skipDataLoad,
+      updateInterval: null,
+      orderBody: this.order
     }
   },
 
   computed: {
     dictionary () {
       return {
+        CodeCopied,
+        CopyCode,
+        CopyErrorMsg,
+        DoPaymentMsg,
+        OrderConfirmationMsg,
+        OrderNumber,
+        PrintBillet,
+        RedirectToPayment,
+        TicketCode,
+        _FinancialStatus,
+        _OrderStatus,
         ...this.mergeDictionary
       }
+    },
+
+    localOrder: {
+      get () {
+        return {
+          ...this.order,
+          ...this.orderBody
+        }
+      },
+      set (body) {
+        this.orderBody = body
+        this.$emit('update:order', body)
+      }
+    },
+
+    financialStatus () {
+      return ''
+    },
+
+    transaction () {
+      const { transactions } = this.localOrder
+      return transactions && transactions.length
+        ? transactions[0]
+        : {}
     }
   },
 
   methods: {
-    i18n (label) {
-      return i18n(this.dictionary[label])
+    i18n (label, prop) {
+      return i18n(prop ? this.dictionary[label][prop] : this.dictionary[label])
+    },
+
+    toClipboard (text) {
+      this.$copyText(text).then(() => {
+        this.$bvToast.toast(text, {
+          title: this.i18n('CodeCopied'),
+          variant: 'success',
+          solid: true,
+          autoHideDelay: 1500
+        })
+      }, err => {
+        console.error(err)
+        this.$bvToast.toast('Oops', {
+          title: `${this.i18n('copyErrorMsg')}: ${text}`,
+          variant: 'warning',
+          solid: true
+        })
+      })
     }
   },
 
   created () {
-    const update = () => {
-      store({ url: `/orders/${this.order._id}.json` })
+    if (this.order._id && !this.skipDataLoad) {
+      const update = () => {
+        store({ url: `/orders/${this.order._id}.json` })
+          .then(({ data }) => {
+            this.localOrder = data
+          })
+          .catch(err => {
+            console.error(err)
+          })
+      }
+      this.updateInterval = setInterval(update, 9000)
+      setTimeout(() => {
+        update()
+        this.loaded = true
+      }, 1000)
     }
-    this.updateInterval = setInterval(update, 9000)
-    setTimeout(update, 3000)
   },
 
   beforeDestroy () {
