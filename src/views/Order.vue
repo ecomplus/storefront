6f4c1427226c4855336c95db65a34981589b97bb
Leonardo Matos <leomp120894@gmail.com>
2019-11-04 18:45:44 -0300

    chore(orders): setup orders view/script

---
+++ src/views/Order.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="account">
+    <ec-account
+      @login="login"
+      @logout="resetCustomer"
+      :customer.sync="customer"
+      :ecomPassport="ecomPassport"
+    />
+  </div>
+</template>
+<script src="./js/Order.js"></script>
