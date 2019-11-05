e29138e89282d19b0afeec7cb43f898642b4676d
Leonardo Matos <leomp120894@gmail.com>
2019-11-05 14:19:39 -0300

    feat(order): functional order component

---
+++ src/components/EcOrder.vue
@@ -0,0 +1,3 @@
+<script src="./js/EcOrder.js"></script>
+<template lang="html" src="./html/EcOrder.html"></template>
+<style lang="scss" src="./scss/EcOrder.scss"></style>
---
+++ src/components/js/EcOrder.js
@@ -0,0 +1,94 @@
+import { store } from '@ecomplus/client'
+import EcomPassport from '@ecomplus/passport-client'
+import EcOrderStep from './../EcOrderStep.vue'
+import EcOrderInfo from './../EcOrderInfo.vue'
+import EcOrderSummary from './../EcOrderSummary.vue'
+
+export default {
+  name: 'EcOrder',
+
+  components: {
+    EcOrderStep,
+    EcOrderInfo,
+    EcOrderSummary
+  },
+
+  props: {
+    order: {
+      type: Object,
+      required: true
+    },
+    skipDataLoad: {
+      type: Boolean
+    },
+    accountOrdersUrl: {
+      type: String,
+      default: '/app/#/account/orders'
+    },
+    ecomPassport: {
+      type: Object,
+      default: () => new EcomPassport()
+    }
+  },
+
+  data () {
+    return {
+      orderBody: {
+        _id: '',
+        ...this.order
+      }
+    }
+  },
+
+  computed: {
+    localOrder: {
+      get () {
+        return this.orderBody
+      },
+      set (body) {
+        this.orderBody = body
+        this.$emit('update:order', body)
+      }
+    }
+  },
+
+  methods: {
+    fetchOrder () {
+      const url = `/orders/${this.orderBody._id}.json`
+      const request = this.ecomPassport.isLogged()
+        ? this.ecomPassport.requestApi(url)
+        : store({ url })
+      request.then(({ data }) => {
+        this.localOrder = data
+      })
+    }
+  },
+
+  watch: {
+    'order._id' (_id) {
+      if (_id && _id !== this.orderBody._id) {
+        this.orderBody = this.order
+      }
+    },
+
+    'orderBody._id' (_id) {
+      if (_id && !this.skipDataLoad) {
+        this.fetchOrder()
+      }
+    }
+  },
+
+  created () {
+    if (!this.skipDataLoad) {
+      if (this.order._id) {
+        this.fetchOrder()
+      } else if (this.order.number) {
+        this.ecomPassport.fetchOrdersList().then(orders => {
+          this.localOrder = orders.find(({ number }) => number === this.order.number) || {}
+        })
+      } else {
+        // TODO: alert invalid order body
+      }
+    }
+  }
+}
