ceab91b7db27ded8e39b254dfba649278ac270e2
Leonardo Matos <leomp120894@gmail.com>
2019-11-13 20:42:28 -0300

    fix(order-info): remove saveCustomerOrder (should be done by checkout ?)

--- src/components/js/EcOrderInfo.js
+++ src/components/js/EcOrderInfo.js
@@ -84,7 +84,6 @@ export default {
       set (body) {
         this.orderBody = body
         this.$emit('update:order', body)
-        this.saveCustomerOrder()
       }
     },
 
@@ -121,64 +120,28 @@ export default {
           solid: true
         })
       })
-    },
-
-    saveCustomerOrder () {
-      const { localOrder, ecomPassport } = this
-      if (localOrder.number && localOrder._id && ecomPassport && ecomPassport.isAuthorized()) {
-        const { orders } = ecomPassport.getCustomer()
-        if (Array.isArray(orders)) {
-          const resumedOrderBody = {}
-          ;[
-            '_id',
-            'created_at',
-            'number',
-            'currency_id',
-            'currency_symbol',
-            'amount',
-            'payment_method_label',
-            'shipping_method_label'
-          ].forEach(field => {
-            if (localOrder[field]) {
-              resumedOrderBody[field] = localOrder[field]
-            }
-          })
-          const orderIndex = orders.findIndex(({ number }) => number === localOrder.number)
-          if (orderIndex > -1) {
-            Object.assign(orders[orderIndex], resumedOrderBody)
-          } else {
-            orders.push(resumedOrderBody)
-          }
-          ecomPassport.requestApi('/me.json', 'patch', { orders })
-        }
-      }
     }
   },
 
   created () {
-    if (this.order._id) {
-      if (this.isNew) {
-        this.saveCustomerOrder()
+    if (this.order._id && !this.skipDataLoad) {
+      const update = () => {
+        store({ url: `/orders/${this.order._id}.json` })
+          .then(({ data }) => {
+            this.localOrder = {
+              ...this.localOrder,
+              ...data
+            }
+          })
+          .catch(err => {
+            console.error(err)
+          })
       }
-      if (!this.skipDataLoad) {
-        const update = () => {
-          store({ url: `/orders/${this.order._id}.json` })
-            .then(({ data }) => {
-              this.localOrder = {
-                ...this.localOrder,
-                ...data
-              }
-            })
-            .catch(err => {
-              console.error(err)
-            })
-        }
-        this.updateInterval = setInterval(update, 9000)
-        setTimeout(() => {
-          update()
-          this.loaded = true
-        }, this.isNew ? 1000 : 3000)
-      }
+      this.updateInterval = setInterval(update, 9000)
+      setTimeout(() => {
+        update()
+        this.loaded = true
+      }, this.isNew ? 1000 : 3000)
     }
   },
 
