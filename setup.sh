ee8163adac5b0429c85c7696c65cef91be452489
Leonardo Matos <leomp120894@gmail.com>
2019-11-28 17:43:10 -0300

    build(setup): copying animate.css source

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ cp -r ./node_modules/bootstrap/scss/* ./scss/bootstrap/scss/;
 mkdir -p ./scss/bootstrap-vue/src;
 cp -r ./node_modules/bootstrap-vue/src/* ./scss/bootstrap-vue/src/;
 find ./scss/ -type f ! -name "*.scss" -delete;
+mkdir -p ./scss/animate.css/source;
+cp -r ./node_modules/animate.css/source/* ./scss/animate.css/source/;
 rm -rf ./dist;
