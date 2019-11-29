bab88ff3403d6a3611cf7286ad84b92234e431ac
Leonardo Matos <leomp120894@gmail.com>
2019-11-29 19:25:39 -0300

    chore(deps): update pkg dependencies

--- src/test.js
+++ src/test.js
@@ -3,7 +3,4 @@ import '@ecomplus/storefront-template/template/js/lib/fetch-info'
 
 import './main'
 
-import '@ecomplus/storefront-template/template/js/lib/icons'
-import '@ecomplus/storefront-template/template/js/lib/utils'
-
 console.info('Storefront App Dev Server')
