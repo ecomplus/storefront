b78ec01f9352059404d9a3dd4276a5d5b88075fb
Leonardo Matos <leomp120894@gmail.com>
2019-12-08 05:17:18 -0300

    build(scss): fix styles.scss.ejs file path

--- scripts/styles.js
+++ scripts/styles.js
@@ -8,7 +8,7 @@ const path = require('path')
 const ejs = require('ejs')
 const { theme } = require(path.resolve(INIT_CWD, 'content/settings.json'))
 
-const templateFile = path.resolve(INIT_CWD, 'template/scss/styles.scss.ejs')
+const templateFile = path.resolve(__dirname, '../template/scss/styles.scss.ejs')
 ejs.renderFile(templateFile, theme, {}, (err, scss) => {
   if (err) {
     console.error(err)
