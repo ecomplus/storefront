6102c295c0fd745bd2cb93a5a8e7d6d3c269ba28
Leonardo Matos <leomp120894@gmail.com>
2019-12-09 18:25:30 -0300

    build(webpack): fix webpack config to run with template

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,82 @@
 
 const devMode = process.env.NODE_ENV !== 'production'
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const templatePath = path.join(process.cwd(), 'node_modules/@ecomplus/storefront-template/dist')
+const { dependencies } = require('./package.json')
+const externals = require('@ecomplus/storefront-template/webpack.externals')
 
-module.exports = {
+// preset default output object
+const output = {
+  library: 'widgetTagManager',
+  libraryTarget: 'umd',
+  libraryExport: 'default',
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'widget-tag-manager.min.js'
+}
+
+// base Webpack config
+const config = {
   mode: devMode ? 'development' : 'production',
-  entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    library: 'EcomTagManager',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'ecom-tag-manager.min.js',
-    globalObject: 'this'
-  },
+  entry: path.resolve(__dirname, devMode ? 'docs/demo.js' : 'src/index.js'),
+  output,
   devServer: {
-    contentBase: path.resolve(__dirname, 'test'),
-    compress: true,
+    contentBase: templatePath,
+    stats: 'minimal',
     port: 9141,
     open: true
-  },
-  module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader'
-      }
-    }]
   },
   stats: {
     colors: true
   },
   devtool: 'source-map',
-  externals: devMode ? '' : /^(@babel\/runtime|core-js|@ecomplus\/utils)/
+
+  plugins: [],
+  module: {
+    rules: [{
+      test: /^(.(?!\.min.js$))+\.m?js$/,
+      loader: 'babel-loader'
+    }]
+  },
+
+  externals: devMode
+    // external ecomUtils on dev server to get config correctly
+    ? {
+      '@ecomplus/utils': {
+        commonjs: '@ecomplus/utils',
+        commonjs2: '@ecomplus/utils',
+        root: 'ecomUtils'
+      }
+    }
+    // exclude all imported libs on production by default
+    : [
+      externals,
+      new RegExp('^(' +
+        Object.entries(dependencies)
+          .map(([pkg]) => pkg).filter(pkg => !externals[pkg]).join('|') +
+        ')(/|$)', 'i')
+    ]
 }
+
+if (devMode) {
+  // inject widget script with HTML plugin
+  config.plugins.push(new HtmlWebpackPlugin({
+    template: path.resolve(templatePath, 'index.html')
+  }))
+}
+
+module.exports = devMode
+  // single config object for dev server
+  ? config
+  // production outputs with and without polyfill
+  : [
+    config,
+    {
+      ...config,
+      output: {
+        ...output,
+        filename: output.filename.replace('.min.js', '.root.min.js')
+      },
+      externals
+    }
+  ]
