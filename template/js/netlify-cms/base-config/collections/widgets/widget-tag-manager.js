a0bebdfdb813d48f6c3424605996a9446f4abbf5
Leonardo Matos <leomp120894@gmail.com>
2019-12-10 20:40:44 -0300

    fix(cms): add enableCheckout to tag manager widget

--- template/js/netlify-cms/base-config/collections/widgets/widget-tag-manager.js
+++ template/js/netlify-cms/base-config/collections/widgets/widget-tag-manager.js
@@ -23,6 +23,11 @@ export default {
       widget: 'boolean'
     },
     {
+      name: 'enableCheckout',
+      widget: 'hidden',
+      default: true
+    },
+    {
       label: 'Opções',
       name: 'options',
       widget: 'object',
