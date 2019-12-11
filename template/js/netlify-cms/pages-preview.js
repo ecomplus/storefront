7635ff8f5f6b4138a548151a6f9c4b6daec6eb24
Talisson Ferreira <talissonf@gmail.com>
2019-12-11 16:23:07 -0300

    chore(pages-preview.js): register cms-preview style

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -1,4 +1,5 @@
 import HomePreview from './preview/home-preview'
 
+window.CMS.registerPreviewStyle('/preview.css')
 window.CMS.registerPreviewStyle('/storefront.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
