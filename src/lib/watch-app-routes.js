1d8af379a86739c5c8ab441d023449261f552cdd
Leonardo Matos <leomp120894@gmail.com>
2019-12-11 23:16:24 -0300

    feat(checkout-events): emit checkout events from app routes

--- src/lib/watch-app-routes.js
+++ src/lib/watch-app-routes.js
@@ -1,6 +1,64 @@
+import ecomCart from '@ecomplus/shopping-cart'
+import { currencyCode, getProductData } from './common'
+
 export default dataLayer => {
   const router = window.storefrontApp && window.storefrontApp.router
   if (router) {
+    let isCartSent = false
+    const getCartProductsList = () => {
+      const products = []
+      if (ecomCart.data && Array.isArray(ecomCart.data.items)) {
+        ecomCart.data.items.forEach(item => {
+          products.push(getProductData(item))
+        })
+      }
+      return products
+    }
+
+    const emitCheckout = (step, option) => {
+      const actionField = { step, option }
+      if (step <= 1 || !isCartSent) {
+        dataLayer.push({
+          event: 'checkout',
+          ecommerce: {
+            currencyCode,
+            checkout: {
+              actionField,
+              products: getCartProductsList()
+            }
+          }
+        })
+        isCartSent = true
+      } else {
+        dataLayer.push({
+          event: 'checkoutOption',
+          ecommerce: {
+            currencyCode,
+            checkout_option: { actionField }
+          }
+        })
+      }
+    }
+
+    const emitPurchase = orderId => {
+      if (ecomCart.data) {
+        const { amount } = window.storefrontApp
+        const revenue = ((amount && amount.total) || ecomCart.data.subtotal || 0).toFixed(2)
+        dataLayer.push({
+          ecommerce: {
+            currencyCode,
+            purchase: {
+              actionField: {
+                id: orderId,
+                revenue
+              },
+              products: getCartProductsList()
+            }
+          }
+        })
+      }
+    }
+
     const addRouteToData = ({ name, params }) => {
       if (name) {
         const data = {
@@ -15,6 +73,16 @@ export default dataLayer => {
           }
         }
         dataLayer.push(data)
+
+        switch (name) {
+          case 'cart':
+          case 'checkout':
+            emitCheckout(2, name)
+            break
+          case 'confirmation':
+            emitPurchase(params.id)
+            break
+        }
       }
     }
 
