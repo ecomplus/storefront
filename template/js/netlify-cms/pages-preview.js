17ad255161141a3df790a818ac50922345a910cc
Talisson Ferreira <talissonf@gmail.com>
2019-12-14 15:57:36 -0300

    chore(pages-preview.js): regiter font-awesome

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -1,5 +1,6 @@
 import HomePreview from './preview/home-preview'
 
+window.CMS.registerPreviewStyle('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css')
 window.CMS.registerPreviewStyle('/preview.css')
 window.CMS.registerPreviewStyle('/storefront.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
