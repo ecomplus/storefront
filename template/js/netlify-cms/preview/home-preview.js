a6e5381825cb97212591c55159cdae5d6ddb54ad
Talisson Ferreira <talissonf@gmail.com>
2019-12-14 16:04:09 -0300

    chore(home-preview.js): reusable functions decoupled

--- template/js/netlify-cms/preview/home-preview.js
+++ template/js/netlify-cms/preview/home-preview.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import virtualDoc from './virtual-doc'
+import displayWidget from './display-widget'
 
 export default class HomePreview extends React.Component {
   constructor () {
     super()
     this.state = {
-      doc: '',
+      vDoc: '',
       html: ''
     }
   }
@@ -18,48 +20,76 @@ export default class HomePreview extends React.Component {
       return response.text()
     })
       .then(html => {
-        // Initialize the DOM parser
-        const parser = new DOMParser()
-        const doc = parser.parseFromString(html, 'text/html')
-        this.setState({ html, doc })
+        const vDoc = virtualDoc(html)
+        this.setState({ html, vDoc })
       })
       .catch(err => {
-        console.log('Failed to fetch page: ', err)
+        console.log('Failed to fetch page:', err)
       })
   }
 
   render () {
-    const { doc } = this.state
-    let newDoc = doc
+    const { vDoc } = this.state
+    let newDoc = vDoc
     let html
-    if (doc) {
+    if (vDoc) {
       const { entry } = this.props
       const data = JSON.parse(entry.getIn(['raw']))
 
       // set slider
-      this.props.widgetsFor('slider').getIn(['data', 'slides']).map(function(slider, index) {
-        // set last img has preview for slider
-        const img = slider.getIn(['img'])
-        const $slider = doc.querySelectorAll('.glide')
-        if (img) {
-          $slider[0].classList.add(...['glide--ltr', 'glide--slider', 'glide--swipeable'])
-          const $imgs = doc.querySelectorAll('.banner-slider img')
-          for (let i = 0; i < $imgs.length; i++) {
-            const $img = $imgs[i]
-            $img.classList.remove()
-            $img.classList.add(...['lozad', 'fade', 'img-fluid', 'show'])
-            $img.setAttribute('src', img)
-            $img.setAttribute('data-loaded', true)
+      this.props.widgetsFor('slider')
+        .getIn(['data', 'slides'])
+        .map(function (slider, index) {
+          // set last img has preview for slider
+          const img = slider.getIn(['img'])
+          const $slider = vDoc.querySelectorAll('.glide')
+          if (img) {
+            $slider[0].classList.add(...['glide--ltr', 'glide--slider', 'glide--swipeable'])
+            const $imgs = vDoc.querySelectorAll('.banner-slider img')
+            for (let i = 0; i < $imgs.length; i++) {
+              const $img = $imgs[i]
+              $img.classList.remove()
+              $img.classList.add(...['lozad', 'fade', 'img-fluid', 'show'])
+              $img.setAttribute('src', img)
+              $img.setAttribute('data-loaded', true)
+            }
+            $slider[0].style.display = 'block'
+          } else {
+            $slider[0].style.display = 'none'
           }
-          $slider[0].style.display = 'block'
-        } else {
-          $slider[0].style.display = 'none'
+        })
+
+      // products caroulse
+      const $caroulse = vDoc.querySelectorAll('.products-carousel ul')
+      if ($caroulse.length) {
+        const $ul = $caroulse[0]
+        $ul.classList.add(...['glide__slides', 'products-carousel__list'])
+        const $glide = vDoc.querySelectorAll('.products-carousel .glide')
+        if ($glide.length) {
+          $glide[0].classList.add(...['glide--ltr', 'glide--slider', 'glide--swipeable'])
         }
-      })
+        const childrens = $ul.children
+        for (let i = 0; i < childrens.length; i++) {
+          const child = childrens[i]
+          child.style.width = '270px'
+          child.style.marginRight = '5px'
+          const $elImg = child.querySelectorAll('img')
+          if ($elImg.length) {
+            $elImg[0].classList.add('show')
+            $elImg[0].setAttribute('src', $elImg[0].dataset.src)
+            $elImg[0].setAttribute('data-loaded', true)
+          }
+
+          const $cardInfo = child.querySelectorAll('.product-card__info')[0]
+          if ($cardInfo) {
+            $cardInfo.parentNode.removeChild($cardInfo)
+          }
+        }
+      }
 
       for (const key in data) {
         const objCurr = entry.getIn(['data', key])
-        newDoc = this.displayWidget(key, objCurr, doc)
+        newDoc = displayWidget(key, objCurr, vDoc)
       }
     }
 
@@ -67,22 +97,5 @@ export default class HomePreview extends React.Component {
       html = newDoc.childNodes[1].innerHTML
     }
     return h('div', { dangerouslySetInnerHTML: { __html: html } })
-  }
-
-  displayWidget (key, value, doc) {
-    const $els = doc.querySelectorAll(`[data-cms-bind="home.${key}"],[data-cms-if="home.${key}"]`)
-    for (let i = 0; i < $els.length; i++) {
-      const { cmsBind, cmsIf } = $els[i].dataset
-      if (cmsIf) {
-        if (value) {
-          $els[i].style.display = 'block'
-        } else {
-          $els[i].style.display = 'none'
-        }
-      } else if (typeof value === 'string') {
-        $els[i].innerHTML = value
-      }
-    }
-    return doc
   }
 }
