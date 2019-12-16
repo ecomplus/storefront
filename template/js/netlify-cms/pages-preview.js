12cb198e6d5f8f121f8079ef875cc7e0fe201758
Talisson Ferreira <talissonf@gmail.com>
2019-12-16 17:27:41 -0300

    chore(pages-preview.js): register product preview

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -1,6 +1,8 @@
 import HomePreview from './preview/home-preview'
+import ProductsPreview from './preview/products-preview'
 
 window.CMS.registerPreviewStyle('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css')
 window.CMS.registerPreviewStyle('/preview.css')
 window.CMS.registerPreviewStyle('/storefront.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
+window.CMS.registerPreviewTemplate('products', ProductsPreview)
