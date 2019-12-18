76a98d0d634f024c3e21f8cb13836e83c68d40cc
Talisson Ferreira <talissonf@gmail.com>
2019-12-18 17:57:44 -0300

    chore(home-preview.js): using previewCarousel()

--- template/js/netlify-cms/preview/home-preview.js
+++ template/js/netlify-cms/preview/home-preview.js
@@ -2,6 +2,8 @@ import React from 'react'
 import virtualDoc from './virtual-doc'
 import displayWidget from './display-widget'
 import fetchPage from './fetch-page'
+import previewCarousel from './preview-carousel'
+
 export default class HomePreview extends React.Component {
   constructor () {
     super()
@@ -51,34 +53,9 @@ export default class HomePreview extends React.Component {
             $slider[0].style.display = 'none'
           }
         })
-
-      // products caroulse
-      const $caroulse = vDoc.querySelectorAll('.products-carousel ul')
-      if ($caroulse.length) {
-        const $ul = $caroulse[0]
-        $ul.classList.add(...['glide__slides', 'products-carousel__list'])
-        const $glide = vDoc.querySelectorAll('.products-carousel .glide')
-        if ($glide.length) {
-          $glide[0].classList.add(...['glide--ltr', 'glide--slider', 'glide--swipeable'])
-        }
-        const childrens = $ul.children
-        for (let i = 0; i < childrens.length; i++) {
-          const child = childrens[i]
-          child.style.width = '270px'
-          child.style.marginRight = '5px'
-          const $elImg = child.querySelectorAll('img')
-          if ($elImg.length) {
-            $elImg[0].classList.add('show')
-            $elImg[0].setAttribute('src', $elImg[0].dataset.src)
-            $elImg[0].setAttribute('data-loaded', true)
-          }
-
-          const $cardInfo = child.querySelectorAll('.product-card__info')[0]
-          if ($cardInfo) {
-            $cardInfo.parentNode.removeChild($cardInfo)
-          }
-        }
-      }
+      
+      // fix carousel
+      previewCarousel(vDoc)
 
       for (const key in data) {
         const objCurr = entry.getIn(['data', key])
