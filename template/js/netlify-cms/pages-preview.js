c2c069ad2d57d666b9cdc3019249e4d1779b7c42
Talisson Ferreira <talissonf@gmail.com>
2019-12-19 00:11:05 -0300

    Update template/js/netlify-cms/pages-preview.js
    Co-Authored-By: Leonardo Matos <leomp120894@gmail.com>

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -2,7 +2,6 @@ import HomePreview from './preview/home-preview'
 import ProductsPreview from './preview/products-preview'
 
 window.CMS.registerPreviewStyle('https://use.fontawesome.com/releases/v5.12.0/css/all.css')
-window.CMS.registerPreviewStyle('/preview.css')
 window.CMS.registerPreviewStyle('/storefront.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
 window.CMS.registerPreviewTemplate('products', ProductsPreview)
