26d60c2bec843c43f3b3a31e265660b974896240
Leonardo Matos <leomp120894@gmail.com>
2019-12-27 13:02:07 -0300

    feat(product-card): handling addToCart on component source

--- src/components/js/EcProductCard.js
+++ src/components/js/EcProductCard.js
@@ -1,5 +1,6 @@
 import { $ecomConfig, name, inStock, onPromotion, price } from '@ecomplus/utils'
 import { store } from '@ecomplus/client'
+import ecomCart from '@ecomplus/shopping-cart'
 import dictionary from '@ecomplus/widget-product/src/lib/dictionary'
 import EcImage from '@ecomplus/widget-product/src/components/EcImage.vue'
 import EcPrices from '@ecomplus/widget-product/src/components/EcPrices.vue'
@@ -24,6 +25,10 @@ export default {
     productId: String,
     product: Object,
     buyText: String,
+    canAddToCart: {
+      type: Boolean,
+      default: true
+    },
     isLoaded: Boolean
   },
 
@@ -58,15 +63,20 @@ export default {
     name,
     inStock,
 
+    setBody (data) {
+      this.body = Object.assign({}, data)
+      delete this.body.body_html
+      delete this.body.body_text
+    },
+
     fetchItem () {
       if (this.productId) {
         this.isLoading = true
         const { storeId, productId } = this
         store({ url: `/products/${productId}.json`, storeId })
           .then(({ data }) => {
-            this.body = data
-            delete this.body.body_html
-            delete this.body.body_text
+            this.$emit('update:product', data)
+            this.setBody(data)
             this.$emit('update:is-loaded', true)
           })
           .catch(err => {
@@ -81,12 +91,25 @@ export default {
             this.isLoading = false
           })
       }
+    },
+
+    buy () {
+      const product = this.body
+      this.$emit('buy', { product })
+      if (this.canAddToCart) {
+        const { variations, slug } = product
+        if (variations && variations.length) {
+          window.location = `/${slug}`
+        } else {
+          ecomCart.addProduct(product)
+        }
+      }
     }
   },
 
   created () {
     if (this.product) {
-      this.body = this.product
+      this.setBody(this.product)
       if (this.product.available === undefined) {
         this.body.available = true
       }
