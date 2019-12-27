c391c1d1af271673f352f681d963e32d635bbce1
Leonardo Matos <leomp120894@gmail.com>
2019-12-27 14:32:23 -0300

    build(webpack): update output paths and add runtime output

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,60 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const templatePath = path.join(process.cwd(), 'node_modules/@ecomplus/storefront-template/dist')
-const { dependencies } = require('./package.json')
+const { dependencies, peerDependencies } = require('./package.json')
 const externals = require('@ecomplus/storefront-template/webpack.externals')
 
-// preset default output object
 const output = {
   library: 'widgetSearchEngine',
   libraryTarget: 'umd',
   libraryExport: 'default',
   path: path.resolve(__dirname, 'dist'),
-  filename: 'widget-search-engine.min.js'
+  filename: 'widget-search-engine.min.js',
+  publicPath: devMode ? '/' : '/assets/vendor/'
 }
+output.chunkFilename = output.filename.replace('.min.js', '.[name].min.js')
 
-// base Webpack config
-const config = {
+const baseModuleRules = [
+  {
+    test: /\.vue$/,
+    loader: 'vue-loader',
+    options: {
+      compilerOptions: {
+        whitespace: devMode ? 'preserve' : 'condense'
+      }
+    }
+  },
+  {
+    test: /\.(png|jpe?g|gif|svg)$/i,
+    use: 'file-loader'
+  },
+  {
+    test: /\.s?css$/,
+    use: [
+      'vue-style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          minimize: !devMode,
+          plugins: [
+            require('autoprefixer')(),
+            require('cssnano')({ preset: 'default' })
+          ]
+        }
+      },
+      'sass-loader'
+    ]
+  }
+]
+
+const moduleRulesWithPolyfill = baseModuleRules.concat([{
+  test: /^(.(?!\.min.js$))+\.m?js$/,
+  loader: 'babel-loader'
+}])
+
+const generalConfig = {
   mode: devMode ? 'development' : 'production',
   entry: path.resolve(__dirname, devMode ? 'docs/demo.js' : 'src/index.js'),
   output,
@@ -32,58 +72,20 @@ const config = {
     colors: true
   },
   devtool: 'source-map',
-  plugins: [
-    new VueLoaderPlugin()
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          compilerOptions: {
-            whitespace: devMode ? 'preserve' : 'condense'
-          }
-        }
-      },
-      {
-        test: /^(.(?!\.min.js$))+\.m?js$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: 'file-loader'
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              minimize: !devMode,
-              plugins: [
-                require('autoprefixer')(),
-                require('cssnano')({ preset: 'default' })
-              ]
-            }
-          },
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-
   resolve: {
     alias: {
       vue$: 'vue/dist/vue.esm.js'
     }
   },
+
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: moduleRulesWithPolyfill
+  },
+
   externals: devMode
-    // external ecomUtils on dev server to get config correctly
     ? {
       '@ecomplus/utils': {
         commonjs: '@ecomplus/utils',
@@ -91,35 +93,44 @@ const config = {
         root: 'ecomUtils'
       }
     }
-    // exclude all imported libs on production by default
     : [
       externals,
       new RegExp('^(' +
-        Object.entries(dependencies)
+        Object.entries({ ...dependencies, ...peerDependencies })
           .map(([pkg]) => pkg).filter(pkg => !externals[pkg]).join('|') +
         ')(/|$)', 'i')
     ]
 }
 
 if (devMode) {
-  // inject widget script with HTML plugin
-  config.plugins.push(new HtmlWebpackPlugin({
+  generalConfig.plugins.push(new HtmlWebpackPlugin({
     template: path.resolve(templatePath, 'search.html')
   }))
 }
 
-module.exports = devMode
-  // single config object for dev server
-  ? config
-  // production outputs with and without polyfill
-  : [
-    config,
-    {
-      ...config,
-      output: {
-        ...output,
-        filename: output.filename.replace('.min.js', '.root.min.js')
-      },
-      externals
-    }
-  ]
+module.exports = devMode ? generalConfig : [
+  generalConfig,
+
+  {
+    ...generalConfig,
+    output: {
+      ...output,
+      libraryTarget: 'var',
+      filename: output.filename.replace('.min.js', '.var.min.js'),
+      path: path.join(output.path, 'public')
+    },
+    externals
+  },
+
+  {
+    ...generalConfig,
+    module: {
+      rules: baseModuleRules
+    },
+    output: {
+      ...output,
+      filename: output.filename.replace('.min.js', '.runtime.min.js')
+    },
+    externals
+  }
+]
