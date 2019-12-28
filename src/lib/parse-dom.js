78c2c64745dfcbfb439be058037545c85b09542f
Leonardo Matos <leomp120894@gmail.com>
2019-12-28 02:59:14 -0300

    feat(impressions): try to set list name when parsing dom impressions

--- src/lib/parse-dom.js
+++ src/lib/parse-dom.js
@@ -4,11 +4,21 @@ export default dataLayer => {
   const $products = document.querySelectorAll('[data-sku]')
   if ($products.length) {
     const skus = []
+    const listNameBySku = {}
+    const isSearchPage = window.location.pathname === '/search'
+
     for (let i = 0; i < $products.length; i++) {
       const $product = $products[i]
       const { sku } = $product.dataset
       if (skus.indexOf(sku) === -1) {
         skus.push(sku)
+
+        if ($product.closest) {
+          const $carousel = $product.closest('.products-carousel')
+          if (!isSearchPage && $carousel) {
+            listNameBySku[sku] = $carousel.dataset.title
+          }
+        }
       }
     }
 
@@ -16,7 +26,14 @@ export default dataLayer => {
       event: 'eec.impressions',
       ecommerce: {
         currencyCode,
-        impressions: skus.map(id => ({ id }))
+        impressions: skus.map(sku => {
+          const listName = isSearchPage ? 'Search results' : listNameBySku[sku]
+          const item = { id: sku }
+          if (listName) {
+            item.list = listName
+          }
+          return item
+        })
       }
     })
   }
