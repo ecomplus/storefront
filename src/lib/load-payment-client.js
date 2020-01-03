0b8df0abcb1adbe7a18cd409c95eaf779e9ac827
Leonardo Matos <leomp120894@gmail.com>
2020-01-03 13:59:12 -0300

    perf(load-payment-client): prevent creating duplicated script tags

--- src/lib/load-payment-client.js
+++ src/lib/load-payment-client.js
@@ -1,12 +1,32 @@
 /* eslint-disable no-eval */
 
+const startedScriptsRefs = []
+
 export default (jsClient, skipOnloadExpression) => {
-  const script = document.createElement('script')
-  script.async = true
-  script.defer = true
-  const loadPromise = new Promise(resolve => {
-    script.onload = () => {
+  const src = jsClient.script_uri
+  let scriptRef = startedScriptsRefs.find(scriptRef => scriptRef.src === src)
+
+  let $script
+  if (scriptRef) {
+    $script = scriptRef.$script
+  } else {
+    $script = document.createElement('script')
+    $script.async = true
+    $script.defer = true
+    scriptRef = { src, $script }
+    startedScriptsRefs.push(scriptRef)
+
+    setTimeout(() => {
+      $script.setAttribute('src', src)
+      document.head.appendChild($script)
+    }, Math.random() * (200 - 50) + 50)
+  }
+
+  return new Promise(resolve => {
+    $script.onload = () => {
+      scriptRef.isLoaded = true
       let expression = jsClient.onload_expression
+
       const runExpression = checkoutData => {
         window._checkout = checkoutData
         Object.assign(window.storefrontApp, window._checkout)
@@ -21,6 +41,7 @@ export default (jsClient, skipOnloadExpression) => {
           }
         }
       }
+
       if (!skipOnloadExpression) {
         runExpression()
         resolve()
@@ -28,8 +49,9 @@ export default (jsClient, skipOnloadExpression) => {
         resolve(runExpression)
       }
     }
+
+    if (scriptRef.isLoaded) {
+      $script.onload()
+    }
   })
-  script.setAttribute('src', jsClient.script_uri)
-  document.head.appendChild(script)
-  return loadPromise
 }
