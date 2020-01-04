595deee1125145eef20247373c36b2cf4c2f9d04
Leonardo Matos <leomp120894@gmail.com>
2020-01-04 04:39:18 -0300

    fix(browser-support): import compiled bootstrap js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import $ from 'jquery'
-
-import 'bootstrap/js/dist/alert'
-import 'bootstrap/js/dist/button'
-// import 'bootstrap/js/dist/carousel'
-import 'bootstrap/js/dist/collapse'
-import 'bootstrap/js/dist/dropdown'
-import 'bootstrap/js/dist/modal'
-import 'bootstrap/js/dist/popover'
-// import 'bootstrap/js/dist/scrollspy'
-import 'bootstrap/js/dist/tab'
-import 'bootstrap/js/dist/toast'
-import 'bootstrap/js/dist/tooltip'
-import 'bootstrap/js/dist/util'
-
+import './bootstrap'
 import './fontawesome-icons'
 
 export {
