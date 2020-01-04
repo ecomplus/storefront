df23cad8c4520ee558609f9c0fef88c5104ca272
Leonardo Matos <leomp120894@gmail.com>
2020-01-04 01:55:12 -0300

    refactor(js): import/export jquery and bootstrap original components
    BREAKING CHANGE: bundle changed, removed bootstrap-vue, js export changed

--- src/index.js
+++ src/index.js
@@ -1,51 +1,32 @@
-import { version } from './../package.json'
+import $ from 'jquery'
+
+import Alert from 'bootstrap/js/dist/alert'
+import Button from 'bootstrap/js/src/button'
+// import Carousel from 'bootstrap/js/dist/carousel'
+import Collapse from 'bootstrap/js/dist/collapse'
+import Dropdown from 'bootstrap/js/dist/dropdown'
+import Modal from 'bootstrap/js/dist/modal'
+import Popover from 'bootstrap/js/dist/popover'
+// import Scrollspy from 'bootstrap/js/dist/scrollspy'
+import Tab from 'bootstrap/js/dist/tab'
+import Toast from 'bootstrap/js/dist/toast'
+import Tooltip from 'bootstrap/js/dist/tooltip'
+import Util from 'bootstrap/js/dist/util'
 
 import './fontawesome-icons'
 
-import {
+export {
+  $,
+  Util,
+  Alert,
+  Button,
+  // Carousel,
   Collapse,
   Dropdown,
   Modal,
+  Popover,
+  // Scrollspy,
+  Tab,
+  Toast,
   Tooltip
-} from 'bootstrap.native'
-
-import Vue from 'vue'
-
-import PortalVue from 'portal-vue'
-
-import {
-  AlertPlugin,
-  CollapsePlugin,
-  DropdownPlugin,
-  PaginationPlugin,
-  PopoverPlugin,
-  ModalPlugin,
-  TabsPlugin,
-  ToastPlugin,
-  VBPopoverPlugin,
-  VBTooltipPlugin
-} from 'bootstrap-vue'
-
-Vue.use(PortalVue)
-
-Vue.use(AlertPlugin)
-Vue.use(CollapsePlugin)
-Vue.use(DropdownPlugin)
-Vue.use(PaginationPlugin)
-Vue.use(PopoverPlugin)
-Vue.use(ModalPlugin)
-Vue.use(TabsPlugin)
-Vue.use(ToastPlugin)
-Vue.use(VBPopoverPlugin)
-Vue.use(VBTooltipPlugin)
-
-if (typeof window === 'object') {
-  window.Vue = Vue
-  window.Collapse = Collapse
-  window.Dropdown = Dropdown
-  window.Modal = Modal
-  window.Tooltip = Tooltip
-  window.__storefront_twbs = {}
 }
-
-export { version }
