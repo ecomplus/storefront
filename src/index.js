95541f999dae20fff00a9bc5a24ce6bee01780aa
Leonardo Matos <leomp120894@gmail.com>
2020-01-04 18:16:26 -0300

    chore(license): add comments (\*!) for license and cc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/*!
+ * @ecomplus/storefront-twbs
+ * (c) E-Com Club <ti@e-com.club>
+ * Released under the MIT License.
+ */
+
 import $ from 'jquery'
 import './bootstrap'
 import './fontawesome-icons'
