bed38627d41caf69f8d1f6e85cf2a53322851d36
Leonardo Matos <leomp120894@gmail.com>
2020-01-04 18:27:05 -0300

    chore(self): export _self with pkg name and version

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@
  * Released under the MIT License.
  */
 
+import { name, version } from './../package.json'
 import $ from 'jquery'
 import './bootstrap'
 import './fontawesome-icons'
 
+const _self = name + '@' + version
+
 export {
+  _self,
   $
 }
