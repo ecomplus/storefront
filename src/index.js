d51ef78a2a10742f4e3245959048b988024270ba
Leonardo Matos <leomp120894@gmail.com>
2020-01-06 19:10:44 -0300

    perf(widget): use render funtion to support vue runtime only

--- src/index.js
+++ src/index.js
@@ -2,41 +2,30 @@ import Vue from 'vue'
 import '@ecomplus/storefront-twbs'
 import EcProduct from './components/EcProduct.vue'
 import ecomCart from '@ecomplus/shopping-cart'
-import { dynamicVueSlots } from './lib/utils'
 
 export default (options = {}, elId = 'product-block') => {
   const $productBlock = document.getElementById(elId)
+
   if ($productBlock) {
     new Vue({
-      components: {
-        EcProduct
-      },
-
-      data () {
-        return {
-          options
+      render: h => h(EcProduct, {
+        attrs: {
+          id: elId
+        },
+        props: {
+          ...options.props,
+          prerenderedHTML: $productBlock.outerHTML
+        },
+        on: {
+          buy ({ product }) {
+            ecomCart.addProduct({
+              ...product,
+              body_html: null,
+              body_text: null
+            })
+          }
         }
-      },
-
-      methods: {
-        addToCart ({ product }) {
-          ecomCart.addProduct({
-            ...product,
-            body_html: null,
-            body_text: null
-          })
-        }
-      },
-
-      template: `
-      <ec-product
-        id="${elId}"
-        v-bind="options.props"
-        @buy="addToCart"
-      >
-        ${$productBlock.outerHTML}
-        ${dynamicVueSlots(options.slots)}
-      </ec-product>`
+      })
     }).$mount($productBlock)
   }
 }
