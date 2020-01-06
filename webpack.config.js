f1a5cbb7324216cd006c3a83e6140877bcfd0cd6
Leonardo Matos <leomp120894@gmail.com>
2020-01-06 19:56:36 -0300

    build(webpack): update config to use storefront-snapshot, edit ES output
    BREAKING CHANGE: replaced .runtime.js output with .es.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const devMode = process.env.NODE_ENV !== 'production'
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const templatePath = path.join(process.cwd(), 'node_modules/@ecomplus/storefront-template/dist')
+const templatePath = path.join(process.cwd(), 'node_modules/@ecomplus/storefront-snapshot')
 const { dependencies, peerDependencies } = require('./package.json')
-const externals = require('@ecomplus/storefront-template/webpack.externals')
+const externals = require('@ecomplus/storefront-snapshot/webpack.externals')
 
 const output = {
   library: 'widgetProductCard',
@@ -121,7 +121,7 @@ module.exports = devMode ? generalConfig : [
     },
     output: {
       ...output,
-      filename: output.filename.replace('.min.js', '.runtime.js')
+      filename: output.filename.replace('.min.js', '.es.js')
     }
   },
 
