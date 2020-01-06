9473815c753bcb68ea10dee5a37ba7b4918ab638
Talisson Ferreira <talissonf@gmail.com>
2020-01-06 15:19:23 -0300

    chore(pages-preview): add 'SearchPreview' and register preview.css

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -1,7 +1,10 @@
 import HomePreview from './preview/home-preview'
 import ProductsPreview from './preview/products-preview'
+import SearchPreview from './preview/search-preview'
 
 window.CMS.registerPreviewStyle('https://use.fontawesome.com/releases/v5.12.0/css/all.css')
 window.CMS.registerPreviewStyle('/storefront.css')
+window.CMS.registerPreviewStyle('/preview.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
 window.CMS.registerPreviewTemplate('products', ProductsPreview)
+window.CMS.registerPreviewTemplate('search', SearchPreview)
