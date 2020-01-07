af61446ee98d0894ede45010f2f197e36b2ed655
Leonardo Matos <leomp120894@gmail.com>
2020-01-07 18:49:36 -0300

    perf(widget): use render funtion to support vue runtime only

--- src/index.js
+++ src/index.js
@@ -7,56 +7,53 @@
 import Vue from 'vue'
 import '@ecomplus/storefront-twbs'
 import EcSearchEngine from './components/EcSearchEngine.vue'
-import { dynamicVueSlots } from '@ecomplus/widget-product/src/lib/utils'
 
 export default (options = {}, elId = 'search-engine') => {
   const $searchEngine = document.getElementById(elId)
 
   if ($searchEngine) {
     const { $overlay } = window.storefront
+    const urlParams = new URLSearchParams(window.location.search)
 
     new Vue({
-      components: {
-        EcSearchEngine
+      data: {
+        showFilters: false
       },
 
-      data () {
-        return {
-          options,
-          urlParams: new URLSearchParams(window.location.search),
-          showFilters: false
-        }
-      },
+      render (createElement) {
+        const vm = this
+        return createElement(EcSearchEngine, {
+          attrs: {
+            id: elId
+          },
+          props: {
+            ...options.props,
+            term: urlParams.get('term'),
+            page: parseInt(urlParams.get('page'), 10),
+            brands: urlParams.getAll('brands'),
+            categories: urlParams.getAll('categories'),
+            navbarId: 'header',
+            showFilters: vm.showFilters,
+            prerenderedHTML: $searchEngine.outerHTML
+          },
 
-      watch: {
-        showFilters (show) {
-          if ($overlay) {
-            if (show) {
-              $overlay.show()
-              $overlay.once('hide', () => {
-                this.showFilters = false
-              })
-            } else {
-              $overlay.hide()
+          on: {
+            'update:showFilters' (canShow) {
+              vm.showFilters = canShow
+              if ($overlay) {
+                if (canShow) {
+                  $overlay.show()
+                  $overlay.once('hide', () => {
+                    vm.showFilters = false
+                  })
+                } else {
+                  $overlay.hide()
+                }
+              }
             }
           }
-        }
-      },
-
-      template: `
-      <ec-search-engine
-        id="${elId}"
-        v-bind="options.props"
-        :term="urlParams.get('term')"
-        :page="parseInt(urlParams.get('page'), 10)"
-        :brands="urlParams.getAll('brands')"
-        :categories="urlParams.getAll('categories')"
-        :showFilters.sync="showFilters"
-        navbar-id="header"
-      >
-        ${dynamicVueSlots(options.slots)}
-        ${$searchEngine.outerHTML}
-      </ec-search-engine>`
-    }).$mount(`#${elId}`)
+        })
+      }
+    }).$mount($searchEngine)
   }
 }
