f9be791394ee90b96236a898effc5342f1f39592
Talisson Ferreira <talissonf@gmail.com>
2020-01-07 22:47:46 -0300

    chore(pages-preview):  register preview.css

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -3,5 +3,6 @@ import ProductsPreview from './preview/products-preview'
 
 window.CMS.registerPreviewStyle('https://use.fontawesome.com/releases/v5.12.0/css/all.css')
 window.CMS.registerPreviewStyle('/storefront.css')
+window.CMS.registerPreviewStyle('/preview.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
 window.CMS.registerPreviewTemplate('products', ProductsPreview)
