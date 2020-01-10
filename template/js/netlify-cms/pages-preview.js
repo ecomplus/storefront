1afc8e416c789a2ece28b4fa477720509d9eddf6
Talisson Ferreira <talissonf@gmail.com>
2020-01-10 17:52:53 -0300

    feat(cms): products page preview (#130)
    * chore(admin.js): import pages-preview.js
    
    * chore(pages-preview.js): regiter storefront style and HomePreview
    
    * chore(home-preview.js): storefront-template home page preview
    
    * chore(home.ejs): setted up data-cms for preview
    
    * chore(pkg): install react
    
    * feat(home-preview.js): render home preview
    
    * Update template/js/netlify-cms/preview/home-preview.js
    
    Co-Authored-By: Leonardo Matos <leomp120894@gmail.com>
    
    * Update template/pages/@/views/home.ejs
    
    Co-Authored-By: Leonardo Matos <leomp120894@gmail.com>
    
    * chore: setup data-cms attr for components
    
    * chore(pages-preview.js): register cms-preview style
    
    * feat(home-preview): show/hide components and update content
    
    * chore(home.ejs): render components inside tag script
    
    * chore(pages-preview.js): regiter font-awesome
    
    * chore(display-widget.js): toggle components in display
    
    * feat(virtual-doc.js): creates DOM document object for preview
    
    * chore(home-preview.js): reusable functions decoupled
    
    * chore(pkg): remove react
    
    * chore(pages-preview): change fontawsome css url
    
    * chore(display-widget): render markdown
    
    * perf(home-preview): remove handler carousel
    
    * chore(cms.ejs): import react and markdownIt from cdn
    
    * chore(home.ejs): import product-block
    
    * feat(product-block.ejs): products-carousel partial
    
    * feat(preview.scss): cms preview stylesheet
    
    * chore(home.ejs): remove product-block
    
    * chore(preview.scss): removed unused selectors
    
    * chore(home-preview): stop hide product-carousel
    
    * chore(products-carousel): revert resolve conflicts
    
    * chore(home-preview.js): change glide classes with js
    
    * chore(products-carousel): revert resolve conflicts
    
    * fix(home.ejs): render markdown
    
    * fix(home-preview): hide slide when no images
    
    * chore(pages-preview.js): register ProductPreview
    
    * chore(head.ejs): set currentPage globaly
    
    * chore: set data-cms-if with currentPage var
    
    * chore(products.ejs): set data-cms for products partial
    
    * feat(display-carousel.js): handle array of el
    
    * feat(fetch-page.js): return html from page
    
    * feat(product-preview.js): preview from product page
    
    * fix: prevents breaking the script when there are no elements
    
    * Delete product-block.ejs
    
    * chore(cms.ejs): remove duplicated script
    
    * chore: change currentPage to pageName
    
    * Update template/pages/@/views/components/pitbar.ejs
    
    Co-Authored-By: Leonardo Matos <leomp120894@gmail.com>
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -1,6 +1,8 @@
 import HomePreview from './preview/home-preview'
+import ProductPreview from './preview/product-preview'
 
 window.CMS.registerPreviewStyle('https://use.fontawesome.com/releases/v5.12.0/css/all.css')
 window.CMS.registerPreviewStyle('/storefront.css')
 window.CMS.registerPreviewStyle('/preview.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
+window.CMS.registerPreviewTemplate('products', ProductPreview)
---
+++ template/js/netlify-cms/preview/product-preview.js
@@ -0,0 +1,143 @@
+import ecomClient from '@ecomplus/client'
+import virtualDoc from './virtual-doc'
+import displayWidget from './display-widget'
+import displayCarousel from './display-carousel'
+import fetchPage from './fetch-page'
+
+export default class ProductsPreview extends window.React.Component {
+  constructor () {
+    super()
+    this.state = {
+      vDoc: '',
+      html: ''
+    }
+  }
+
+  componentDidMount () {
+    this.fetchPage()
+  }
+
+  async fetchPage () {
+    ecomClient.store({ url: '/products.json' })
+      .then(async response => {
+        const { result } = response.data
+        if (result.length) {
+          const productSlug = result[Math.floor(Math.random() * result.length)].slug
+          const html = await fetchPage(`/${productSlug}`)
+          const vDoc = virtualDoc(html)
+          this.setState({ html, vDoc })
+        }
+      })
+      .catch(error => {
+        console.error(error)
+        if (error.response) {
+          console.log(error.response)
+        }
+      })
+  }
+
+  render () {
+    const { vDoc } = this.state
+    let newDoc = vDoc
+    let html
+    if (vDoc) {
+      const { entry } = this.props
+      const data = JSON.parse(entry.getIn(['raw']))
+
+      // related
+      const $related = vDoc.querySelectorAll('[data-cms-if="products.related"]')
+
+      // hide by default
+      if (!entry.getIn(['data', 'related', 'showcase']) && Array.isArray($related) && $related.length) {
+        $related[0].style.display = 'none'
+      }
+
+      if ($related.length) {
+        this.props.widgetsFor('related')
+          .getIn(['data'])
+          .map(function (value, key) {
+            switch (key) {
+              case 'showcase':
+                if (value) {
+                  $related[0].style.display = 'block'
+                } else {
+                  $related[0].style.display = 'none'
+                }
+                break
+              case 'title':
+                $related[0].children.forEach(child => {
+                  if (child.className === 'products-carousel__title') {
+                    child.children[0].innerText = value
+                  }
+                })
+                break
+              default:
+                break
+            }
+          })
+      }
+
+      // recommended
+      const $recommended = vDoc.querySelectorAll('[data-cms-if="products.recommended"]')
+
+      // hide by default
+      if (!entry.getIn(['data', 'recommended', 'showcase']) && Array.isArray($recommended) && $recommended.length) {
+        $recommended[0].style.display = 'none'
+      }
+
+      if ($recommended.length) {
+        this.props.widgetsFor('recommended')
+          .getIn(['data'])
+          .map(function (value, key) {
+            switch (key) {
+              case 'showcase':
+                if (value) {
+                  $recommended[0].style.display = 'block'
+                } else {
+                  $recommended[0].style.display = 'none'
+                }
+                break
+              case 'title':
+                $recommended[0].children.forEach(child => {
+                  if (child.className === 'products-carousel__title') {
+                    child.children[0].innerText = value
+                  }
+                })
+                break
+              default:
+                break
+            }
+          })
+      }
+
+      // fix image widget
+      const $productPicture = vDoc.querySelectorAll('.product__picture')[0]
+      if ($productPicture && $productPicture.children) {
+        const children = $productPicture.children[0]
+        if (children.dataset.iesrc) {
+          children.setAttribute('data-loaded', true)
+          const $img = document.createElement('img')
+          $img.setAttribute('src', children.dataset.iesrc)
+          children.replaceWith($img)
+        }
+      }
+
+      const $spiners = vDoc.querySelectorAll('.spinner-border')
+      $spiners.forEach(element => element.remove())
+      //
+      displayCarousel(vDoc)
+
+      for (const key in data) {
+        const objCurr = entry.getIn(['data', key])
+        if (typeof objCurr !== 'object') {
+          newDoc = displayWidget('products', key, objCurr, vDoc)
+        }
+      }
+    }
+
+    if (newDoc.childNodes && newDoc.childNodes.length) {
+      html = newDoc.childNodes[1].innerHTML
+    }
+    return h('div', { dangerouslySetInnerHTML: { __html: html } })
+  }
+}
