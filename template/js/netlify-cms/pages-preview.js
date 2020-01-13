3b98c4d6cd007f7316c580fad08bc60ab67baae8
Leonardo Matos <leomp120894@gmail.com>
2020-01-13 13:23:49 -0300

    chore(cms): move cms-preview.css to assets to skip compilation

--- template/js/netlify-cms/pages-preview.js
+++ template/js/netlify-cms/pages-preview.js
@@ -3,6 +3,6 @@ import ProductPreview from './preview/product-preview'
 
 window.CMS.registerPreviewStyle('https://use.fontawesome.com/releases/v5.12.0/css/all.css')
 window.CMS.registerPreviewStyle('/storefront.css')
-window.CMS.registerPreviewStyle('/preview.css')
+window.CMS.registerPreviewStyle('/assets/cms-preview.css')
 window.CMS.registerPreviewTemplate('home', HomePreview)
 window.CMS.registerPreviewTemplate('products', ProductPreview)
