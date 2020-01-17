4210d4d4d694cad44760f6c5820a5e1f6542ae9e
Leonardo Matos <leomp120894@gmail.com>
2020-01-17 14:41:32 -0300

    chore(sync-cart): handling cart upsert on router and views source

--- src/store/modules/checkout.js
+++ src/store/modules/checkout.js
@@ -1,13 +1,5 @@
 import ecomCart from '@ecomplus/shopping-cart'
 import ecomClient from '@ecomplus/client'
-import { sendCart } from './../../lib/sync-cart'
-import { delay } from './../../lib/delay'
-import ecomPassport from '@ecomplus/passport-client'
-
-const handleSendCart = () => sendCart().catch(console.error)
-sendCart()
-  .catch(() => ecomPassport.once('login', handleSendCart))
-  .finally(() => ecomCart.on('change', delay(handleSendCart, 5000)))
 
 const fixMoneyValue = num => Math.round(num * 100) / 100
 
