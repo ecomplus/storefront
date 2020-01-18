b700ea8069609856518bd515f34a01ca047aad58
Leonardo Matos <leomp120894@gmail.com>
2020-01-18 16:59:45 -0300

    feat(cms): handling json file for custom merged cms config

--- template/js/netlify-cms/init.js
+++ template/js/netlify-cms/init.js
@@ -1,4 +1,5 @@
 import merge from 'lodash.merge'
 import baseConfig from './base-config/'
+import contentConfig from './../../../content/.config.json'
 
-export default customConfig => window.CMS.init(merge(baseConfig, customConfig))
+export default customConfig => window.CMS.init(merge(baseConfig, contentConfig, customConfig))
