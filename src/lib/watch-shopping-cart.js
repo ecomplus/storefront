c01444dfc6ca8997483528b726ea4e975dbf0122
igorsfreitas <igor_s_freitas@hotmail.com>
2020-01-23 17:33:25 -0300

    feat(fbq): added watch-shopping-cart for the fb pixel tracking

---
+++ src/lib/watch-shopping-cart.js
@@ -0,0 +1,65 @@
+import ecomCart from '@ecomplus/shopping-cart'
+import { currencyCode, getProductData } from './common'
+
+export default fbq => {
+  const productsBySku = {}
+
+  const emitAddToCart = item => {
+    const productData = getProductData(item)
+    fbq('eec.add', {
+      ecommerce: {
+        currencyCode,
+        add: {
+          products: [productData]
+        }
+      }
+    })
+    fbq('addToCart')
+    productsBySku[item.sku] = productData
+  }
+
+  const emitRemoveFromCart = item => {
+    const productData = productsBySku[item.sku]
+    fbq('eec.remove', {
+      ecommerce: {
+        currencyCode,
+        remove: {
+          products: [
+            (productData ? Object.assign({}, productData) : getProductData(item))
+          ]
+        }
+      }
+    })
+    fbq('removeFromCart')
+    delete productsBySku[item.sku]
+  }
+
+  ecomCart.on('addItem', ({ item }) => emitAddToCart(item))
+
+  ecomCart.on('increaseItemQnt', ({ item }) => {
+    const productData = productsBySku[item.sku]
+    if (!productData) {
+      emitAddToCart(item)
+    } else {
+      const quantity = item.quantity - productData.quantity
+      if (quantity > 0) {
+        emitAddToCart({ ...item, quantity })
+      } else {
+        emitRemoveFromCart({
+          ...item,
+          quantity: -quantity
+        })
+      }
+    }
+  })
+
+  ecomCart.on('removeItem', ({ item }) => emitRemoveFromCart(item))
+
+  ecomCart.on('clear', () => {
+    for (const sku in productsBySku) {
+      if (productsBySku[sku]) {
+        emitRemoveFromCart(productsBySku[sku])
+      }
+    }
+  })
+}
