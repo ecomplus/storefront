319b77c82a21bb1df68eab35a7802037cd0980ec
Leonardo Matos <leomp120894@gmail.com>
2020-01-23 13:12:44 -0300

    fix(wp-links): add +55 to phone number when not yet present
    [skip ci]

--- template/js/lib/utils.js
+++ template/js/lib/utils.js
@@ -6,7 +6,7 @@ for (let i = 0; i < $wpLinks.length; i++) {
   const tel = $link.dataset.tel
   if (tel) {
     let href = 'https://' + (window.screen.width <= 575.98 ? 'api' : 'web') +
-      '.whatsapp.com/send?phone=' + encodeURIComponent(tel)
+      '.whatsapp.com/send?phone=' + encodeURIComponent(tel.charAt(0) === '+' ? tel : `+55${tel}`)
     if ($link.dataset.text) {
       href += '&text=' + encodeURIComponent($link.dataset.text)
     }
