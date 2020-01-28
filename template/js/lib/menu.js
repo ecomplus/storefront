c77dbb071aa122c6eaed25a1dbba844bbdd6b6e8
Leonardo Matos <leomp120894@gmail.com>
2020-01-28 12:57:04 -0300

    fix(sidenav): preventing loop error after sidenav close (isVisible)

--- template/js/lib/menu.js
+++ template/js/lib/menu.js
@@ -3,20 +3,25 @@ import $overlay from './$overlay'
 import animateCss from './animate-css'
 
 const $menu = $('#menu')
+let isVisible = false
 
 const toggleSidenav = (isClose) => {
-  if (isClose !== true && $menu.style.display !== 'block') {
-    $overlay.show()
-    $overlay.once('hide', () => {
-      toggleSidenav(true)
-    })
-    $menu.style.display = 'block'
-    animateCss($menu, 'slideInLeft')
+  if (!isVisible) {
+    if (isClose !== true) {
+      $overlay.show()
+      $overlay.once('hide', () => {
+        toggleSidenav(true)
+      })
+      $menu.style.display = 'block'
+      animateCss($menu, 'slideInLeft')
+      isVisible = true
+    }
   } else {
     animateCss($menu, 'slideOutLeft').then(() => {
       $menu.style.display = null
       $overlay.hide()
     })
+    isVisible = false
   }
 }
 
