0217b2382caf8caa9e80af4d1a62686d3e03d858
Leonardo Matos <leomp120894@gmail.com>
2020-01-28 12:12:31 -0300

    fix(sidenav): preventing loop error after sidenav close

--- template/js/lib/menu.js
+++ template/js/lib/menu.js
@@ -4,35 +4,23 @@ import animateCss from './animate-css'
 
 const $menu = $('#menu')
 
-const sidenav = {
-  isVisible: false,
-
-  toggle () {
-    if (!this.isVisible) {
-      $overlay.show()
-      $overlay.once('hide', () => {
-        this.close()
-      })
-      $menu.style.display = 'block'
-      animateCss($menu, 'slideInLeft')
-    } else {
-      animateCss($menu, 'slideOutLeft').then(() => {
-        $menu.style.display = null
-        $overlay.hide()
-      })
-    }
-    this.isVisible = !this.isVisible
-  },
-
-  close () {
-    this.isVisible = true
-    this.toggle()
+const toggleSidenav = (isClose) => {
+  if (isClose !== true && $menu.style.display !== 'block') {
+    $overlay.show()
+    $overlay.once('hide', () => {
+      toggleSidenav(true)
+    })
+    $menu.style.display = 'block'
+    animateCss($menu, 'slideInLeft')
+  } else {
+    animateCss($menu, 'slideOutLeft').then(() => {
+      $menu.style.display = null
+      $overlay.hide()
+    })
   }
 }
 
-window.toggleSidenav = () => {
-  sidenav.toggle()
-}
+window.toggleSidenav = toggleSidenav
 
 window.closeCollapsedMenu = () => {
   $('#menu .collapse.show').classList.remove('show')
