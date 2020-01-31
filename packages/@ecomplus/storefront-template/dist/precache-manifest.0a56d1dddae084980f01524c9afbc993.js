9745a03218d3ff8925a4b2b334aa7ff16bad0f46
Leonardo Matos <leomp120894@gmail.com>
2020-01-31 14:05:50 -0300

    refactor(src): starting working tree for lerna monorepo

---
+++ packages/@ecomplus/storefront-template/dist/precache-manifest.0a56d1dddae084980f01524c9afbc993.js
@@ -0,0 +1,124 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/05feae43f4fd9e3eb976.css"
+  },
+  {
+    "url": "/05feae43f4fd9e3eb976.js"
+  },
+  {
+    "url": "/05feae43f4fd9e3eb976.js.LICENSE.txt"
+  },
+  {
+    "url": "/07dd887d496ab59eb5e3.css"
+  },
+  {
+    "url": "/07dd887d496ab59eb5e3.js"
+  },
+  {
+    "url": "/098bb02168bb17d57aa7.js"
+  },
+  {
+    "url": "/0b2ab4ad1455160b58df.css"
+  },
+  {
+    "url": "/0b2ab4ad1455160b58df.js"
+  },
+  {
+    "url": "/0b2ab4ad1455160b58df.js.LICENSE.txt"
+  },
+  {
+    "url": "/23e8b90839e3c1aa2709.js"
+  },
+  {
+    "url": "/23e8b90839e3c1aa2709.js.LICENSE.txt"
+  },
+  {
+    "url": "/2816f5aefd56664848c4.js"
+  },
+  {
+    "url": "/2b491d80e83399aa7bda.css"
+  },
+  {
+    "url": "/2b491d80e83399aa7bda.js"
+  },
+  {
+    "url": "/2b491d80e83399aa7bda.js.LICENSE.txt"
+  },
+  {
+    "url": "/63e07a14b800f86fa31d.js"
+  },
+  {
+    "url": "/84306d94b62e716e740d.js"
+  },
+  {
+    "revision": "915f07d27128adfc0e71",
+    "url": "/admin.js"
+  },
+  {
+    "revision": "f35866a743369337a67c4a07d19bbe21",
+    "url": "/admin.js.LICENSE.txt"
+  },
+  {
+    "revision": "0962880e8b1ccb28c0fa",
+    "url": "/checkout.js"
+  },
+  {
+    "revision": "3fad7e17ca725f5577a797c2baeae51b",
+    "url": "/checkout.js.LICENSE.txt"
+  },
+  {
+    "url": "/e00b788a7da5be254120.js"
+  },
+  {
+    "url": "/e7292658b509e2072706.css"
+  },
+  {
+    "url": "/e7292658b509e2072706.js"
+  },
+  {
+    "url": "/e7292658b509e2072706.js.LICENSE.txt"
+  },
+  {
+    "url": "/e877444539bd6912516f.css"
+  },
+  {
+    "url": "/e877444539bd6912516f.js"
+  },
+  {
+    "url": "/e877444539bd6912516f.js.LICENSE.txt"
+  },
+  {
+    "url": "/ff9f21e4acc7f96a39e4.js"
+  },
+  {
+    "url": "/icon_128x128.c58f8562c132df87ecb10c0598db083c.png"
+  },
+  {
+    "url": "/icon_192x192.d3b4a59e45ac4460fb8bca55139e47f4.png"
+  },
+  {
+    "url": "/icon_384x384.0240fb21e4a86ec97b70a10a0be82bb6.png"
+  },
+  {
+    "url": "/icon_512x512.a0ba80003b3cf0f275a47885c5bf31c0.png"
+  },
+  {
+    "url": "/icon_96x96.570e1710e5824f55c3c7a5c606cbfc9c.png"
+  },
+  {
+    "revision": "4743e7cabe3cd0df2a48a3a6d27fd3b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "fa973652e5af4f8e6f54",
+    "url": "/storefront.css"
+  },
+  {
+    "revision": "fa973652e5af4f8e6f54",
+    "url": "/storefront.js"
+  },
+  {
+    "revision": "46d1610a89323c10553652c9cbe87392",
+    "url": "/storefront.js.LICENSE.txt"
+  }
+]);
