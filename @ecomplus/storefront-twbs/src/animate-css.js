c804245e4dea36f973116b1a9353bcb84d066e0f
Leonardo Matos <leomp120894@gmail.com>
2020-02-07 13:03:15 -0300

    refactor(animate-css): using normal functions to improve compat

--- @ecomplus/storefront-twbs/src/animate-css.js
+++ @ecomplus/storefront-twbs/src/animate-css.js
@@ -1,10 +1,12 @@
-export default ($el, animationName) => new Promise(resolve => {
-  function handleAnimationEnd () {
-    $el.classList.remove('animated', animationName)
-    $el.removeEventListener('animationend', handleAnimationEnd)
-    resolve()
-  }
+export default function ($el, animationName) {
+  return new Promise(function (resolve) {
+    function handleAnimationEnd () {
+      $el.classList.remove('animated', animationName)
+      $el.removeEventListener('animationend', handleAnimationEnd)
+      resolve()
+    }
 
-  $el.classList.add('animated', animationName)
-  $el.addEventListener('animationend', handleAnimationEnd)
-})
+    $el.classList.add('animated', animationName)
+    $el.addEventListener('animationend', handleAnimationEnd)
+  })
+}
