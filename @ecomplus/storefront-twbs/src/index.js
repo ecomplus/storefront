f7c66b2a2db0a265aa568b8a5ab1a94b612654e4
Leonardo Matos <leomp120894@gmail.com>
2020-02-07 13:23:49 -0300

    fix(js): imports with absolute path to support webpack aliases

--- @ecomplus/storefront-twbs/src/index.js
+++ @ecomplus/storefront-twbs/src/index.js
@@ -5,10 +5,11 @@
  */
 
 import { name, version } from './../package.json'
-import $ from './$'
-import animateCss from './animate-css'
-import './bootstrap'
-import './fontawesome-icons'
+
+import $ from '@ecomplus/storefront-twbs/src/$'
+import animateCss from '@ecomplus/storefront-twbs/src/animate-css'
+import '@ecomplus/storefront-twbs/src/bootstrap'
+import '@ecomplus/storefront-twbs/src/fontawesome-icons'
 
 const _self = name + '@' + version
 
