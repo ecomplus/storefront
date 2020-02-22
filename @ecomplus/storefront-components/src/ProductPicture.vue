dbc3e2b0ba08d6f7a8492944984da27d8b2d5c8f
Leonardo Matos <leomp120894@gmail.com>
2020-02-21 21:15:31 -0300

    chore(packages): remove @ecomplus/storefront-components (not used on v1)

--- @ecomplus/storefront-components/src/ProductPicture.vue
+++
@@ -1,3 +0,0 @@
-<script src="@ecomplus/storefront-components/src/js/StPicture.js"></script>
-<template lang="html" src="@ecomplus/storefront-components/src/html/StPicture.html"></template>
-<style lang="scss" src="@ecomplus/storefront-components/src/scss/StPicture.scss"></style>
