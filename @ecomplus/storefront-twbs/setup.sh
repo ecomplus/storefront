3197ef99457b69978f4f4b9dc3fcd4a8cdee67c3
Leonardo Matos <leomp120894@gmail.com>
2020-02-21 21:28:27 -0300

    build(setup): fix copyed files path for monorepo

--- @ecomplus/storefront-twbs/setup.sh
+++ @ecomplus/storefront-twbs/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir -p ./scss/bootstrap/scss;
-cp -r ./node_modules/bootstrap/scss/* ./scss/bootstrap/scss/;
+cp -r ./../../node_modules/bootstrap/scss/* ./scss/bootstrap/scss/;
 mkdir -p ./scss/bootstrap-vue/src;
-cp -r ./node_modules/bootstrap-vue/src/* ./scss/bootstrap-vue/src/;
+cp -r ./../../node_modules/bootstrap-vue/src/* ./scss/bootstrap-vue/src/;
 mkdir -p ./scss/animate.css/source;
-cp -r ./node_modules/animate.css/source/* ./scss/animate.css/source/;
+cp -r ./../../node_modules/animate.css/source/* ./scss/animate.css/source/;
 find ./scss/ -type f -name "*.css" -exec rename .css .scss '{}' \;
 find ./scss/ -type f ! -name "*.scss" -delete;
