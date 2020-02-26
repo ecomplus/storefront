77293403387f78a00232207d6e6b7359af553588
Leonardo Matos <leomp120894@gmail.com>
2020-02-26 14:52:49 -0300

    feat(prices): prices atom component

---
+++ @ecomplus/storefront-components/src/APrices.vue
@@ -0,0 +1,3 @@
+<script src="@ecomplus/storefront-components/src/js/APrices.js"></script>
+<template lang="html" src="@ecomplus/storefront-components/src/html/APrices.html"></template>
+<style lang="scss" src="@ecomplus/storefront-components/src/scss/APrices.scss"></style>
