57062269e782d14f18f8e93c5c6ae1cdfeca9bcb
Leonardo Matos <leomp120894@gmail.com>
2020-02-27 18:31:56 -0300

    test(build-starter): check INIT_CWD env, exit with build status

--- __tests__/build-starter.sh
+++ __tests__/build-starter.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+ROOT=${INIT_CWD:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."}
+ERROR=1
 
 mkdir -p $ROOT/__tests__/packages/ && \
   cp -r $ROOT/@ecomplus $ROOT/__tests__/packages/ && \
   cd $ROOT/__tests__/storefront-starter && \
   npm link $ROOT/__tests__/packages/@ecomplus/storefront-template && \
-  npm run build
+  npm run build && \
+  ERROR=0
 
 cd $ROOT/__tests__/storefront-starter && npm unlink --no-save @ecomplus/storefront-template
 cd $ROOT/__tests__/packages/@ecomplus/storefront-template && npm unlink
 
 cd $ROOT
 rm -rf $ROOT/__tests__/packages/@ecomplus
+
+if [ $ERROR -eq 0 ]
+then
+  echo "<< Build with success >>"
+fi
+exit $ERROR
