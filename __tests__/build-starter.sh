a8f3cc3c226ac2c1c766b9ee0b88117e6cb67535
Leonardo Matos <leomp120894@gmail.com>
2020-02-27 19:22:10 -0300

    test(build-starter): run cd with both absolute and relative paths

--- __tests__/build-starter.sh
+++ __tests__/build-starter.sh
@@ -4,20 +4,30 @@ ROOT=${MONOREPO_DIR:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 &
 ERROR=1
 
 ls "$ROOT/__tests__"
+
 mkdir -p $ROOT/__tests__/packages/ && cp -r $ROOT/@ecomplus $ROOT/__tests__/packages/
 echo "1. Packages copied"
 
-cd $ROOT/__tests__/storefront-starter && \
-  npm link $ROOT/__tests__/packages/@ecomplus/storefront-template && \
+cd $ROOT/__tests__/packages/@ecomplus/storefront-template && \
+  npm link && \
+  echo "1.1 Global npm link created" && \
+  cd ../../../storefront-starter && \
+  npm link @ecomplus/storefront-template && \
+  echo "1.2 Link installed" && \
   npm run build && \
   ERROR=0
 
+cd ../../
+ls
+
 cd $ROOT/__tests__/storefront-starter && npm unlink --no-save @ecomplus/storefront-template
+cd ../..
 cd $ROOT/__tests__/packages/@ecomplus/storefront-template && npm unlink
+cd ../../../..
 
 cd $ROOT
 rm -rf $ROOT/__tests__/packages/@ecomplus
-echo "2. Clear"
+echo "3. Clear"
 
 if [ $ERROR -eq 0 ]
 then
