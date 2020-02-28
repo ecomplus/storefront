8a0cfa89ae8286d8f18365219f11a320e4529f11
Matheus <matheus@e-com.club>
2020-02-28 15:46:17 -0300

    chore: insert of seal trustvox

--- @ecomplus/widget-trustvox/src/index.js
+++ @ecomplus/widget-trustvox/src/index.js
@@ -1,12 +1,14 @@
 import ratingCards from './lib/rating-products'
 import commentsProduct from './lib/comments-products'
 import ratingSearch from './lib/rating-search'
+import sealTrustvox from './lib/seal-trustvox'
 
 export default options => {
   if (options && options.trustvoxStoreId) {
     ;[
       '//static.trustvox.com.br/assets/widget.js',
-      '//s3-sa-east-1.amazonaws.com/trustvox-rate-widget-js/widget.js'
+      '//s3-sa-east-1.amazonaws.com/trustvox-rate-widget-js/widget.js',
+      '//certificate.trustvox.com.br/widget.js'
     ].forEach(script => {
       const scriptEl = document.createElement('script')
       scriptEl.src = script
@@ -16,6 +18,7 @@ export default options => {
     ratingCards(options)
     ratingSearch(options)
     commentsProduct(options)
+    document.querySelectorAll('.footer__stamps').appendChild(sealTrustvox(options))
   } else {
     console.error(new Error('Can\'t setup Trustvox widget without `trustvoxStoreId` option'))
   }
---
+++ @ecomplus/widget-trustvox/src/lib/seal-trustvox.js
@@ -0,0 +1,6 @@
+export default () => {
+  const $div = document.createElement('div')
+  $div.classList.add('col-auto')
+  $div.setAttribute('data-trustvox-certificate-fixed', 'data-trustvox-certificate-fixed')
+  return $div
+}
