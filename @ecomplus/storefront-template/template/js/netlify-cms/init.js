7def5203ddac6be31df7dd5f486fef180e465d52
Leonardo Matos <leomp120894@gmail.com>
2020-03-09 11:28:41 -0300

    feat(cms): fetch custom config from /admin/config.json

--- @ecomplus/storefront-template/template/js/netlify-cms/init.js
+++ @ecomplus/storefront-template/template/js/netlify-cms/init.js
@@ -1,9 +1,22 @@
 import * as merge from 'lodash.merge'
+import axios from 'axios'
 import baseConfig from './base-config/'
 
-export default customConfig => {
-  const config = merge(baseConfig, customConfig)
-  window.CMS.init({ config })
-  console.log('Netlify CMS config:', config)
-  return config
-}
+export default customConfig => new Promise(resolve => {
+  let config = merge(baseConfig, customConfig)
+
+  axios.get('/admin/config.json')
+    .then(({ data }) => {
+      if (typeof data === 'object' && data) {
+        config = merge(config, data)
+      }
+    })
+    .catch(() => {
+      console.log('No custom config file at /admin/config.json')
+    })
+    .finally(() => {
+      window.CMS.init({ config })
+      console.log('Netlify CMS config:', config)
+      resolve(config)
+    })
+})
