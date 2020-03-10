c2b4d92672cee1f32b5403d6105659a87d304c92
Leonardo Matos <leomp120894@gmail.com>
2020-03-09 21:13:32 -0300

    fix(backdrop): sync visibility state by prop, also ensure fade in
    [skip ci]

--- @ecomplus/storefront-components/src/js/ABackdrop.js
+++ @ecomplus/storefront-components/src/js/ABackdrop.js
@@ -2,40 +2,65 @@ export default {
   name: 'ABackdrop',
 
   props: {
-    isClickable: {
+    isVisible: {
       type: Boolean,
       default: true
     },
-    canAutoHide: {
-      type: Boolean,
-      default: true
+    zIndexOnShow: {
+      type: Number,
+      default: 1080
+    },
+    transitionMs: {
+      type: Number,
+      default: 150
     }
   },
 
   data () {
     return {
-      style: {
-        cursor: this.isClickable ? 'pointer' : null,
-        opacity: 0.5
+      opacity: 0,
+      zIndex: null
+    }
+  },
+
+  computed: {
+    style () {
+      const { zIndex, transitionMs, opacity } = this
+      return {
+        transition: `opacity ${transitionMs}ms linear`,
+        opacity,
+        zIndex
       }
     }
   },
 
   methods: {
-    handleClick () {
-      if (this.isClickable) {
-        this.$emit('click')
-        if (this.canAutoHide) {
-          this.style.opacity = 0
-          setTimeout(() => {
-            this.$destroy()
-          }, 150)
-        }
+    hide () {
+      this.$emit('update:isVisible', false)
+    }
+  },
+
+  watch: {
+    isVisible (isVisible) {
+      this.opacity = isVisible ? null : 0
+    },
+
+    opacity (opacity) {
+      if (opacity === 0) {
+        setTimeout(() => {
+          this.zIndex = null
+        }, this.transitionMs)
+      } else {
+        this.zIndex = this.zIndexOnShow
       }
     }
   },
 
-  beforeDestroy () {
-    this.$el.remove()
+  mounted () {
+    if (this.isVisible) {
+      setTimeout(() => {
+        this.opacity = null
+      }, this.transitionMs)
+    }
   }
 }
