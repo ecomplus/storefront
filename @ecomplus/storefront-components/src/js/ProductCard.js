0d60177bf29f7fb805b90b3cde498582e98a5e8a
Leonardo Matos <leomp120894@gmail.com>
2020-03-13 19:31:49 -0300

    fix(links): using internal VLink to support vue router
    [skip ci]

--- @ecomplus/storefront-components/src/js/ProductCard.js
+++ @ecomplus/storefront-components/src/js/ProductCard.js
@@ -15,6 +15,7 @@ import {
 
 import { store } from '@ecomplus/client'
 import ecomCart from '@ecomplus/shopping-cart'
+import VLink from '../_internal/VLink.vue'
 import APicture from '../APicture.vue'
 import APrices from '../APrices.vue'
 
@@ -22,6 +23,7 @@ export default {
   name: 'ProductCard',
 
   components: {
+    VLink,
     APicture,
     APrices
   },
