3ab60a119e002bb8a56fdb532aebf7012c457523
Leonardo Matos <leomp120894@gmail.com>
2020-03-13 18:41:11 -0300

    chore(internal): v-link component for router and normal a tag
    [skip ci]

---
+++ @ecomplus/storefront-components/src/_internal/VLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <component
+    :is="isRouter ? 'router-link' : 'a'"
+    :href="isRouter ? null : href"
+    :to="isRouter ? (to || href) : null"
+  >
+    <slot/>
+  </component>
+</template>
+
+<script>
+  export default {
+    name: 'VLink',
+
+    props: {
+      href: {
+        type: String,
+        default: ''
+      },
+      to: [String, Object]
+    },
+
+    computed: {
+      isRouter () {
+        return this.$router && !this.href.startsWith('http')
+      }
+    }
+  }
+</script>
