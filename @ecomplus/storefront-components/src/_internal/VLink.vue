974b1b09e964ca3af5c0754b78f7656643ee449c
Leonardo Matos <leomp120894@gmail.com>
2020-03-13 21:31:56 -0300

    fix(links): check if route path exists to use router-link
    [skip ci]

--- @ecomplus/storefront-components/src/_internal/VLink.vue
+++ @ecomplus/storefront-components/src/_internal/VLink.vue
@@ -13,16 +13,17 @@
     name: 'VLink',
 
     props: {
-      href: {
-        type: String,
-        default: ''
-      },
+      href: String,
       to: [String, Object]
     },
 
     computed: {
       isRouter () {
-        return this.$router && !this.href.startsWith('http')
+        if (this.$router) {
+          return !this.href ? true
+            : Boolean(this.$router.options.routes.find(({ path }) => path === this.href))
+        }
+        return false
       }
     }
   }
