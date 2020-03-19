a9d69509ed948aa25b9d23a32c7de4c34907a20b
Leonardo Matos <leomp120894@gmail.com>
2020-03-19 18:32:45 -0300

    build(vuepress): move vuepress config to root, add repo md docs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,14 +1,21 @@
 const path = require('path')
 const fs = require('fs')
-const directoryPath = path.join(__dirname, '../components')
-const files = fs.readdirSync(directoryPath)
-const componentDocs = []
+const componentsPath = path.join(__dirname, '../@ecomplus/storefront-components/docs')
+const files = fs.readdirSync(componentsPath)
+const components = []
 
 for (const file of files) {
-  componentDocs.push(`/components/${file}`)
+  components.push(`/components/${file}`)
 }
 
 module.exports = {
+  base: '/storefront/',
+  patterns: [
+    '*.md',
+    '@ecomplus/*.md',
+    '@ecomplus/*/docs/**.md',
+    '@ecomplus/*/docs/**.vue'
+  ],
   themeConfig: {
     repo: 'ecomplus/storefront',
     logo: '/assets/img/logo.png',
@@ -23,11 +30,6 @@ module.exports = {
         title: 'Getting Started',
         collapsable: false,
         children: ['']
-      },
-      {
-        title: 'Components',
-        collapsable: true,
-        children: [...componentDocs]
       }
     ]
   }
--- docs/components/ABackdrop.md
+++
@@ -1,9 +0,0 @@
-# ABackdrop
-
-Simple clickable body overlay for modal and off-canvas.
-
-## Features
-
-- Hide on click;
-- Built-in fade in and out effects;
-- Ensure overlap with `z-index` consistent with `opacity`;
