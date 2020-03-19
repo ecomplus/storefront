e497acbbf075496af7d413bc928adafac2837809
Leonardo Matos <leomp120894@gmail.com>
2020-03-18 22:37:11 -0300

    docs(vuepress): add nav with home and github link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,25 @@ const fs = require('fs')
 const directoryPath = path.join(__dirname, '../components')
 const files = fs.readdirSync(directoryPath)
 const componentDocs = []
-for (let file of files) {
+
+for (const file of files) {
   componentDocs.push(`/components/${file}`)
 }
-
 
 module.exports = {
   themeConfig: {
     repo: 'ecomplus/storefront',
     logo: '/assets/img/logo.png',
+    nav: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/ecomplus/storefront'
+      }
+    ],
     sidebar: [
       {
         title: 'Getting Started',
@@ -21,9 +31,8 @@ module.exports = {
       {
         title: 'Components',
         collapsable: true,
-        children: [...componentDocs, ]
-      },
+        children: [...componentDocs]
+      }
     ]
   }
 }
-
