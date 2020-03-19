cf1a200ea1d0c7ca697e32cd5f9669d5c906e439
Leonardo Matos <leomp120894@gmail.com>
2020-03-18 21:50:18 -0300

    Update App.vue

--- @ecomplus/storefront-components/__tests__/server/App.vue
+++ @ecomplus/storefront-components/__tests__/server/App.vue
@@ -1,31 +1,44 @@
 <template>
   <div>
-    <TsProductCard/>
-    <TsBootstrap/>
-    <TsAPrices/>
-    <TsAPicture/>
-
+    <!-- <TsBootstrap/> -->
+    <!-- <TsAPrices/> -->
+    <!-- <TsAPicture/> -->
+    <!-- <TsABackdrop/> -->
+    <!-- <TsSearchEngine/> -->
+    <!-- <TsInstantSearch/> -->
+    <!-- <TsCartItem/> -->
+    <!-- <TsProductCard/> -->
+    <!-- <TsCartQuickview/> -->
+    <TsProductGallery/>
   </div>
 </template>
 
 <script>
   import '@ecomplus/storefront-twbs'
   import TsBootstrap from '__/components/TsBootstrap.vue'
+  import TsABackdrop from '__/components/TsABackdrop.vue'
   import TsAPicture from '__/components/TsAPicture.vue'
   import TsAPrices from '__/components/TsAPrices.vue'
   import TsProductCard from '__/components/TsProductCard.vue'
+  import TsSearchEngine from '__/components/TsSearchEngine.vue'
+  import TsInstantSearch from '__/components/TsInstantSearch.vue'
+  import TsCartItem from '__/components/TsCartItem.vue'
+  import TsCartQuickview from '__/components/TsCartQuickview.vue'
+  import TsProductGallery from '__/components/TsProductGallery.vue'
 
   export default {
     name: 'Playground',
     components: {
       TsBootstrap,
+      TsABackdrop,
       TsAPicture,
       TsAPrices,
-      TsProductCard
+      TsProductCard,
+      TsSearchEngine,
+      TsInstantSearch,
+      TsCartItem,
+      TsCartQuickview,
+      TsProductGallery
     }
   }
 </script>
-
-<style lang="scss">
-  @import '@ecomplus/storefront-twbs/scss/styles';
-</style>
