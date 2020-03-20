6ab36dc80ef48cad8b47353af99e8e60c81c9129
Leonardo Matos <leomp120894@gmail.com>
2020-03-20 17:26:52 -0300

    feat(product-variations): add new ProductVariations component :tada:
    list all grids and variation options
    
    support variation select (sync)
    
    [skip ci]

---
+++ @ecomplus/storefront-components/src/ProductVariations.vue
@@ -0,0 +1,3 @@
+<script src="./js/ProductVariations.js"></script>
+<template lang="html" src="./html/ProductVariations.html"></template>
+<style lang="scss" src="./scss/ProductVariations.scss"></style>
