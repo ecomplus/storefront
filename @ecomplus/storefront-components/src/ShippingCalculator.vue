07a40945fff66cdf569775f7702a7f3417cedbda
Leonardo Matos <leomp120894@gmail.com>
2020-03-20 15:25:03 -0300

    feat(shipping-calculator): add new ShippingCalculator component :tada:
    zip input with cleave to format
    
    option to select shipping option
    
    store zip code locally to reuse
    
    [skip ci]

---
+++ @ecomplus/storefront-components/src/ShippingCalculator.vue
@@ -0,0 +1,3 @@
+<script src="./js/ShippingCalculator.js"></script>
+<template lang="html" src="./html/ShippingCalculator.html"></template>
+<style lang="scss" src="./scss/ShippingCalculator.scss"></style>
---
+++ @ecomplus/storefront-components/src/js/ShippingCalculator.js
@@ -0,0 +1,194 @@
+import {
+  i19calculateShipping,
+  i19zipCode
+} from '@ecomplus/i18n'
+
+import {
+  $ecomConfig,
+  i18n,
+  price,
+  formatMoney
+} from '@ecomplus/utils'
+
+import { modules } from '@ecomplus/client'
+import CleaveInput from 'vue-cleave-component'
+import ShippingLine from '../ShippingLine.vue'
+
+const { localStorage } = window
+const zipStorageKey = 'shipping-to-zip'
+
+const reduceItemBody = itemOrProduct => {
+  const shippedItem = {}
+  ;[
+    'product_id',
+    'variation_id',
+    'sku',
+    'name',
+    'quantity',
+    'currency_id',
+    'currency_symbol',
+    'price',
+    'final_price',
+    'dimensions',
+    'weight'
+  ].forEach(field => {
+    if (itemOrProduct[field] !== undefined) {
+      shippedItem[field] = itemOrProduct[field]
+    }
+  })
+  return shippedItem
+}
+
+export default {
+  name: 'ShippingCalculator',
+
+  components: {
+    CleaveInput,
+    ShippingLine
+  },
+
+  props: {
+    zipCode: String,
+    canSelectServices: Boolean,
+    canInputZip: {
+      type: Boolean,
+      default: true
+    },
+    countryCode: {
+      type: String,
+      default: $ecomConfig.get('country_code')
+    },
+    shippedItems: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    shippingResult: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    shippingData: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+
+  data () {
+    return {
+      localZipCode: this.zipCode,
+      shippingServices: [],
+      selectedService: null,
+      isWaiting: false
+    }
+  },
+
+  computed: {
+    i19calculateShipping: () => i18n(i19calculateShipping),
+    i19zipCode: () => i18n(i19zipCode),
+
+    cleaveOptions () {
+      return this.countryCode === 'BR'
+        ? { blocks: [5, 3], delimiter: '-' }
+        : { blocks: [30] }
+    }
+  },
+
+  methods: {
+    formatMoney,
+
+    updateZipCode () {
+      this.$emit('update:zip-code', this.localZipCode)
+    },
+
+    parseShippingOptions (shippingResult = [], isRetry) {
+      this.shippingServices = []
+      let canRetry
+      if (shippingResult.length) {
+        shippingResult.forEach(appResult => {
+          const { validated, error, response } = appResult
+          if (validated && !error) {
+            response.shipping_services.forEach(service => {
+              this.shippingServices.push({
+                app_id: appResult.app_id,
+                ...service
+              })
+            })
+          } else if (isRetry !== true && (!response || !response.error)) {
+            canRetry = true
+          }
+        })
+        if (!this.shippingServices.length) {
+          if (canRetry) {
+            this.fetchShippingServices(true)
+          }
+        } else {
+          this.setSelectedService(0)
+        }
+      }
+    },
+
+    fetchShippingServices (isRetry) {
+      const { storeId } = this
+      const url = '/calculate_shipping.json'
+      const method = 'POST'
+      const data = {
+        ...this.shippingData,
+        to: {
+          zip: this.localZipCode,
+          ...this.shippingData.to
+        }
+      }
+      if (this.shippedItems.length) {
+        data.items = this.shippedItems.map(reduceItemBody)
+        const itemsToSubtotal = (subtotal, item) => subtotal + price(item) * item.quantity
+        data.subtotal = data.items.reduce(itemsToSubtotal, 0)
+      }
+      this.isWaiting = true
+      modules({ url, method, storeId, data })
+        .then(({ data }) => this.parseShippingOptions(data.result, isRetry))
+        .catch(console.error)
+        .finally(() => {
+          this.isWaiting = false
+        })
+    },
+
+    submitZipCode (e) {
+      this.updateZipCode()
+      if (localStorage) {
+        localStorage.setItem(zipStorageKey, this.localZipCode)
+      }
+      this.fetchShippingServices()
+    },
+
+    setSelectedService (i) {
+      if (this.canSelectServices) {
+        this.$emit('select-service', this.shippingServices[i])
+        this.selectedService = i
+      }
+    }
+  },
+
+  created () {
+    if (localStorage) {
+      if (!this.zipCode) {
+        const storedZip = localStorage.getItem(zipStorageKey)
+        if (storedZip) {
+          this.localZipCode = storedZip
+          this.updateZipCode()
+        }
+      }
+      if (!this.shippingResult.length) {
+        if (this.localZipCode) {
+          this.fetchShippingServices()
+        }
+      } else {
+        this.parseShippingOptions(this.shippingResult)
+      }
+    }
+  }
+}
