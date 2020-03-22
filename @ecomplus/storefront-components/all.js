3e61b7e9f1b97d26e15f9dd7b1b730f6390b0c11
Leonardo Matos <leomp120894@gmail.com>
2020-03-22 19:21:09 -0300

    build(all): setup js entry file with all components
    [skip ci]

---
+++ @ecomplus/storefront-components/all.js
@@ -0,0 +1,29 @@
+import ABackdrop from './src/ABackdrop.vue'
+import APicture from './src/APicture.vue'
+import APrices from './src/APrices.vue'
+import CartItem from './src/CartItem.vue'
+import CartQuickview from './src/CartQuickview.vue'
+import InstantSearch from './src/InstantSearch.vue'
+import ProductCard from './src/ProductCard.vue'
+import ProductGallery from './src/ProductGallery.vue'
+import ProductVariations from './src/ProductVariations.vue'
+import SearchEngine from './src/SearchEngine.vue'
+import ShippingCalculator from './src/ShippingCalculator.vue'
+import ShippingLine from './src/ShippingLine.vue'
+import TheProduct from './src/TheProduct.vue'
+
+export {
+  ABackdrop,
+  APicture,
+  APrices,
+  CartItem,
+  CartQuickview,
+  InstantSearch,
+  ProductCard,
+  ProductGallery,
+  ProductVariations,
+  SearchEngine,
+  ShippingCalculator,
+  ShippingLine,
+  TheProduct
+}
