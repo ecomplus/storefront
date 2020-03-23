3184ebb28b728a76a0df99c8de64a03076332d23
Leonardo Matos <leomp120894@gmail.com>
2020-03-22 22:52:25 -0300

    chore(internal): setup internal BAlert component
    [skip ci]

---
+++ @ecomplus/storefront-components/src/_internal/BAlert.vue
@@ -0,0 +1,38 @@
+<template>
+  <transition enter-active-class="animated fadeInDown fast">
+    <div v-if="canShow">
+      <div
+        class="alert alert-warning alert-dismissible fade show"
+        role="alert"
+        v-once
+      >
+        <slot/>
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          :aria-label="i19close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  import { i19close } from '@ecomplus/i18n'
+  import { i18n } from '@ecomplus/utils'
+
+  export default {
+    name: 'BAlert',
+
+    props: {
+      canShow: Boolean
+    },
+
+    computed: {
+      i19close: () => i18n(i19close)
+    }
+  }
+</script>
