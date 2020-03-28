e9f9ec8c2cd71fc2516f664674366fa8a85cf8e7
Leonardo Matos <leomp120894@gmail.com>
2020-03-28 01:07:48 -0300

    docs(vuepress): fix product card demo with .demo class runtime load

---
+++ .vuepress/components/DemoProductCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="demo">
+    <div style="max-width: 290px">
+      <ProductCard :product="product"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import product from '../../@ecomplus/storefront-components/__tests__/data/product'
+
+export default {
+  name: 'DemoProductCard',
+
+  data () {
+    return {
+      product
+    }
+  }
+}
+</script>
---
+++ .vuepress/components/ProductCard.vue
@@ -0,0 +1,8 @@
+<script src="../../@ecomplus/storefront-components/src/js/ProductCard.js"></script>
+<template lang="html" src="../../@ecomplus/storefront-components/src/html/ProductCard.html"></template>
+
+<styles lang="scss">
+  .demo {
+    @import "../../@ecomplus/storefront-components/src/scss/ProductCard";
+  }
+</styles>
