78b59f30f5dcba00539b4bae02d59561026d302d
Leonardo Matos <leomp120894@gmail.com>
2020-03-28 00:56:17 -0300

    build(webpack): update default custom config with externals only

--- @ecomplus/storefront-template/storefront.webpack.js
+++ @ecomplus/storefront-template/storefront.webpack.js
@@ -3,13 +3,5 @@ module.exports = {
     vue: 'Vue',
     jquery: '$',
     'popper.js': 'Popper'
-  },
-  resolve: {
-    alias: {
-      /* Uncomment to use customized product card Vue component
-      '@ecomplus/widget-product-card/src/components/EcProductCard.vue$':
-        require('path').resolve(__dirname, 'template/js/components/ProductCard.vue')
-      */
-    }
   }
 }
