7149f7c0d0fb222bc5a22c349cde1b9f13ee24a1
Leonardo Matos <leomp120894@gmail.com>
2020-04-03 04:08:14 -0300

    build(webpack): alias to empty scss for preimported component styles
    [skip ci]

--- @ecomplus/storefront-template/storefront.webpack.js
+++ @ecomplus/storefront-template/storefront.webpack.js
@@ -1,7 +1,20 @@
+const path = require('path')
+const emptyScss = path.resolve(__dirname, '__fixtures__/empty.scss')
+
 module.exports = {
   externals: {
+    // imported from CDN
     vue: 'Vue',
     jquery: '$',
     'popper.js': 'Popper'
+  },
+  resolve: {
+    alias: {
+      // components SCSS directly imported to styles.scss
+      './scss/APicture.scss': emptyScss,
+      './scss/APrices.scss': emptyScss,
+      './scss/ProductCard.scss': emptyScss,
+      './scss/TheProduct.scss': emptyScss
+    }
   }
 }
