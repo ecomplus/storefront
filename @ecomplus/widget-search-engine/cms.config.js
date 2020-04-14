2b8c1fb72dc8bfbcca35bf836d46fc1c217ae80e
Leonardo Matos <leomp120894@gmail.com>
2020-04-14 18:51:13 -0300

    feat(cms): setup widget cms config
    https://github.com/ecomplus/storefront/issues/165
    
    [skip ci]

---
+++ @ecomplus/widget-search-engine/cms.config.js
@@ -0,0 +1,24 @@
+import { name } from './package.json'
+
+export default () => ({
+  label: 'Motor de busca',
+  fields: [
+    {
+      name: 'pkg',
+      widget: 'hidden',
+      default: name
+    },
+    {
+      label: 'Ativar',
+      hint: 'Renderiza motor completo e resultados na página de busca',
+      name: 'active',
+      widget: 'boolean'
+    },
+    {
+      label: 'Apenas em desktops',
+      name: 'desktopOnly',
+      hint: 'Desativa o widget em dispositivos móveis',
+      widget: 'boolean'
+    }
+  ]
+})
