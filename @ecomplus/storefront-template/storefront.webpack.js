727e86909bd19ca0ac962251b9575e4971b3df0e
Leonardo Matos <leomp120894@gmail.com>
2020-04-14 04:42:34 -0300

    build(webpack): custom config use cdn on production only
    [skip ci]

--- @ecomplus/storefront-template/storefront.webpack.js
+++ @ecomplus/storefront-template/storefront.webpack.js
@@ -1,20 +1,27 @@
 const path = require('path')
 const emptyScss = path.resolve(__dirname, '__fixtures__/empty.scss')
 
-module.exports = {
-  externals: {
-    // imported from CDN
-    vue: 'Vue',
-    jquery: '$',
-    'popper.js': 'Popper'
-  },
-  resolve: {
-    alias: {
-      // components SCSS directly imported to styles.scss
-      './scss/APicture.scss': emptyScss,
-      './scss/APrices.scss': emptyScss,
-      './scss/ProductCard.scss': emptyScss,
-      './scss/TheProduct.scss': emptyScss
+module.exports = ({ devMode }) => {
+  const customConfig = {
+    resolve: {
+      alias: {
+        // components SCSS directly imported to styles.scss
+        './scss/APicture.scss': emptyScss,
+        './scss/APrices.scss': emptyScss,
+        './scss/ProductCard.scss': emptyScss,
+        './scss/TheProduct.scss': emptyScss
+      }
     }
   }
+  if (!devMode) {
+    customConfig.externals = {
+      // imported from CDN
+      vue: 'Vue',
+      jquery: '$',
+      'popper.js': 'Popper'
+    }
+  } else {
+    customConfig.resolve.alias.jquery$ = 'jquery/dist/jquery.slim'
+  }
+  return customConfig
 }
