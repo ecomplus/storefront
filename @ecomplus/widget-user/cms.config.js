6cae7cf0b15f0d7adb5a8352f3e5c64484d69622
Leonardo Matos <leomp120894@gmail.com>
2020-04-15 01:49:04 -0300

    feat(cms): setup widget-user config
    https://github.com/ecomplus/storefront/issues/165
    
    [skip ci]

---
+++ @ecomplus/widget-user/cms.config.js
@@ -0,0 +1,24 @@
+import { name } from './package.json'
+
+export default () => ({
+  label: 'Modal de login',
+  fields: [
+    {
+      name: 'pkg',
+      widget: 'hidden',
+      default: name
+    },
+    {
+      label: 'Ativar',
+      hint: 'Caixa para login/logout sem redirecionar para a página minha conta',
+      name: 'active',
+      widget: 'boolean'
+    },
+    {
+      label: 'Apenas em desktops',
+      name: 'desktopOnly',
+      hint: 'Desativa o widget em dispositivos móveis',
+      widget: 'boolean'
+    }
+  ]
+})
