c4c001b8a42797b1baef962f4c87d0f136ffc6d2
Leonardo Matos <leomp120894@gmail.com>
2020-05-15 23:10:01 -0300

    feat(payment-option): add new 'PaymentOption' component :tada:
    show payment gateway info
    
    calculate product price with discount by payment method

---
+++ @ecomplus/storefront-components/src/PaymentOption.vue
@@ -0,0 +1,3 @@
+<script src="./js/PaymentOption.js"></script>
+<template lang="html" src="./html/PaymentOption.html"></template>
+<style lang="scss" src="./scss/PaymentOption.scss"></style>
---
+++ @ecomplus/storefront-components/src/js/PaymentOption.js
@@ -0,0 +1,64 @@
+import {
+  i19atSight,
+  i19interestFree,
+  i19of,
+  i19ofDiscount,
+  i19onFreight
+} from '@ecomplus/i18n'
+
+import {
+  i18n,
+  formatMoney
+} from '@ecomplus/utils'
+
+export default {
+  name: 'PaymentOption',
+
+  props: {
+    paymentGateway: {
+      type: Object,
+      required: true
+    },
+    price: Number
+  },
+
+  computed: {
+    i19atSight: () => i18n(i19atSight),
+    i19interestFree: () => i18n(i19interestFree),
+    i19of: () => i18n(i19of),
+    i19ofDiscount: () => i18n(i19ofDiscount),
+    i19onFreight: () => i18n(i19onFreight),
+
+    discount () {
+      if (this.paymentGateway.discount && this.paymentGateway.discount.value > 0) {
+        return this.paymentGateway.discount
+      }
+    },
+
+    installmentOptions () {
+      if (
+        this.paymentGateway.installment_options &&
+        this.paymentGateway.installment_options.length
+      ) {
+        return this.paymentGateway.installment_options.concat().sort((a, b) => {
+          return a.number - b.number
+        })
+      }
+    },
+
+    priceWithDiscount () {
+      if (this.price && this.discount && this.discount.apply_at !== 'freight') {
+        if (this.discount.type === 'percentage') {
+          return this.price * (100 - this.discount.value) / 100
+        } else {
+          return Math.min(this.price - this.discount.value, 0)
+        }
+      }
+      return this.price
+    }
+  },
+
+  methods: {
+    formatMoney
+  }
+}
