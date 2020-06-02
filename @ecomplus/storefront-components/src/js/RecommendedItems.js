083ddd7d4b1b675103524753e86523fa625cc112
Leonardo Matos <leomp120894@gmail.com>
2020-06-02 06:58:55 -0300

    feat(recommended-items): new 'RecommendedItems' component :tada:
    for cart/checkout recommendation
    
    try best match with current cart items
    
    sort by sales by default
    
    using default product card with grid and card customization options

---
+++ @ecomplus/storefront-components/src/js/RecommendedItems.js
@@ -0,0 +1,157 @@
+import {
+  i19add,
+  i19seeMore
+  // i19weRecommendedForYou
+} from '@ecomplus/i18n'
+
+import {
+  i18n,
+  recommendedIds as getRecommendedIds
+} from '@ecomplus/utils'
+
+import { graphs } from '@ecomplus/client'
+import EcomSearch from '@ecomplus/search-engine'
+import ecomCart from '@ecomplus/shopping-cart'
+import addIdleCallback from './lib/add-idle-callback'
+import ProductCard from '../ProductCard.vue'
+
+export default {
+  name: 'RecommendedItems',
+
+  components: {
+    ProductCard
+  },
+
+  props: {
+    pageSize: {
+      type: Number,
+      default: 4
+    },
+    sortOrder: {
+      type: String,
+      default: 'sales'
+    },
+    canLoadMore: {
+      type: Boolean,
+      default: true
+    },
+    rowClassName: {
+      type: String,
+      default: 'row no-gutters'
+    },
+    colClassName: {
+      type: String,
+      default: 'col-6 col-md-4 col-lg-3'
+    },
+    productCardProps: {
+      type: Object,
+      default () {
+        return {
+          isSmall: true,
+          buyText: i18n(i19add)
+        }
+      }
+    },
+    ecomCart: {
+      type: Object,
+      default () {
+        return ecomCart
+      }
+    }
+  },
+
+  data () {
+    return {
+      ecomSearch: new EcomSearch()
+        .mergeFilter({
+          range: {
+            quantity: {
+              gt: 0
+            }
+          }
+        })
+        .mergeFilter({
+          term: {
+            available: true
+          }
+        }),
+      pageNumber: 1,
+      items: []
+    }
+  },
+
+  computed: {
+    i19seeMore: () => i18n(i19seeMore),
+    i19weRecommendedForYou: () => 'Nós recomendados para você'
+  },
+
+  methods: {
+    fetchItems () {
+      delete this.ecomSearch.dsl.aggs
+      this.ecomSearch.fetch().then(() => {
+        this.items = this.items.concat(this.ecomSearch.getItems())
+        this.totalCount = this.ecomSearch.getTotalCount()
+        if (this.totalCount) {
+          this.$emit('recommend-items', {
+            items: this.items,
+            totalCount: this.totalCount
+          })
+        }
+      })
+    }
+  },
+
+  created () {
+    const fetchRecommendations = (matchType = 'recommended') => {
+      const promises = []
+      const items = this.ecomCart.data.items.sort((a, b) => a.quantity > b.quantity ? -1 : 1)
+      for (let i = 0; i < items.length && i <= 4; i++) {
+        promises.push(graphs({ url: `/products/${items[i]._id}/${matchType}.json` }))
+      }
+      Promise.allSettled(promises).then(results => {
+        const productIds = []
+        results.forEach(({ status, value }) => {
+          if (status === 'fulfilled') {
+            getRecommendedIds(value.data).forEach(productId => {
+              if (
+                !productIds.includes(productId) &&
+                !this.ecomCart.data.items.find(item => item.product_id === productId)
+              ) {
+                productIds.push(productId)
+              }
+            })
+          }
+        })
+        if (productIds.length) {
+          this.ecomSearch.setProductIds(productIds.slice(0, 24))
+          this.fetchItems()
+        } else if (matchType === 'recommended') {
+          fetchRecommendations('related')
+        }
+      })
+    }
+    addIdleCallback(() => {
+      fetchRecommendations()
+    })
+  },
+
+  watch: {
+    pageSize: {
+      handler (pageSize) {
+        this.ecomSearch.setPageSize(pageSize)
+      },
+      immediate: true
+    },
+
+    sortOrder: {
+      handler (sortOrder) {
+        this.ecomSearch.setSortOrder(sortOrder)
+      },
+      immediate: true
+    },
+
+    pageNumber () {
+      this.fetchItems()
+    }
+  }
+}
