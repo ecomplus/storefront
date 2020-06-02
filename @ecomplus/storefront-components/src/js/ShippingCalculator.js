6a911885b6440dd9041002304284416f992ffd27
Leonardo Matos <leomp120894@gmail.com>
2020-06-01 22:34:08 -0300

    feat(shipping-calculator): add 'free-from-value' and 'option' slots
    handling 'almost free shipping' percentage progress by default

--- @ecomplus/storefront-components/src/js/ShippingCalculator.js
+++ @ecomplus/storefront-components/src/js/ShippingCalculator.js
@@ -1,5 +1,7 @@
 import {
+  // i19add$1ToEarn,
   i19calculateShipping,
+  i19freeShipping,
   i19zipCode
 } from '@ecomplus/i18n'
 
@@ -81,20 +83,34 @@ export default {
   data () {
     return {
       localZipCode: null,
+      localShippedItems: [],
+      amountSubtotal: null,
       shippingServices: [],
       selectedService: null,
-      isWaiting: false
+      hasFreeOption: false,
+      freeFromValue: null,
+      isScheduled: false,
+      isWaiting: false,
+      hasCalculated: false
     }
   },
 
   computed: {
+    i19add$1ToEarn: () => 'Adicione $1 para ganhar',
     i19calculateShipping: () => i18n(i19calculateShipping),
     i19zipCode: () => i18n(i19zipCode),
+    i19freeShipping: () => i18n(i19freeShipping).toLowerCase(),
 
     cleaveOptions () {
       return this.countryCode === 'BR'
         ? { blocks: [5, 3], delimiter: '-' }
         : { blocks: [30] }
+    },
+
+    freeFromPercentage () {
+      return !this.hasFreeOption && this.amountSubtotal < this.freeFromValue
+        ? Math.round(this.amountSubtotal * 100 / this.freeFromValue)
+        : null
     }
   },
 
@@ -118,6 +134,13 @@ export default {
                 ...service
               })
             })
+            const freeShippingFromValue = response.free_shipping_from_value
+            if (
+              freeShippingFromValue &&
+              (!this.freeFromValue || this.freeFromValue > freeShippingFromValue)
+            ) {
+              this.freeFromValue = freeShippingFromValue
+            }
           } else if (isRetry !== true && (!response || !response.error)) {
             canRetry = true
           }
@@ -128,33 +151,42 @@ export default {
           }
         } else {
           this.setSelectedService(0)
+          this.hasFreeOption = Boolean(this.shippingServices.find(service => {
+            return !service.shipping_line.total_price && !service.shipping_line.price
+          }))
         }
       }
     },
 
     fetchShippingServices (isRetry) {
-      const { storeId } = this
-      const url = '/calculate_shipping.json'
-      const method = 'POST'
-      const data = {
-        ...this.shippingData,
-        to: {
-          zip: this.localZipCode,
-          ...this.shippingData.to
-        }
+      if (!this.isScheduled) {
+        this.isScheduled = true
+        setTimeout(() => {
+          this.isScheduled = false
+          const { storeId } = this
+          const url = '/calculate_shipping.json'
+          const method = 'POST'
+          const data = {
+            ...this.shippingData,
+            to: {
+              zip: this.localZipCode,
+              ...this.shippingData.to
+            }
+          }
+          if (this.localShippedItems.length) {
+            data.items = this.localShippedItems
+            data.subtotal = this.amountSubtotal
+          }
+          this.isWaiting = true
+          modules({ url, method, storeId, data })
+            .then(({ data }) => this.parseShippingOptions(data.result, isRetry))
+            .catch(console.error)
+            .finally(() => {
+              this.hasCalculated = true
+              this.isWaiting = false
+            })
+        }, this.hasCalculated ? 150 : 50)
       }
-      if (this.shippedItems.length) {
-        data.items = this.shippedItems.map(reduceItemBody)
-        const itemsToSubtotal = (subtotal, item) => subtotal + getPrice(item) * item.quantity
-        data.subtotal = data.items.reduce(itemsToSubtotal, 0)
-      }
-      this.isWaiting = true
-      modules({ url, method, storeId, data })
-        .then(({ data }) => this.parseShippingOptions(data.result, isRetry))
-        .catch(console.error)
-        .finally(() => {
-          this.isWaiting = false
-        })
     },
 
     submitZipCode () {
@@ -174,6 +206,20 @@ export default {
   },
 
   watch: {
+    shippedItems: {
+      handler () {
+        this.localShippedItems = this.shippedItems.map(reduceItemBody)
+        this.amountSubtotal = this.shippedItems.reduce((subtotal, item) => {
+          return subtotal + getPrice(item) * item.quantity
+        }, 0)
+        if (this.hasCalculated) {
+          this.fetchShippingServices()
+        }
+      },
+      deep: true,
+      immediate: true
+    },
+
     localZipCode (zipCode) {
       if (this.countryCode === 'BR' && zipCode.replace(/\D/g, '').length === 8) {
         this.submitZipCode()
