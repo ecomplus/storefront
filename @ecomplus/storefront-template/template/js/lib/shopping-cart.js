5c26b3c80e787f3f88ef56b1e77eabb98c5a33a1
Leonardo Matos <leomp120894@gmail.com>
2020-06-05 19:21:29 -0300

    fix(shopping-cart): remove temporary items on page load

--- @ecomplus/storefront-template/template/js/lib/shopping-cart.js
+++ @ecomplus/storefront-template/template/js/lib/shopping-cart.js
@@ -12,3 +12,9 @@ if ($cartCount) {
   ecomCart.on('change', showItemsCount)
   showItemsCount()
 }
+
+ecomCart.data.items.forEach(({ _id, flags }) => {
+  if (Array.isArray(flags) && flags.includes('__tmp')) {
+    ecomCart.removeItem(_id)
+  }
+})
