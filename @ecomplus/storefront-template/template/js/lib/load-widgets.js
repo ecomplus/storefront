abcded397003a604b245808b3d06a3d97c75dd9e
Talisson Ferreira <talissonf@gmail.com>
2020-06-16 12:05:33 -0300

    chore(load-widgets): using @ecomplus/widget-offers-notification

--- @ecomplus/storefront-template/template/js/lib/load-widgets.js
+++ @ecomplus/storefront-template/template/js/lib/load-widgets.js
@@ -100,6 +100,10 @@ Promise.all(widgetsLoadPromises).then(() => {
         '@ecomplus/widget-ebit',
         () => import('@ecomplus/widget-ebit')
       )
+      loadWidget(
+        '@ecomplus/widget-offers-notification',
+        () => import('@ecomplus/widget-offers-notification')
+      )
     })
   }
 
