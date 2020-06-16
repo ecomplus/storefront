a81f3e55fb2cd62a6e69f0e56a1fc93b9719b334
Talisson Ferreira <talissonf@gmail.com>
2020-06-16 17:29:13 -0300

    feat(index): open new window with app-offers-notification

--- @ecomplus/widget-offers-notification/src/index.js
+++ @ecomplus/widget-offers-notification/src/index.js
@@ -1,30 +1,14 @@
 
 export default (options = {}) => {
-  const { body } = window._context
-  const { _widgets } = window
-  options.btnOutStock = false
-  options.btnPriceChange = false
-
-  if (_widgets && _widgets['@ecomplus/widget-offers-notification']) {
-    const { enabledOutOfStock, enabledPriceChange } = _widgets['@ecomplus/widget-offers-notification']
-
-    if (body.quantity === 0 && enabledOutOfStock) {
-      options.btnOutStock = true
-    }
-
-    if (body.quantity > 0 && enabledPriceChange) {
-      options.btnPriceChange = true
-    }
-  }
-
   const $offersBtns = window.document.querySelectorAll('.offers-notification')
+  const offerBaseUrl = 'https://us-central1-ecom-offers-notification.cloudfunctions.net/app/offers/notification'
 
   if ($offersBtns) {
     for (let i = 0; i < $offersBtns.length; i++) {
       const element = $offersBtns[i]
-      element.onClick = function () {
-        console.log(this)
-        // open iframe
+      element.onclick = function () {
+        const url = offerBaseUrl + `?store_id=${this.dataset.store_id}&criterias=${this.dataset.criterias}&productId=${this.dataset.product_id}`
+        window.open(url, '_blank', 'location=yes,height=280,width=580,scrollbars=yes,status=yes')
       }
     }
   }
