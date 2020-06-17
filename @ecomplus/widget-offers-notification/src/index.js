4a2d68d3f37ac1e64fecc81c6b18d87d12327bb3
Talisson Ferreira <talissonf@gmail.com>
2020-06-17 01:36:27 -0300

    chore(index): changed window width to 320

--- @ecomplus/widget-offers-notification/src/index.js
+++ @ecomplus/widget-offers-notification/src/index.js
@@ -8,7 +8,7 @@ export default (options = {}) => {
       const element = $offersBtns[i]
       element.onclick = function () {
         const url = offerBaseUrl + `?store_id=${this.dataset.store_id}&criterias=${this.dataset.criterias}&productId=${this.dataset.product_id}`
-        window.open(url, '_blank', 'location=yes,height=280,width=580,scrollbars=yes,status=yes')
+        window.open(url, '_blank', 'location=yes,height=280,width=320,status=yes')
       }
     }
   }
