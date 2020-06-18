4117ae30bfb56cbbfc6acd9dc76d1fe96f953f83
Talisson Ferreira <talissonf@gmail.com>
2020-06-17 22:26:02 -0300

    chore(index): removed unused script

--- @ecomplus/widget-offers-notification/src/index.js
+++ @ecomplus/widget-offers-notification/src/index.js
@@ -1,15 +1,3 @@
 
 export default (options = {}) => {
-  const $offersBtns = window.document.querySelectorAll('.offers-notification')
-  const offerBaseUrl = 'https://us-central1-ecom-offers-notification.cloudfunctions.net/app/offers/notification'
-
-  if ($offersBtns) {
-    for (let i = 0; i < $offersBtns.length; i++) {
-      const element = $offersBtns[i]
-      element.onclick = function () {
-        const url = offerBaseUrl + `?store_id=${this.dataset.store_id}&criterias=${this.dataset.criterias}&productId=${this.dataset.product_id}`
-        window.open(url, '_blank', 'location=yes,height=280,width=320,status=yes')
-      }
-    }
-  }
 }
