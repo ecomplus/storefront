c86da84cede8e6d2631c6124624f01f95547a426
Leonardo Matos <leomp120894@gmail.com>
2020-07-29 15:52:18 -0300

    chore(lib): clearfix moved to /lib/ecom.js

--- @ecomplus/storefront-template/template/js/lib/clearfix.js
+++
@@ -1,7 +0,0 @@
-import ecomCart from '@ecomplus/shopping-cart'
-
-ecomCart.data.items.forEach(({ _id, flags }) => {
-  if (Array.isArray(flags) && flags.includes('__tmp')) {
-    ecomCart.removeItem(_id)
-  }
-})
