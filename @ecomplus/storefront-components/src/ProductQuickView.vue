c5c0d9c4bb878f2fbc69601764568451348797ea
Talisson Ferreira <talissonf@gmail.com>
2020-08-17 15:33:32 -0300

    chore(product-quick-view): initial commit

---
+++ @ecomplus/storefront-components/src/ProductQuickView.vue
@@ -0,0 +1,3 @@
+<script src="./js/ProductQuickView.js"></script>
+<template lang="html" src="./html/ProductQuickView.html"></template>
+<style lang="scss" src="./scss/ProductQuickView.scss"></style>
---
+++ @ecomplus/storefront-components/src/js/ProductQuickView.js
@@ -0,0 +1,26 @@
+export default {
+  name: 'ProductQuickView',
+
+  components: {
+  },
+
+  props: {
+    product: Object
+  },
+
+  data () {
+    return {
+      isVisible: false
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  },
+
+  created () {
+    console.log(this)
+  }
+}
