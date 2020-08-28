f56c30cbdd81f9d04f75e237164a4676d266bdde
Leonardo Matos <leomp120894@gmail.com>
2020-08-27 21:33:53 -0300

    chore: removing renamed component files
    [skip ci]

--- @ecomplus/storefront-components/src/ProductQuickView.vue
+++
@@ -1,3 +0,0 @@
-<script src="./js/ProductQuickView.js"></script>
-<template lang="html" src="./html/ProductQuickView.html"></template>
-<style lang="scss" src="./scss/ProductQuickView.scss"></style>
