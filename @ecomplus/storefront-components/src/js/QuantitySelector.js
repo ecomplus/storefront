1c116f3c8466bd656945c1e0ae5b8c027aea5748
Talisson Ferreira <talissonf@gmail.com>
2020-09-04 17:58:57 -0300

    chore: emitting events when the kit is changed

--- @ecomplus/storefront-components/src/js/QuantitySelector.js
+++ @ecomplus/storefront-components/src/js/QuantitySelector.js
@@ -1,4 +1,4 @@
-import { i18n } from '@ecomplus/utils'
+import { i18n, inStock } from '@ecomplus/utils'
 import AAlert from '../AAlert.vue'
 
 export default {
@@ -10,7 +10,7 @@ export default {
 
   data () {
     return {
-      itemsSelected: [],
+      selectedQuantities: {},
       isMax: false,
       isMin: false
     }
@@ -26,37 +26,56 @@ export default {
       default: 1
     },
     max: Number,
-    title: String
+    title: String,
+    buyButton: {
+      type: Boolean,
+      default: true
+    }
   },
 
   computed: {
     selectedQuantity () {
-      return this.itemsSelected.map(item => parseInt(item.quantity_selected, 10))
+      let total = 0
+      const { selectedQuantities } = this
+      Object.keys(selectedQuantities).forEach(key => {
+        if (selectedQuantities[key]) {
+          total += selectedQuantities[key]
+        }
+      })
+      return total
     },
+
     quantityRemaining () {
       return this.max
-        ? this.max - this.selectedQuantity.reduce((a, b) => a + b, 0)
+        ? this.max - this.selectedQuantity
         : 9999999
     },
+
     i19MaximumQuantity () {
       return i18n({
         pt_br: 'Quantidade Máxima',
         en_eu: 'Maximum Quantity'
       })
+    },
+
+    i19MinimumQuantity () {
+      return i18n({
+        pt_br: 'Quantidade mínima',
+        en_eu: 'Minimum Quantity'
+      })
+    },
+
+    i19BuyKit () {
+      return i18n({
+        pt_br: 'Comprar Kit',
+        en_eu: 'Buy Kit'
+      })
     }
   },
 
   methods: {
-    loadItems () {
-      this.items.forEach(item => {
-        this.itemsSelected.push({
-          productId: item._id,
-          quantity: item.quantity,
-          min_quantity: item.min_quantity,
-          quantity_selected: 0,
-          name: item.name
-        })
-      })
+    isInStock (item) {
+      return inStock(item)
     },
 
     change () {
@@ -65,11 +84,22 @@ export default {
         setTimeout(() => {
           this.isMax = false
         }, 6000)
+        return
       }
-    }
-  },
 
-  created () {
-    this.loadItems()
+      this.$emit('addToKit', this.selectedQuantities)
+    },
+
+    buyKit () {
+      if (this.selectedQuantity <= this.min) {
+        this.isMin = true
+        setTimeout(() => {
+          this.isMin = false
+        }, 6000)
+        return
+      }
+
+      this.$emit('buyKit', this.selectedQuantities)
+    }
   }
 }
