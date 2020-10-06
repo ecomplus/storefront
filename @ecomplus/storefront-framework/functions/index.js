2ba11798fd9126fd62b23d44329702419606314f
Leonardo Matos <leomp120894@gmail.com>
2020-10-06 13:53:09 -0300

    build(firebase-functions): replace @ecomplus/storefront-framework with @ecomplus/storefront-renderer
    reduce dependency tree for ssr only

--- @ecomplus/storefront-framework/functions/index.js
+++ @ecomplus/storefront-framework/functions/index.js
@@ -1,80 +1,44 @@
 const path = require('path')
 const functions = require('firebase-functions')
-const { firestore } = require('firebase-admin')
 
 const {
   getAssetsReferences,
   renderer,
   minifyHtml
-} = require('@ecomplus/storefront-framework')
-
-const freshViewCache = 'public, max-age=60, s-maxage=604800, stale-while-revalidate=2592000'
+} = require('@ecomplus/storefront-renderer')
 
 exports.ssr = functions.https.onRequest((req, res) => {
   const url = req.url.replace(/\?.*$/, '').replace(/\.html$/, '')
-  const { deploy } = functions.config()
-  const deployId = deploy && deploy.id
 
-  let documentRef
+  let cache
   try {
-    documentRef = firestore().doc(`views/${url}`)
-  } catch (e) {
+    cache = require(path.join(process.cwd(), '.bundles.json'))
+  } catch (err) {
+    console.error(err)
   }
 
-  return new Promise(resolve => {
-    if (documentRef) {
-      return documentRef.get()
-        .then(documentSnapshot => {
-          if (documentSnapshot.exists) {
-            if (deployId === documentSnapshot.get('deployKey')) {
-              res.set('Cache-Control', freshViewCache)
-            } else {
-              res.set('Cache-Control', 'public, max-age=10')
-            }
-            resolve(documentSnapshot.get('html'))
-          }
-        })
-        .catch(err => {
-          console.error(err)
-          resolve(null)
-        })
-    }
-    resolve(null)
-  })
+  const fallback = () => res
+    .set('Cache-Control', 'max-age=10')
+    .set('Location', '/404')
+    .status(302).end()
 
-    .then(storedHtml => {
-      if (storedHtml) {
-        res.status(200).send(storedHtml)
-      }
+  return renderer(url)
 
-      if (documentRef || !storedHtml) {
-        let cache
-        try {
-          cache = require(path.join(process.cwd(), '.bundles.json'))
-        } catch (err) {
-          console.error(err)
+    .then(html => {
+      if (html) {
+        if (cache) {
+          html = minifyHtml(html, getAssetsReferences(cache.assetsByChunkName))
         }
-
-        renderer(url)
-          .then(html => {
-            if (html) {
-              if (cache) {
-                html = minifyHtml(html, getAssetsReferences(cache.assetsByChunkName))
-              }
-              if (!storedHtml) {
-                res.set('Cache-Control', freshViewCache).status(200).send(html)
-              }
-              if (documentRef) {
-                return documentRef.set({ html, deployId })
-              }
-            } else {
-              res.set('Location', '/404').status(302).end()
-            }
-            return false
-          })
-          .catch(console.error)
+        res.set('Cache-Control', 'public, max-age=60, s-maxage=604800, stale-while-revalidate=2592000')
+          .status(200).send(html)
+      } else {
+        fallback()
       }
+      return false
+    })
 
-      return true
+    .catch(err => {
+      fallback()
+      console.error(err)
     })
 })
