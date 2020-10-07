9f2a9e489ad842e1558e9cd4addacce1ec6a50f9
Leonardo Matos <leomp120894@gmail.com>
2020-10-07 02:38:41 -0300

    refactor(functions): import ssr function from @ecomplus/storefront-renderer

--- @ecomplus/storefront-framework/functions/index.js
+++ @ecomplus/storefront-framework/functions/index.js
@@ -1,44 +1,5 @@
-const path = require('path')
 const functions = require('firebase-functions')
 
-const {
-  getAssetsReferences,
-  renderer,
-  minifyHtml
-} = require('@ecomplus/storefront-renderer')
+const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
-exports.ssr = functions.https.onRequest((req, res) => {
-  const url = req.url.replace(/\?.*$/, '').replace(/\.html$/, '')
-
-  let cache
-  try {
-    cache = require(path.join(process.cwd(), '.bundles.json'))
-  } catch (err) {
-    console.error(err)
-  }
-
-  const fallback = () => res
-    .set('Cache-Control', 'max-age=10')
-    .set('Location', '/404')
-    .status(302).end()
-
-  return renderer(url)
-
-    .then(html => {
-      if (html) {
-        if (cache) {
-          html = minifyHtml(html, getAssetsReferences(cache.assetsByChunkName))
-        }
-        res.set('Cache-Control', 'public, max-age=60, s-maxage=604800, stale-while-revalidate=2592000')
-          .status(200).send(html)
-      } else {
-        fallback()
-      }
-      return false
-    })
-
-    .catch(err => {
-      fallback()
-      console.error(err)
-    })
-})
+exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
