4204af38711488c43e52e2303442ade758f44596
Talisson Ferreira <talissonf@gmail.com>
2020-12-02 17:31:09 -0300

    fix(build-themes): import boostwatch variables, mixins and functions

--- scripts/build-themes.js
+++ scripts/build-themes.js
@@ -5,6 +5,8 @@ const ROOT_DIR = path.resolve(__dirname, '..')
 
 let scssVars = ''
 ;[
+  '@ecomplus/storefront-twbs/scss/bootstrap/scss/_functions.scss',
+  '@ecomplus/storefront-twbs/scss/bootstrap/scss/_variables.scss',
   '@ecomplus/storefront-template/template/scss/_variables.scss',
   '@ecomplus/storefront-twbs/scss/_variables.scss'
 ].forEach(filepath => {
@@ -20,6 +22,20 @@ const themesPath = path.resolve(ROOT_DIR, '@ecomplus/storefront-template/templat
 const themes = fs.readdirSync(themesPath)
 themes.forEach(theme => {
   let styleSheet = ''
+  try {
+    // fix mixins
+    styleSheet += fs.readFileSync(path.resolve(ROOT_DIR, '@ecomplus/storefront-twbs/scss/bootstrap/scss/_mixins.scss')).toString('utf8')
+    const bootswatchThemePath = path.resolve(`${ROOT_DIR}/node_modules/bootswatch/dist`, theme)
+    const boostwatchScssFiles = fs.readdirSync(bootswatchThemePath)
+    ;['_variables.scss', '_bootswatch.scss'].forEach(scssFile => {
+      if (boostwatchScssFiles.includes(scssFile)) {
+        styleSheet += fs.readFileSync(path.resolve(bootswatchThemePath, scssFile)).toString('utf8')
+      }
+    })
+  } catch (error) {
+    console.log(`[skiped-dir] Theme ${theme} not found in bootswatch/dist`)
+  }
+
   const themePath = path.resolve(themesPath, theme)
   const scssFiles = fs.readdirSync(themePath)
   ;['_variables.scss', '_custom.scss'].forEach(scssFile => {
@@ -29,7 +45,11 @@ themes.forEach(theme => {
   })
 
   const outputCss = sass.renderSync({
-    data: `${scssVars} ${styleSheet}`
+    data: `${scssVars} ${styleSheet}`,
+    includePaths: [
+      '@ecomplus/storefront-twbs/scss/bootstrap/scss/',
+    ],
+    outputStyle: 'expanded'
   }).css.toString()
 
   if (outputCss) {
