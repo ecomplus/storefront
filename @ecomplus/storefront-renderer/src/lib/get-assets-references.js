289e0c558138f9961445c01281aaebb213e39f05
Leonardo Matos <leomp120894@gmail.com>
2021-02-26 23:29:55 -0300

    fix(get-assets-reference): should not require './entry.js'

--- @ecomplus/storefront-renderer/src/lib/get-assets-references.js
+++ @ecomplus/storefront-renderer/src/lib/get-assets-references.js
@@ -1,17 +1,20 @@
 'use strict'
 
-const entry = require('./entry')
-
 module.exports = (assetsByChunkName, cb) => {
   // map assets to replace from absolute filename to output chunk with hash
   const entryAssetsReference = {}
-  for (const entryName in entry) {
-    if (entry[entryName] && Array.isArray(assetsByChunkName[entryName])) {
-      assetsByChunkName[entryName].forEach(outputFilename => {
+  for (const chunkName in assetsByChunkName) {
+    const assets = assetsByChunkName[chunkName]
+    if (
+      Array.isArray(assets) &&
+      typeof assets[0] === 'string' &&
+      !assets[0].startsWith('chunk.')
+    ) {
+      assets.forEach(outputFilename => {
         const ext = outputFilename.split('.').pop()
         if (ext !== 'map') {
           // eg.: storefront.js => storefront.{hash}.js
-          const refFilename = `${entryName}.${ext}`
+          const refFilename = `${chunkName}.${ext}`
           entryAssetsReference[refFilename] = outputFilename
 
           if (typeof cb === 'function') {
