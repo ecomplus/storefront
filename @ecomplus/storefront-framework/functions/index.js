90819d1b631617dd6d90c42fe0d52138f8d5087c
Leonardo Matos <leomp120894@gmail.com>
2021-03-28 06:06:14 -0300

    fix(ssr): setting custom cache control for firebase by status code

--- @ecomplus/storefront-framework/functions/index.js
+++ @ecomplus/storefront-framework/functions/index.js
@@ -2,4 +2,14 @@ const functions = require('firebase-functions')
 
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
-exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
+exports.ssr = functions.https.onRequest((req, res) => {
+  return ssr(req, res, statusCode => {
+    switch (statusCode) {
+      case 200:
+        return 'public, max-age=60, s-maxage=604800'
+      case 404:
+        return 'public, max-age=60, s-maxage=86400'
+    }
+    return 'public, max-age=30, s-maxage=3600'
+  })
+})
