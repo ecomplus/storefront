3d92a306fa1145a48699cc2628526e6340ab8a61
Leonardo Matos <leomp120894@gmail.com>
2021-03-29 16:50:40 -0300

    chore(ssr): edit default framework firebase functions

--- @ecomplus/storefront-framework/functions/index.js
+++ @ecomplus/storefront-framework/functions/index.js
@@ -2,14 +2,4 @@ const functions = require('firebase-functions')
 
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
-exports.ssr = functions.https.onRequest((req, res) => {
-  return ssr(req, res, statusCode => {
-    switch (statusCode) {
-      case 200:
-        return 'public, max-age=60, s-maxage=604800'
-      case 404:
-        return 'public, max-age=60, s-maxage=86400'
-    }
-    return 'public, max-age=30, s-maxage=3600'
-  })
-})
+exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
