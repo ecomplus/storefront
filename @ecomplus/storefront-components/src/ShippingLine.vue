b9bd5fd7129dc40d18326b3de92ca00e55831a12
Leonardo Matos <leomp120894@gmail.com>
2021-06-16 15:36:40 -0300

    feat(components/shipping-line): better notes for same and next day delivery (#474)
    show delivery instructions
    
    fixes https://github.com/ecomplus/storefront/issues/474
    
    [skip ci]

--- @ecomplus/storefront-components/src/ShippingLine.vue
+++ @ecomplus/storefront-components/src/ShippingLine.vue
@@ -1,2 +1,3 @@
 <script src="./js/ShippingLine.js"></script>
 <template lang="html" src="./html/ShippingLine.html"></template>
+<style lang="scss" src="./scss/ShippingLine.scss"></style>
--- @ecomplus/storefront-components/src/js/ShippingLine.js
+++ @ecomplus/storefront-components/src/js/ShippingLine.js
@@ -1,6 +1,10 @@
 import {
   i19days,
+  i19free,
   i19freeShipping,
+  // i19pickUpToday,
+  i19receiveToday,
+  i19untilTomorrow,
   i19upTo,
   i19workingDays
 } from '@ecomplus/i18n'
@@ -9,6 +13,8 @@ import {
   i18n,
   formatMoney
 } from '@ecomplus/utils'
+
+const i19pickUpToday = 'Retire hoje'
 
 export default {
   name: 'ShippingLine',
@@ -29,17 +35,24 @@ export default {
       if (shipping.delivery_time) {
         days += shipping.delivery_time.days
       }
-      return `${i18n(i19upTo)} ${days} ${i18n(isWorkingDays ? i19workingDays : i19days)}`
+      if (days > 1) {
+        return `${i18n(i19upTo)} ${days} ` +
+          i18n(isWorkingDays ? i19workingDays : i19days).toLowerCase()
+      }
+      return i18n(days === 1
+        ? i19untilTomorrow
+        : shipping.pick_up ? i19pickUpToday : i19receiveToday)
     },
 
     freightValueStr () {
-      const freight = typeof this.shippingLine.total_price === 'number'
-        ? this.shippingLine.total_price
-        : this.shippingLine.price
+      const { shippingLine } = this
+      const freight = typeof shippingLine.total_price === 'number'
+        ? shippingLine.total_price
+        : shippingLine.price
       if (freight) {
         return formatMoney(freight)
       } else {
-        return i18n(i19freeShipping)
+        return i18n(shippingLine.pick_up ? i19free : i19freeShipping)
       }
     }
   }
