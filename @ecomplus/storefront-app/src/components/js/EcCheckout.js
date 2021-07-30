8d58834029eaaada4ff765dbc38778690737f732
Leonardo Matos <leomp120894@gmail.com>
2021-07-30 19:59:03 -0300

    fix(app/checkout): fix (and show even on guest) account/address forms submit buttons

--- @ecomplus/storefront-app/src/components/js/EcCheckout.js
+++ @ecomplus/storefront-app/src/components/js/EcCheckout.js
@@ -4,6 +4,7 @@ import {
   i19buyAlsoMsg,
   i19checkout,
   i19orderNotesInputMsg,
+  // i19proceed,
   i19selectedOffers
 } from '@ecomplus/i18n'
 
@@ -135,6 +136,7 @@ export default {
     i19buyAlsoMsg: () => i18n(i19buyAlsoMsg),
     i19checkout: () => i18n(i19checkout),
     i19orderNotesInputMsg: () => i18n(i19orderNotesInputMsg),
+    i19proceed: () => i18n({ en_us: 'Proceed', pt_br: 'Prosseguir' }),
     i19selectedOffers: () => i18n(i19selectedOffers),
     modulesPayload: () => baseModulesRequestData,
 
