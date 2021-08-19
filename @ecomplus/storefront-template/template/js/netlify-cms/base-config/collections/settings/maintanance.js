a02b6fcafc92af29314f27b508553293ab35c58b
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-08-19 01:13:22 -0300

    refactor: no dry and fix head ejs

--- @ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/settings/maintanance.js
+++ @ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/settings/maintanance.js
@@ -1,25 +1,25 @@
 export default ({ baseDir }) => ({
-    label: 'Manutenção',
-    file: `${baseDir}content/maintanance.json`,
-    name: 'maintanance',
-    fields: [
-      {
-        label: 'Título da Página de Manutenção',
-        required: false,
-        name: 'title',
-        widget: 'string'
-      },
-      {
-        label: 'Corpo',
-        name: 'body',
-        widget: 'markdown'
-      },
-      {
-        label: 'Ativar Página de Manutenção',
-        name: 'activate',
-        widget: 'boolean',
-        default: false,
-        required: false
-      } 
-    ]
-  })
+  label: 'Manutenção',
+  file: `${baseDir}content/maintanance.json`,
+  name: 'maintanance',
+  fields: [
+    {
+      label: 'Título da Página de Manutenção',
+      required: false,
+      name: 'title',
+      widget: 'string'
+    },
+    {
+      label: 'Corpo',
+      name: 'body',
+      widget: 'markdown'
+    },
+    {
+      label: 'Ativar Página de Manutenção',
+      name: 'activate',
+      widget: 'boolean',
+      default: false,
+      required: false
+    } 
+  ]
+})
