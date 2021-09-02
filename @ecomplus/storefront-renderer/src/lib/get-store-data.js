f281e4308f26ef8e315901705db544b46c598f32
Leonardo Matos <leomp120894@gmail.com>
2021-09-02 12:31:27 -0300

    fix(renderer/get-store-data): catch els index not found (any items) error on base store data setup

--- @ecomplus/storefront-renderer/src/lib/get-store-data.js
+++ @ecomplus/storefront-renderer/src/lib/get-store-data.js
@@ -71,9 +71,17 @@ module.exports = (storeId = config.storeId, pubSrc = paths.pub, ecomManifest) =>
             })
           } else if (search) {
             // request with search engine instance
-            req = ecomSearch.fetch().then(() => {
-              data[prop] = ecomSearch.getItems()
-            })
+            req = ecomSearch.fetch()
+              .then(() => {
+                data[prop] = ecomSearch.getItems()
+              })
+              .catch(error => {
+                if (error.response && error.response.status === 404) {
+                  data[prop] = []
+                } else {
+                  throw error
+                }
+              })
           }
 
           // add request to promises list
