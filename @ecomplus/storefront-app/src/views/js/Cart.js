3db80381e9e492fd4afd24f8351af00587b41517
Leonardo Matos <leomp120894@gmail.com>
2021-10-05 05:12:40 +0000

    feat(app/cart): parse `cart_items` url params if set, then clear url query

--- @ecomplus/storefront-app/src/views/js/Cart.js
+++ @ecomplus/storefront-app/src/views/js/Cart.js
@@ -48,6 +48,35 @@ export default {
       this.fetchCartItems({ items: [item] })
     }
     ecomCart.on('addItem', fetchAddedItem)
+    const urlParams = new URLSearchParams(window.location.search)
+    const urlItems = urlParams.get('cart_items')
+    if (urlItems) {
+      try {
+        const items = JSON.parse(urlItems)
+        if (Array.isArray(items)) {
+          items.forEach(item => {
+            if (item) {
+              ecomCart.addItem({
+                quantity: 1,
+                price: 1,
+                ...item
+              })
+            }
+          })
+        }
+      } catch (err) {
+        console.error(err)
+      }
+      if (window.history) {
+        urlParams.delete('cart_items')
+        const query = urlParams.toString()
+        const { pathname } = window.location
+        window.history.pushState({
+          pathname,
+          query
+        }, document.title, pathname + (query ? `?${query}` : ''))
+      }
+    }
     this.fetchCartItems({}).then(() => {
       upsertCart().then(() => {
         ecomCart.on('change', upsertCart)
