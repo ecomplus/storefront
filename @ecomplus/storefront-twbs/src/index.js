3f3bb459b934074a4dcd0249b4a9c4f0eefea612
Leonardo Matos <leomp120894@gmail.com>
2022-03-21 19:14:12 -0300

    fix(twbs/js): fix bs5 imports and remove jquery
    BREAKING CHANGE: jquery no more set on window nor exported

--- @ecomplus/storefront-twbs/src/index.js
+++ @ecomplus/storefront-twbs/src/index.js
@@ -6,7 +6,6 @@
 
 import { name, version } from './../package.json'
 
-import $ from './$'
 import animateCss from './animate-css'
 import './bootstrap'
 
@@ -16,8 +15,5 @@ export * from './_env'
 
 export {
   _self,
-  $,
   animateCss
 }
-
-export default $
