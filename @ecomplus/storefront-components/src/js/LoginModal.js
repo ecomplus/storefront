909528dc25e65b5242eb7fee2323ceaf71ba857e
Leonardo Matos <leomp120894@gmail.com>
2022-03-25 17:36:19 -0300

    chore(components/login-modal): no subscriptions link at all
    https://github.com/ecomplus/storefront/issues/660\#issuecomment-1079162759

--- @ecomplus/storefront-components/src/js/LoginModal.js
+++ @ecomplus/storefront-components/src/js/LoginModal.js
@@ -53,10 +53,6 @@ export default {
       type: String,
       default: '/app/#/account/favorites'
     },
-    subscriptionsUrl: {
-      type: String,
-      default: '/app/#/account/subscriptions'
-    },
     ecomPassport: {
       type: Object,
       default () {
@@ -96,10 +92,6 @@ export default {
     i19myFavorites: () => i18n(i19myFavorites),
     i19myOrders: () => i18n(i19myOrders),
     i19noProfileFoundWithEmail: () => i18n(i19noProfileFoundWithEmail),
-    i19mySubscriptions: () => i18n({
-      pt_br: 'Minhas assinaturas',
-      en_us: 'My subscriptions'
-    }),
     i19signInWith: () => i18n(i19signInWith),
     i19signInWithAnotherEmail: () => i18n(i19signInWithAnotherEmail),
     i19signUp: () => i18n(i19signUp),
