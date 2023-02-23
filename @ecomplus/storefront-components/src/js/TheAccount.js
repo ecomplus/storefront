4217874aa013dc546045cf570516af6d98694ad9
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-02-23 20:19:06 -0300

    fix(components/account): remove vertical scroll and watch login to setup subscription tab (#873)
    * fix(the-account): eliminate vertical scroll and insert subscription tab with watch
    
    * chore(the-account): modify add tab subscription method
    
    * chore(the-account): change authentication for reactive method
    
    * chore(the-account): on login insert tab subscription
    
    * Update @ecomplus/storefront-components/src/js/TheAccount.js
    
    ---------
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>

--- @ecomplus/storefront-components/src/js/TheAccount.js
+++ @ecomplus/storefront-components/src/js/TheAccount.js
@@ -90,6 +90,27 @@ export default {
   },
 
   methods: {
+    hasTab (tabValue) {
+      return this.navTabs.some(tab => tab.value === tabValue)
+    },
+
+    insertSubscriptionTab () {
+      const hasSubscriptions = this.hasTab('subscriptions')
+      if (this.ecomPassport.checkAuthorization() && !hasSubscriptions) {
+        this.ecomPassport.requestApi('/orders.json?transactions.type=recurrence&limit=1&fields=_id')
+          .then(({ data }) => {
+            const { result } = data
+            if (result.length) {
+              this.navTabs.push({
+                label: this.i19subscriptions,
+                value: 'subscriptions'
+              })
+            }
+          })
+          .catch(console.error)
+      }
+    },
+
     login (ecomPassport) {
       if (ecomPassport.checkAuthorization()) {
         this.localCustomer = ecomPassport.getCustomer()
@@ -108,7 +129,7 @@ export default {
   watch: {
     customer: {
       handler (customer) {
-        const hasPoints = this.navTabs.some(tab => tab.value === 'points')
+        const hasPoints = this.hasTab('points')
         if (Array.isArray(customer.loyalty_points_entries) && customer.loyalty_points_entries.length && !hasPoints) {
           this.navTabs.push({
             label: 'Cashback',
@@ -138,18 +159,10 @@ export default {
     ]
     const { favorites } = this.ecomPassport.getCustomer()
     this.favoriteIds = favorites || []
-    if (this.ecomPassport.checkAuthorization()) {
-      this.ecomPassport.requestApi('/orders.json?transactions.type=recurrence&limit=1&fields=_id')
-        .then(({ data }) => {
-          const { result } = data
-          if (result.length) {
-            this.navTabs.push({
-              label: this.i19subscriptions,
-              value: 'subscriptions'
-            })
-          }
-        })
-        .catch(console.error)
-    }
+    this.insertSubscriptionTab()
+    this.ecomPassport.on('login', this.insertSubscriptionTab)
+    this.$once('hook:beforeDestroy', () => {
+      this.ecomPassport.off('login', this.insertSubscriptionTab)
+    })
   }
 }
