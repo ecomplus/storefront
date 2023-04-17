b60f660d2aea9064ce956ec2b576a4ae82341a48
Leonardo Matos <leomp120894@gmail.com>
2023-04-17 15:50:31 -0300

    chore(template): persisting `referall` from URL param on user session

--- @ecomplus/storefront-template/template/js/lib/persist-utm.js
+++ @ecomplus/storefront-template/template/js/lib/persist-utm.js
@@ -2,7 +2,7 @@ const { sessionStorage } = window
 const storageKey = 'ecomUtm'
 
 let isCurrentUtm
-const utm = JSON.parse(window.sessionStorage.getItem(storageKey)) || {}
+const utm = JSON.parse(sessionStorage.getItem(storageKey)) || {}
 
 const urlParams = new URLSearchParams(window.location.search)
 ;['source', 'medium', 'campaign', 'term', 'content'].forEach(utmParam => {
@@ -15,6 +15,9 @@ const urlParams = new URLSearchParams(window.location.search)
 
 if (isCurrentUtm) {
   sessionStorage.setItem(storageKey, JSON.stringify(utm))
+}
+if (urlParams.get('referral') && !sessionStorage.getItem('ecomReferral')) {
+  sessionStorage.setItem('ecomReferral', urlParams.get('referral'))
 }
 
 export default utm
