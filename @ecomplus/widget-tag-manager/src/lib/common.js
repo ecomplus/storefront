7e2627b1bd7d777b13e6e576cfc5a930de2a40d1
Leonardo Matos <leomp120894@gmail.com>
2023-05-11 18:24:46 -0300

    fix(widget-tag-manager): fallback try to normalize `item.name` by last space or custom regex
    when variation founb and no / separation
    
    fix impressions matching only .product-card elements

--- @ecomplus/widget-tag-manager/src/lib/common.js
+++ @ecomplus/widget-tag-manager/src/lib/common.js
@@ -1,12 +1,23 @@
-import { $ecomConfig, name, price } from '@ecomplus/utils'
+import {
+  $ecomConfig,
+  name as getName,
+  price as getPrice
+} from '@ecomplus/utils'
 
 export const currencyCode = $ecomConfig.get('currency') || 'BRL'
 
 export const getProductData = item => {
+  const [name, ...variants] = getName(item).split(' / ')
   const productData = {
-    name: name(item).replace(/\s\/\s.*/, ''),
+    name,
     id: item.sku,
-    price: price(item).toFixed(2)
+    price: getPrice(item).toFixed(2)
+  }
+  if (variants && variants.length) {
+    productData.variant = variants.join(' / ')
+  } else if (item.variation_id) {
+    productData.name = name.replace(window.__customGTMVariantRegex || /\s[^\s]+$/, '')
+    productData.variant = name.replace(productData.name, '').trim()
   }
   if (item.quantity) {
     productData.quantity = item.quantity
