56c17b5fe6037b086321c5da5ec39d91399797b0
matheusgnreis <matheus@e-com.club>
2023-05-31 17:54:54 -0300

    feat(multi-shelf): first step for multi shelf

--- @ecomplus/storefront-template/template/js/netlify-cms/base-config/sections.js
+++ @ecomplus/storefront-template/template/js/netlify-cms/base-config/sections.js
@@ -323,6 +323,97 @@ export default ({ state }) => [
     ]
   },
   {
+    label: 'Multi Estante de produtos',
+    name: 'multi-collection-shelf',
+    widget: 'object',
+    icon: 'https://api.iconify.design/bi:bag-check.svg',
+    fields: [
+      {
+        label: 'Coleções de produtos',
+        required: false,
+        name: 'collection_ids',
+        hint: 'Se este campo não for preenchido, serão listados os produtos mais populares da loja',
+        widget: 'select',
+        multiple: true,
+        options: [{
+          resource: 'collections',
+          label: ''
+        }, {
+          resource: 'categories',
+          label: 'Categoria: '
+        }, {
+          resource: 'brands',
+          label: 'Marca: '
+        }].reduce((options, shelf) => {
+          state.routes.forEach(({ _id, resource, name, path }) => {
+            if (resource === shelf.resource) {
+              options.push({
+                label: shelf.label + name,
+                value: `${_id}:${resource}:${name}:${path}`
+              })
+            }
+          })
+          return options
+        }, [])
+      },
+      searchOrderField,
+      {
+        label: 'Embaralhar produtos',
+        name: 'shuffle',
+        widget: 'boolean',
+        default: false
+      },
+      {
+        label: 'Título da estante',
+        required: false,
+        name: 'title',
+        hint: 'Usará o nome da coleção por padrão',
+        widget: 'string'
+      },
+      {
+        label: 'Link no título',
+        required: false,
+        name: 'link',
+        hint: 'Usará o slug da coleção por padrão',
+        widget: 'string'
+      },
+      {
+        label: 'Sem cabeçalho',
+        required: false,
+        name: 'headless',
+        widget: 'boolean',
+        hint: 'Mostrar apenas a lista de produtos, sem título ou link'
+      },
+      {
+        label: 'Limite de itens',
+        required: false,
+        name: 'limit',
+        widget: 'number',
+        min: 1,
+        max: 24,
+        default: 12
+      },
+      {
+        label: 'Paginação',
+        required: false,
+        name: 'page',
+        hint: 'Aumente o número da página para pular os itens iniciais e repetir estantes com a mesma coleção',
+        widget: 'number',
+        min: 1,
+        default: 1
+      },
+      {
+        label: 'Carousel autoplay',
+        required: false,
+        name: 'autoplay',
+        hint: 'Exibição de cada página em milisegundos, 0 desativa o autoplay',
+        min: 0,
+        step: 1000,
+        widget: 'number'
+      }
+    ]
+  },
+  {
     label: 'Vitrine dinâmica',
     name: 'dynamic-showcase',
     widget: 'object',
