2d322b217e47aa9900c5bba11a4937c58bd3917a
Leonardo Matos <leomp120894@gmail.com>
2023-08-22 14:38:47 -0300

    Update @ecomplus/storefront-components/src/js/TheProduct.js

--- @ecomplus/storefront-components/src/js/TheProduct.js
+++ @ecomplus/storefront-components/src/js/TheProduct.js
@@ -427,7 +427,7 @@ export default {
         searchParams.set('variation_id', variationId)
         window.history.pushState({
           pathname,
-          param: searchParams.toString()
+          params: searchParams.toString()
         }, '', `${pathname}?${searchParams.toString()}`)
         this.showVariationPicture(this.selectedVariation)
       }
