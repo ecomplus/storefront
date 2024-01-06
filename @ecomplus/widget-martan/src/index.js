30e391069adaf5f7119aed640f5cf21194b84daa
Talisson Trindade <talissonf@gmail.com>
2024-01-06 11:37:56 -0300

    chore(martan): targets that received widgets

--- @ecomplus/widget-martan/src/index.js
+++ @ecomplus/widget-martan/src/index.js
@@ -1,7 +1,23 @@
-export default options => {
-  if (options && options.martanStoreId && options.martanWidgetKey) {
+import Reviews from './widgets/reviews'
+import Snippets from './widgets/snippets'
+
+export const MARTAN_API = 'https://widgets.martan.app/v1'
+
+export default (options) => {
+  if (options && options.store_id && options.web_id) {
     console.log('Martan widget started with EJS appends')
+    if (options.widget_snippet && options.widget_snippet.is_enabled) {
+      Snippets(options)
+    }
+
+    if (options.widget_review && options.widget_review.is_enabled) {
+      Reviews(options)
+    }
   } else {
-    console.error(new Error('Can\'t setup Martan widget without `martanStoreId` and `martanWidgetKey`'))
+    console.error(
+      new Error(
+        "Can't setup Martan widget without `martanStoreId` and `martanWidgetKey`"
+      )
+    )
   }
 }
