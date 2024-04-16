7bae9b41b7a376ce7fa9d882fb2fd99a1640e351
Leonardo Matos <leomp120894@gmail.com>
2024-04-16 02:38:06 -0300

    fix(app/account): early redirect to external auth when unauthorized on account view

--- @ecomplus/storefront-app/src/views/js/Account.js
+++ @ecomplus/storefront-app/src/views/js/Account.js
@@ -86,6 +86,13 @@ export default {
     }
   },
 
+  created () {
+    const isExternalAuth = Boolean(window.$firebaseConfig && window.$firebaseConfig.authDomain)
+    if (isExternalAuth && !this.ecomPassport.checkAuthorization()) {
+      window.location.href = '/app/account'
+    }
+  },
+
   mounted () {
     this.isMounted = true
   }
